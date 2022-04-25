b0f6674357cb6115cf9f2e89aa34ebd9b5912b86
ShineD-161 (Arun) <ShineD-161@domain.wgs.local>
2022-04-25 12:40:21 +0530

    change to loader

--- src/Components/Emails/CreateEmail.js
+++ src/Components/Emails/CreateEmail.js
@@ -108,11 +108,18 @@ const CreateEmail = (props) => {
       });
   };
 
-  const templateClicked = (template) => {
-    console.log(template);
+  const templateClicked = (template,e) => {
+    const div = document.querySelector("img.active");
+   
+    if(div){
+     div.classList.remove('active');
+    }
+   
+    
     setTemplateId(template.id);
     setTemplateName(template.name);
     setTemplate(template.source_code);
+    e.target.classList.toggle('active');
   };
 
   const emailSubjectChanged = (e) => {
@@ -213,46 +220,21 @@ const CreateEmail = (props) => {
               <OwlCarousel
                 className="mail-templates owl-carousel owl-theme"
                 margin={20}
-                items={4}
+                items={5}
+              
                 loop
                 nav
               >
-                {/* <div className="item">
-                  <img src={path_image + "content_added1.png"} alt="" />
-                  <p>Template 1</p>
-                </div>
-                <div className="item">
-                  <img src={path_image + "welcome_email1.png"} alt="" />
-                  <p>Template 2</p>
-                </div>
-                <div className="item">
-                  <img src={path_image + "content_added1.png"} alt="" />
-                  <p>Template 3</p>
-                </div>
-                <div className="item">
-                  <img src={path_image + "welcome_email1.png"} alt="" />
-                  <p>Template 4</p>
-                </div>
-                <div className="item">
-                  <img src={path_image + "content_added1.png"} alt="" />
-                  <p>Template 5</p>
-                </div>
-                <div className="item">
-                  <img src={path_image + "welcome_email1.png"} alt="" />
-                  <p>Template 6</p>
-                </div> */}
-
                 {templateList.map((template) => {
                   return (
                     <>
                       <div
                         className="item"
-                        onClick={() => templateClicked(template)}
+                        onClick={(e) => templateClicked(template,e)}
                       >
                         <img src={path_image + "content_added1.png"} alt="" />
                         <p>{template.name}</p>
                       </div>
-                      ;
                     </>
                   );
                 })}
