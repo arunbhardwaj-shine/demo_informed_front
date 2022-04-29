8c5a1d24f6bd9cb85cd88e722f561ccb4f4e3388
ShineD-117 <ShineD-117@domain.wgs.local>
2022-04-29 13:49:40 +0530

    mail sent via smartlist

--- src/Components/Emails/SelectHCP.js
+++ src/Components/Emails/SelectHCP.js
@@ -61,7 +61,16 @@ const SelectHCP = () => {
                     Next{" "}
                   </button>
                 ) : (
-                  <Link to="/VerifyHCP" state={{ UserSelected: templateId }}>
+                  // <Link to="/VerifyHCP" state={{ UserSelected: templateId }}>
+                  //   <button className="btn btn-primary btn-filled next">
+                  //     Next
+                  //   </button>
+                  // </Link>
+
+                  <Link
+                    to="/SelectSmartList"
+                    state={{ UserSelected: templateId }}
+                  >
                     <button className="btn btn-primary btn-filled next">
                       Next
                     </button>
