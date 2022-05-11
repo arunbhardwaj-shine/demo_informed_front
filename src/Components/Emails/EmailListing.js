69e78bd166e97c02abe7fc257ad14fba6ac068f8
ShineD-117 <ShineD-117@domain.wgs.local>
2022-05-11 11:28:05 +0530

    added creator name

--- src/Components/Emails/EmailListing.js
+++ src/Components/Emails/EmailListing.js
@@ -1246,7 +1246,7 @@ const EmailList = (props) => {
                                 className="btn btn-primary edit btn-filled"
                                 onClick={() => {
                                   getEmailData(null);
-                                  getSelectedSmartListData(null);
+                                  // getSelectedSmartListData(null);
                                   draftNavigate(
                                     data.id,
                                     data.pdf_id,
