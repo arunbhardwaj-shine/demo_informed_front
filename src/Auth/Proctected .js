45f04cb1c1781f6d34128eb8dacca13be010b11b
ShineD-117 <ShineD-117@domain.wgs.local>
2022-05-17 15:34:24 +0530

    edit functionality

--- src/Auth/Proctected .js
+++ src/Auth/Proctected .js
@@ -1,16 +1,15 @@
-import React, { useEffect } from 'react'
-import { Navigate, Outlet, } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Navigate, Outlet } from "react-router-dom";
 const Proctected = () => {
+  // console.log(<Outlet/>)
+  let Token = localStorage.getItem("Token");
+  return Token ? (
+    <>
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to="/webinar" />
+  );
+};
 
-// console.log(<Outlet/>)
-let Token = localStorage.getItem("Token")
-return Token? (
-<>
-<Outlet/>
-</>
-): <Navigate to="/webinar"/> 
-
-
-}
-
-export default Proctected
+export default Proctected;
