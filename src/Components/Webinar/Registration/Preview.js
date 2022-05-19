7ee7a6333e8af5c97252b1d356c7af00ea5aeba1
ShineD-188 <ShineD-188@domain.wgs.local>
2022-05-19 15:22:20 +0530

    changes to template

--- src/Components/Webinar/Registration/Preview.js
+++ src/Components/Webinar/Registration/Preview.js
@@ -3,7 +3,7 @@ import { Button, Modal } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 import { ensurePluginOrder } from 'react-table';
 import ExportApi from '../../../Api/ExportApi';
-import { browserName, browserVersion } from "react-device-detect";
+// import { browserName, browserVersion } from "react-device-detect";
 const Preview = () => {
   const [data, setData] = useState();
   const [show, setShow] = useState(false);
@@ -37,7 +37,7 @@ const Preview = () => {
      let email=document.getElementById("email").value
       // bind function for close the popup
   
-      ExportApi.CreateParticipant(name,country,browserName,email,parms.url,parms.stats).then((resp) => {
+      ExportApi.CreateParticipant(name,country,email,parms.url,parms.stats).then((resp) => {
         if (resp.ok) {
           console.log(resp.data)
           if(resp.data.code==200)
