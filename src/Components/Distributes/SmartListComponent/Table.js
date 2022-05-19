62a86779792617cf71aed9e351244c207859c7c4
varunshinedezign <varun.verma@shinedezign.email>
2022-05-19 17:28:21 +0530

    Change the message.

--- src/Components/Distributes/SmartListComponent/Table.js
+++ src/Components/Distributes/SmartListComponent/Table.js
@@ -840,7 +840,7 @@ const Table = (props, ref) => {
                     class="btn btn-primary btn-filled create"
                     onClick={showFileInReadersList}
                   >
-                    Craete
+                    Create
                   </button>
                 </div>
               </div>
