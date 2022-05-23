b10cc4457e47f11b8a848c3dd8119f3c60ac2502
varunshinedezign <varun.verma@shinedezign.email>
2022-05-23 15:37:24 +0530

    prevent the selected country value.

--- src/Components/Distributes/SmartListComponent/Table.js
+++ src/Components/Distributes/SmartListComponent/Table.js
@@ -1420,7 +1420,7 @@ const Table = (props, ref) => {
                                   onContactTypeChange(event, i)
                                 }
                               >
-                                <option selected>Select Type</option>
+                                <option selected value="">Select Type</option>
                                 <option value="HCP">HCP</option>
                                 <option value="Staff">Staff</option>
                                 <option value="Test Users">Test Users</option>
@@ -1435,7 +1435,7 @@ const Table = (props, ref) => {
                                 aria-label="select"
                                 onChange={(event) => onCountryChange(event, i)}
                               >
-                                <option selected>Select Country</option>
+                                <option value="" selected>Select Country</option>
                                 {countryall.length === 0
                                   ? ""
                                   : Object.entries(countryall).map(
