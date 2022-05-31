e2e1eeb076ca1fd3e028fbf26b05544aac60aebf
ShineD-188 <ShineD-188@domain.wgs.local>
2022-05-31 18:28:02 +0530

    create event hide page solve

--- src/Components/Webinar/Event/Add.js
+++ src/Components/Webinar/Event/Add.js
@@ -103,13 +103,13 @@ function Add(props) {
       if (Speakername[index].name.length == 0) {
         err = false;
         const copydataErr = SpeakerErr[index];
-        copydataErr.name = "name is requred ";
+        copydataErr.name = "Name is requred ";
         setSpeakerErr([...SpeakerErr]);
       }
       if (Speakername[index].email.length == 0) {
         err = false;
         const copydataErr = SpeakerErr[index];
-        copydataErr.email = "email is requred  ";
+        copydataErr.email = "Email is requred  ";
         setSpeakerErr([...SpeakerErr]);
       }
     }
