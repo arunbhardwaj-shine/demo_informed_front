cadc58576158a00d835213b7d53f11f26e82f879
varunshinedezign <varun.verma@shinedezign.email>
2022-05-31 18:13:10 +0530

    add remove the active class.

--- src/Components/Emails/CreateEmail.js
+++ src/Components/Emails/CreateEmail.js
@@ -738,6 +738,14 @@ const CreateEmail = (props) => {
   };
 
   const addFile = (e) => {
+    const addfile_btn = document.getElementById('add_file_btn');
+    if (document.querySelector('#add_file_btn .active') !== null) {
+        addfile_btn.classList.remove('active');
+    }else{
+       addfile_btn.classList.add('active');
+    }
+     document.querySelector('#add_hcp_btn').classList.remove('active');
+
     e.preventDefault();
     setActiveExcel("active");
     setActiveManual("");
@@ -799,6 +807,14 @@ const CreateEmail = (props) => {
   };
 
   const addHcp = (e) => {
+    const addhcp_btn = document.getElementById('add_hcp_btn');
+    if (document.querySelector('#add_hcp_btn .active') !== null) {
+        addhcp_btn.classList.remove('active');
+    }else{
+       addhcp_btn.classList.add('active');
+    }
+     document.querySelector('#add_file_btn').classList.remove('active');
+
     e.preventDefault();
     setActiveExcel("");
     setActiveManual("active");
@@ -1944,6 +1960,7 @@ const CreateEmail = (props) => {
                   <ul className="nav nav-tabs" role="tablist">
                     <li className="nav-item add_hcp">
                       <a
+                        id="add_hcp_btn"
                         onClick={(e) => addHcp(e)}
                         className="nav-link active btn-bordered"
                         data-bs-toggle="tab"
@@ -1954,6 +1971,7 @@ const CreateEmail = (props) => {
                     </li>
                     <li className="nav-item add-file">
                       <a
+                        id="add_file_btn"
                         onClick={(e) => addFile(e)}
                         className="nav-link btn-filled"
                         data-bs-toggle="tab"
