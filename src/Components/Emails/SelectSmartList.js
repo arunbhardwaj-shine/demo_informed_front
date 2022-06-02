7868b0c9c501f9e978319f3bf72d02cbe6e66643
varunshinedezign <varun.verma@shinedezign.email>
2022-06-02 12:17:01 +0530

    Implemented the refresh smart list functionality.

--- src/Components/Emails/SelectSmartList.js
+++ src/Components/Emails/SelectSmartList.js
@@ -30,6 +30,10 @@ const SelectSmartList = (props) => {
   const inputElement = useRef();
   axios.defaults.baseURL = process.env.REACT_APP_API_KEY;
   useEffect(() => {
+    getSmartListData();
+  }, []);
+
+  const getSmartListData = () => {
     const body = {
       user_id: 18207,
       search: "",
@@ -45,7 +49,8 @@ const SelectSmartList = (props) => {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }
+
   useEffect(() => {
     let listid = props.getEmailData
       ? smartListSelected.id
@@ -151,7 +156,9 @@ const SelectSmartList = (props) => {
   };
 
   const redirectToList = () => {
-    navigate("/CreateSmartList");
+    setpopupopeningstatus(false);
+    window.open("/CreateSmartList", "_blank")
+    // navigate("/CreateSmartList");
   };
 
   const openSmartListPopup = async(smart_list_id) => {
@@ -183,6 +190,10 @@ const SelectSmartList = (props) => {
     e.preventDefault();
     setShowLessInfo(!showLessInfo);
   };
+
+  const refreshSmartList = () => {
+    getSmartListData();
+  }
 
   return (
     <>
@@ -266,11 +277,18 @@ const SelectSmartList = (props) => {
                 >
                   Create new smart list
                 </button>
-                <div class="upload-btn" onClick={() => setpopupopeningstatus((getpopupopeningstatus) => !getpopupopeningstatus)}>
-									  <label for="input-file">Upload Exel File</label>
-									  <input id="input-file" type="file" />
-								</div>
+                <button class="upload-btn btn btn-primary btn-bordered" onClick={() => setpopupopeningstatus((getpopupopeningstatus) => !getpopupopeningstatus)}>
+                  Upload excel file
+								</button>
               </div>
+            </div>
+            <div className="col smartlist-refresh_div">
+              <button
+                className="btn btn-primary btn-bordered back"
+                onClick={refreshSmartList}
+              >
+                Refresh List
+              </button>
             </div>
             <div className="col smartlist-result-block">
               {SendListData.map((template) => {
