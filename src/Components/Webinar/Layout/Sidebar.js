d9605381dc4acdb2838ba6afed535b8aa27acad2
Bablu <ShineD-188@domain.wgs.local>
2022-06-13 12:24:52 +0530

    merging conflicts

--- src/Components/Webinar/Layout/Sidebar.js
+++ src/Components/Webinar/Layout/Sidebar.js
@@ -467,12 +467,12 @@ const Sidebar = () => {
                     </li>
                     <li
                       className={
-                        location.pathname === "/webinar/portal/registration"
+                        location.pathname === "/webinar/portal/RegistrationDetails"
                           ? "active"
                           : "side_li"
                       }
                     >
-                      <Link to="/webinar/portal/registration">
+                      <Link to="/webinar/portal/RegistrationDetails">
                         <img src={path_image + "webinar/event.svg"} />
                         <p>Registration Page</p>
                       </Link>
