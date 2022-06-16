6573e9700fe350ba8c10039d1862da1d8f6aba2c
Avtar Singh <avtar.shinedezign@gmail.com>
2022-06-16 16:14:06 +0530

    update reader page

--- src/Api/BaseApi.js
+++ src/Api/BaseApi.js
@@ -1,5 +1,6 @@
 import { create } from 'apisauce';
 const BaseApi = create({
+   //baseURL: 'http://lumen.docintel.com:8000/api/',
    baseURL: 'http://51.89.210.56:8000/api/',
    headers:{Authorization:localStorage.getItem("Token")},
 });
