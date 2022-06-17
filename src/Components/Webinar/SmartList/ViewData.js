875497b01a2c4828ddce24580fa4462ded59eac7
ShineD-117 <ShineD-117@domain.wgs.local>
2022-06-17 14:06:47 +0530

    changes to the image colour

--- src/Components/Webinar/SmartList/ViewData.js
+++ src/Components/Webinar/SmartList/ViewData.js
@@ -972,14 +972,35 @@ const ViewData = (props) => {
     return (
       <>
         <div class="right-sidebar">
+          <div className="loader" id="custom_loader">
+            <span className="loader-view"> </span>
+          </div>
+          <ToastContainer />
           <div class="top-header">
-            <div className="loader" id="custom_loader">
-              <span className="loader-view"> </span>
-            </div>
-            <ToastContainer />
             <div class="page-title">
-              <div class="header-btn-right back_btn">
-                <a class="btn btn-primary btn-filled light" href="#">
+              <div class="header-btn-left">
+                <button
+                  class="btn btn-primary btn-filled back"
+                  onClick={() => {
+                    navigate("/webinar/email/WebinarSmartList");
+                  }}
+                >
+                  <svg
+                    width="12"
+                    height="19"
+                    viewBox="0 0 12 19"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M8.31557 17.82C8.97165 18.476 10.0354 18.476 10.6915 17.82C11.3475 17.1639 11.3475 16.1002 10.6915 15.4441L4.7522 9.50484L10.6927 3.56431C11.3488 2.90823 11.3488 1.84451 10.6927 1.18843C10.0367 0.532347 8.97294 0.532347 8.31686 1.18843L1.2212 8.28409C1.21 8.29469 1.19891 8.30548 1.18794 8.31646C0.531858 8.97254 0.531858 10.0363 1.18794 10.6923L8.31557 17.82Z"
+                      fill="white"
+                    />
+                  </svg>
+                </button>
+                {/* <a class="btn btn-primary btn-filled light" href="#">
                   <img
                     src={path_image + "arrow-left.svg"}
                     alt=""
@@ -987,7 +1008,7 @@ const ViewData = (props) => {
                       navigate("/webinar/email/WebinarSmartList");
                     }}
                   />
-                </a>
+                </a> */}
               </div>
               <h2>Name of the list</h2>
             </div>
@@ -1044,7 +1065,7 @@ const ViewData = (props) => {
                   </button>
                 </form>
               </div>
-              <div class="filter-by nav-item dropdown">
+              {/* <div class="filter-by nav-item dropdown">
                 <button
                   class="btn btn-secondary dropdown-toggle"
                   type="button"
@@ -1127,7 +1148,7 @@ const ViewData = (props) => {
                     </a>
                   </li>
                 </ul>
-              </div>
+              </div> */}
               <div class="hcp-user-download">
                 <ReactHTMLTableToExcel
                   id="test-table-xls-button"
@@ -1163,24 +1184,30 @@ const ViewData = (props) => {
               </div>
             </div>
           </div>
-          {saveOpen ? (
-            <>
-              <button
-                className="btn btn-primary btn-filled"
-                onClick={closeClicked}
-              >
-                Close
-              </button>
 
-              <button
-                className="btn btn-primary btn-bordered"
-                onClick={saveEditClicked}
-              >
-                Save
-              </button>
-            </>
-          ) : null}
           <section class="search-hcp">
+            <div class="table-title">
+              <div class="selected-hcp-table-action">
+                {" "}
+                {saveOpen ? (
+                  <>
+                    <button
+                      className="btn btn-primary btn-filled"
+                      onClick={closeClicked}
+                    >
+                      Close
+                    </button>
+
+                    <button
+                      className="btn btn-primary btn-bordered"
+                      onClick={saveEditClicked}
+                    >
+                      Save
+                    </button>
+                  </>
+                ) : null}
+              </div>
+            </div>
             <div class="result-hcp-table">
               <div class="selected-hcp-list">
                 <table class="table" id="table-to-xls">
