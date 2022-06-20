41236a7d320955cc223db6852a4768864069d8c5
varunshinedezign <varun.verma@shinedezign.email>
2022-06-20 14:48:21 +0530

    Add the logout section in header area.

--- src/Components/CommonComponent/HeaderComponent/Header.js
+++ src/Components/CommonComponent/HeaderComponent/Header.js
@@ -44,7 +44,7 @@ const Header = () => {
 					  <li className="nav-item">
 						<a className="nav-link" >Analytics</a>
 					  </li>
-					  <li className="nav-item">
+					  <li className="nav-item active active-main">
 						<a className="nav-link" >Distrubute</a>
 					  </li>
 					  <div className="container">
@@ -55,6 +55,17 @@ const Header = () => {
         </div>
 				</ul>
 				</div>
+				<div className="user-login">
+					<ul>
+						<li className="nav-item dropdown">
+						  <a className="nav-link dropdown-toggle" href="javascript:;" role="button" data-bs-toggle="dropdown"><span>Hi,</span>Jacob Flindt</a>
+						  <ul className="dropdown-menu">
+								<li><a className="dropdown-item" href="#">Profile</a></li>
+								<li><a className="dropdown-item" href="#">Logout</a></li>
+						  </ul>
+						</li>
+					  </ul>
+					</div>
 			  </div>
 			</nav>
 
