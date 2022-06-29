f9db9d9ceadc1e94a05b4cd87e1a963b14f96352
Bablu <ShineD-188@domain.wgs.local>
2022-06-29 18:37:12 +0530

    Event Change

--- src/Api/BaseApi.js
+++ src/Api/BaseApi.js
@@ -1,50 +1,10 @@
 
 import { create } from 'apisauce';
-
-
-
-// const BaseApi = create({
-
-
-
-//    //baseURL: 'http://lumen.docintel.com:8000/api/',
-
-
-
-//    baseURL: 'http://51.89.210.56:8000/api/',
-
-
-
-//    headers:{Authorization:localStorage.getItem("Token")},
-
-
-
-// });
-
-
-
 const BaseApi = create({
-
-
-
-   //baseURL: 'http://lumen.docintel.com:8000/api/',
-
-
-
-   baseURL: 'http://192.168.0.46:8000/api/',
-
-   // baseURL: 'http://192.168.0.101:8000/api/',
-
-
-
-   headers:{Authorization:localStorage.getItem("Token")},
+   baseURL: 'https://webinarapi.shinedezign.pro/api',
+headers:{Authorization:localStorage.getItem("Token")},
 
 });
- const BaseUrlImage = 'http://192.168.0.46:8000'
-
-// const BaseUrlImage = 'http://192.168.0.101:8000'
-
-
-
+ const BaseUrlImage = 'https://webinarapi.shinedezign.pro'
 
 export { BaseApi,BaseUrlImage }
