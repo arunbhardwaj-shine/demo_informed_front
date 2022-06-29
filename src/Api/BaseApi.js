a3288d295d12fb6734cbcbb54364967f74af5896
Bablu <ShineD-188@domain.wgs.local>
2022-06-29 12:45:12 +0530

    Event Change

--- src/Api/BaseApi.js
+++ src/Api/BaseApi.js
@@ -15,11 +15,13 @@ const BaseApi = create({
 
    //baseURL: 'http://lumen.docintel.com:8000/api/',
 
-   baseURL: 'http://192.168.0.46:8000/api/',
+   // baseURL: 'http://192.168.0.46:8000/api/',
+   baseURL: 'http://192.168.0.101:8000/api/',
 
    headers:{Authorization:localStorage.getItem("Token")},
 });
-const BaseUrlImage = 'http://192.168.0.46:8000'
+// const BaseUrlImage = 'http://192.168.0.46:8000'
+const BaseUrlImage = 'http://192.168.0.101:8000/'
 
 
 export { BaseApi,BaseUrlImage };
