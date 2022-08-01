1c4ec2a5283e6b77109f25f003b7517d81de60f9
ShineD-159 <ShineD-159@domain.wgs.local>
2022-08-01 11:15:29 +0530

    Fixed design issue

--- src/Components/Webinar/Emails/WebinarVerifyHCP.js
+++ src/Components/Webinar/Emails/WebinarVerifyHCP.js
@@ -93,7 +93,7 @@ const WebinarVerifyHCP = () => {
                 </li>
               </ul>
             </div>
-          <div className="col-12 col-md-2">
+          <div className="col-12 col-md-3">
             <div className="header-btn">
               <button
                 // onClick={saveAsDraft}
