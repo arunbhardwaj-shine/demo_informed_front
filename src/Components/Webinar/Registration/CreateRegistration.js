a92e9f767776a5dd3d2e0e41f20ef1e1c0a9a62d
Avtar <avtar.singh@shinedezign.com>
2022-08-02 14:58:33 +0530

    update registation page

--- src/Components/Webinar/Registration/CreateRegistration.js
+++ src/Components/Webinar/Registration/CreateRegistration.js
@@ -8,54 +8,12 @@ import "../webinar.css";
 import { toast, ToastContainer } from "react-toastify";
 import AliceCarousel from "react-alice-carousel";
 import { loader } from "../../../loader";
+
 const CreateRegistration = (props) => {
-  let path_image = "/" + process.env.REACT_APP_ASSETS_PATH_INFORMED_DESIGN;
   let navigate = useNavigate();
   const [templateList, setTemplateList] = useState();
   const [activeIndex, setActiveIndex] = useState(0);
-  const syncActiveIndex = ({ item }) => setActiveIndex(item);
   const [TemplateIdActive, setTemplateIdActive] = useState();
-  const responsive = {
-    0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 5 },
-  };
-  const templateClicked = (template, e) => {
-    const div = document.querySelector("img.select_mm");
-    if (div) {
-      div.classList.remove("select_mm");
-    }
-    setTemplateIdActive(template.id);
-    e.target.classList.toggle("select_mm");
-  };
-  // const handleGetTemplateList = (id) => {
-  //   ExportApi.UserTemplateList(id).then((resp) => {
-  //     if (resp.ok) {
-  //       if (resp.data.code == 200) {
-  //         loader("hide")
-  //         console.log("resp.data.data",resp.data.data)
-  //         setTemplateList(resp.data.data);
-  //         // handleGetTemplate(resp.data.data[0].id);
-  //          setTemplateIdActive(resp.data.data[0].id);
-  //       } else {
-  //         loader("hide")
-  //       }
-  //     }
-  //   });
-  // };
-  // useEffect(() => {
-  //   loader("show")
-  //   window.addEventListener("EventId", () =>
-  //     // handleGetTemplateList(localStorage.getItem("EventIdHeader"))
-  //   );
-  //   // handleGetTemplateList(localStorage.getItem("EventIdHeader"));
-  //   if (localStorage.getItem("EventIdHeader")) {
-  //     console.log("done");
-  //   } else {
-  //     loader("hide");
-  //     // setMessage("Please create Event");
-  //   }
-  // }, []);
   const formik = useFormik({
     initialValues: {
       RegistrationPageTitle: "",
@@ -64,184 +22,133 @@ const CreateRegistration = (props) => {
       mode:''
     },
     validationSchema: Yup.object({
-      RegistrationPageTitle: Yup.string().required(
-        "Enter your registration page title"
-      ),
+      RegistrationPageTitle: Yup.string().required("Enter your registration page title"),
       body: Yup.string().required("Enter a body text"),
       mode: Yup.string().required("Please  select mode"),
       url: Yup.string()
         .matches(/^[a-zA-Z]+$/u, "Only alphabets are allowed")
-        .required("Enter url alias"),
+        .required("Enter url alias")
     }),
     enableReinitialize: true,
     onSubmit: (values) => {
-      loader("show")
-      let formData = new FormData();
-      formData.append("event_id",localStorage.getItem("EventIdHeader") );
-      formData.append("body", values.body);
-      formData.append("title", values.RegistrationPageTitle);
-      formData.append("url", values.url);
-       formData.append("mode", values.mode);
-      ExportApi.CreateRegistrationPage(formData).then((resp) => {
+        loader("show");
+        let formData = new FormData();
+        formData.append("event_id",localStorage.getItem("EventIdHeader") );
+        formData.append("body", values.body);
+        formData.append("title", values.RegistrationPageTitle);
+        formData.append("url", values.url);
+        formData.append("mode", values.mode);
+        ExportApi.CreateRegistrationPage(formData).then((resp) => {
         if (resp.ok) {
-          if (resp.data.code == 200) {
-            loader("hide")
-            toast.success(resp.data.message, {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-            });
+            if (resp.data.code === 200) {
+                loader("hide");
+                toast.success(resp.data.message, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined
+                });
             setTimeout(() => {
               navigate("/webinar/portal/Registrations")
             }, 2000);
           } else {
-            loader("hide")
+            loader("hide");
             toast.error(resp.data.message, {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
             });
           }
         }
       });
-    },
+    }
   });
   return (
     <>
-      <div className="loader" id="custom_loader">
-        <span className="loader-view"> </span>
-      </div>
-    <div class="right-sidebar col">
-    <div class="top-header">
-        <div class="page-title">
-          <h3>Create Registration Page </h3>
+        <div className="loader" id="custom_loader">
+            <span className="loader-view"> </span>
         </div>
-      </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-        <div className="webinar-modal-data create-registration">
-        <form onSubmit={formik.handleSubmit}>
-        <div className="modal-body-content">
-        <div className="form-inline row justify-content-between align-items-center">
-              <div className="form-group col-12 col-md-8">
-                <label> Title</label>
-                <Form.Control
-                  name="RegistrationPageTitle"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.Subject}
-                  type="text"
-                  placeholder="Title"
-                />
-                <div className="error">
-                {formik.touched.RegistrationPageTitle &&
-                formik.errors.RegistrationPageTitle ? (
-                  <div style={{ color: "red" }}>
-                    {formik.errors.RegistrationPageTitle}
-                  </div>
-                ) : null}
+        <div class="right-sidebar col">
+            <div class="top-header">
+                <div class="page-title">
+                    <h3>Create Registration Page </h3>
+                </div>
             </div>
-            </div>
-            </div>
-            <div className="form-inline row justify-content-between align-items-center">
-              <div className="form-group col-12 col-md-8">
-                <label>Mode </label>
-                <div className="form-inline-option">
-                            <div class="form-check">
-                              <Form.Label>Onsite</Form.Label>
-                              <Form.Control
-                                 name="mode"
-                                 type="radio"
-                                 onChange={formik.handleChange}
-                                 onBlur={formik.handleBlur}
-                                 value="onsite"
-                                 />
-                            </div>
-                            <div class="form-check">
-                              <Form.Label> Virtual</Form.Label>
-                              <Form.Control
-                               name="mode"
-                                type="radio"
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={"virtual"}
-                              />
-                            </div>
-                              {formik.touched.mode &&
-                              formik.errors.mode? (
-                                <div style={{ color: "red" }}>
-                                  {formik.errors.mode}
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+            <div className="webinar-modal-data create-registration">
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="modal-body-content">
+                        <div className="form-inline row justify-content-between align-items-center">
+                            <div className="form-group col-12 col-md-8">
+                                <label> Title</label>
+                                <Form.Control 
+                                    name="RegistrationPageTitle" 
+                                    onChange={formik.handleChange} 
+                                    onBlur={formik.handleBlur} 
+                                    value={formik.values.Subject} 
+                                    type="text" 
+                                    placeholder="Title" 
+                                />
+                                <div className="error">
+                                    {formik.touched.RegistrationPageTitle &&
+                                    formik.errors.RegistrationPageTitle ? (
+                                        <div style={{ color: "red" }}>
+                                            {formik.errors.RegistrationPageTitle}
+                                        </div>
+                                    ) : null}
                                 </div>
-                              ) : null}
-                            {/* <div class="form-check">
-                              <Form.Label> B</Form.Label>
-                              <Form.Control
-                                type="radio"
-                              />
-                            </div> */}
-                    </div>
-                </div>
-                </div>
-          {/* <section className="select-mail-template">
-                <div className="row select-mail-template-slider">
-                <AliceCarousel
-                  mouseTracking
-                  disableDotsControls
-                  activeIndex={activeIndex}
-                  responsive={responsive}
-                  onSlideChanged={syncActiveIndex}
-                >
-                  {templateList ? (
-                    templateList?.map((val, i) => (
-                      <div
-                        key={i}
-                        className="item"
-                         onClick={(e) => templateClicked(val, e)}
-                      >
-                        <div class="item-list">
-                          <div class="item-top-schedule">
-                            <img  src={path_image + "webinar/mail-schedule.png"} alt="" />
                             </div>
-                          <img
-                            value={val.id}
-                            src={path_image + "content_added1.png"}
-                            alt=""
-                            className={
-                              typeof TemplateIdActive !== "undefined" &&
-                              TemplateIdActive == val.id
-                                ? "select_mm"
-                                : ""
-                            }
-                          />
                         </div>
-                        <p>{val.name}</p>
-                      </div>
-                    ))
-                  ) : (
-                    <h2>{null}</h2>
-                  )}
-                </AliceCarousel>
-              </div>
-
-            
-          </section> */}
+                        <div className="form-inline row justify-content-between align-items-center">
+                            <div className="form-group col-12 col-md-8">
+                                <label>Mode </label>
+                                <div className="form-inline-option">
+                                    <div class="form-check">
+                                        <Form.Label>Onsite</Form.Label>
+                                        <Form.Control
+                                           name="mode"
+                                           type="radio"
+                                           onChange={formik.handleChange}
+                                           onBlur={formik.handleBlur}
+                                           value="onsite"
+                                        />
+                                    </div>
+                                    <div class="form-check">
+                                        <Form.Label> Virtual</Form.Label>
+                                        <Form.Control
+                                         name="mode"
+                                          type="radio"
+                                          onChange={formik.handleChange}
+                                          onBlur={formik.handleBlur}
+                                          value={"virtual"}
+                                        />
+                                    </div>
+                                    {formik.touched.mode &&
+                                    formik.errors.mode? (
+                                      <div style={{ color: "red" }}>
+                                        {formik.errors.mode}
+                                      </div>
+                                    ) : null}
+                                </div>
+                            </div>
+                        </div>
            <div className="form-inline row justify-content-between align-items-center">
               <div className="form-group col-12 col-md-8">
                 <label>Url Alias </label>
