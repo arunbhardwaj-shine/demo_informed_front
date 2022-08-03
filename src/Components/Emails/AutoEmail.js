3e9253af7b9708991803ee5fba7ad0d955b4a613
ShineD-117 <ShineD-117@domain.wgs.local>
2022-08-03 10:40:35 +0530

    added auto email to the sidebar

---
+++ src/Components/Emails/AutoEmail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AutoEmail = () => {
+  return (
+    <>
+      <div className="right-sidebar">
+        <h1>hi theree</h1>
+      </div>
+    </>
+  );
+};
+export default AutoEmail;
