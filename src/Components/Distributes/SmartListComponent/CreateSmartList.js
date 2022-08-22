cf8bb1b876070513c05d78de8d1df4453c599cf6
varunshinedezign <varun.verma@shinedezign.email>
2022-08-22 18:06:18 +0530

    Update user ids to download the excel sheet for sample.

--- src/Components/Distributes/SmartListComponent/CreateSmartList.js
+++ src/Components/Distributes/SmartListComponent/CreateSmartList.js
@@ -246,7 +246,7 @@ const CreateSmartList = () => {
   const downloadFile = () => {
     let user_id = localStorage.getItem("user_id");
     let link = document.createElement("a");
-    if(user_id == "wW0geGtDPvig5gF+6KbJrg==" || user_id == "qDgwPdToP05Kgzc+g2VjIQ==" ||  user_id == "z2TunmZQf3QwCsICFTLGGQ==" || user_id == "UbCJcnLM9fe+HsRMgX8c1A=="){
+    if(user_id == "wW0geGtDPvig5gF 6KbJrg==" || user_id == "qDgwPdToP05Kgzc g2VjIQ==" ||  user_id == "z2TunmZQf3QwCsICFTLGGQ==" || user_id == "UbCJcnLM9fe HsRMgX8c1A=="){
       link.href = "https://informed.pro/sample_st.xls";
     }else{
       link.href = "https://informed.pro/sample.xls";
