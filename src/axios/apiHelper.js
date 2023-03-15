5a22308faf18076253c5ca3aad084c3488f9df3b
Anuj sharma <anuj.sharma@shinedezign.com>
2023-03-15 10:34:26 +0530

    Resolve conflicts

--- src/axios/apiHelper.js
+++ src/axios/apiHelper.js
@@ -6,8 +6,8 @@ export const postData = (end_point, data) => {
 export const getData = (end_point) => {
   return axios.get(end_point);
 };
-export const postFormData = (end_point,data,config) => {
-  return axios.post(end_point,data,config);
+export const postFormData = (end_point, data, config) => {
+  return axios.post(end_point, data, config);
 };
 export const deleteData = (end_point, user_id) => {
   return axios.delete(end_point + "/" + user_id);
