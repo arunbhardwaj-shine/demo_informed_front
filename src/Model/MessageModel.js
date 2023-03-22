84e2f38f1e5a9d9a027154c465cb5efbedd108f5
varunshine <ShineD-97@domain.wgs.local>
2023-03-22 15:48:33 +0530

    Implemented the functionality to optimize the article for ebook.

---
+++ src/Model/MessageModel.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Dropdown, DropdownButton, Modal, Form } from "react-bootstrap";
+
+const MessageModel = ({
+  show,
+  onClose,
+  heading,
+  data,
+  footerButton,
+  handleSubmit,
+  handleQR,
+}) => {
+  const handleClose = () => {
+    onClose(false);
+  };
+
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        className="send-confirm"
+        id="message_modal"
+      >
+        <Modal.Header>
+          <h5 className="modal-title" id="staticBackdropLabel">
+            {heading}
+          </h5>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="modal"
+            onClick={handleClose}
+          ></button>
+        </Modal.Header>
+        <Modal.Body>
+          <h4 className="message"
+          onClick={() => {handleClose();}}>
+            {data}
+          </h4>
+        </Modal.Body>
+        <div className="modal-footer">
+        {
+          footerButton !== ""
+          ?
+          <button
+            type="button"
+            className="btn btn-primary save btn-filled"
+            onClick={() => {
+              handleSubmit();
+
+              handleClose();
+            }}
+          >
+            {footerButton}
+          </button>
+          :null
+        }
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default MessageModel;
