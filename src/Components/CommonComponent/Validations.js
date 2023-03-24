f093a5c25a96fab8e675f4669173fd6412f6754b
Anuj sharma <anuj.sharma@shinedezign.com>
2023-03-24 16:32:17 +0530

    changes in validations

--- src/Components/CommonComponent/Validations.js
+++ src/Components/CommonComponent/Validations.js
@@ -19,8 +19,8 @@ export const createContent = (data, fileCheck) => {
   if (!data?.limit) {
     error.limit = "Limit is required";
   } else if (data?.limit) {
-    if (data?.limit < 1) {
-      error.limit = "Limit must be greater than 0";
+    if (data?.limit < 0) {
+      error.limit = "Limit can't be negetive";
     }
   }
   if (!data?.docintelFormat) {
--- src/Components/Validations/LibraryValidation/LibraryEditValidation.js
+++ src/Components/Validations/LibraryValidation/LibraryEditValidation.js
@@ -8,8 +8,8 @@ export const LibraryEditValidation = (data) => {
   if (!data?.limit) {
     error.limit = "Limit is required";
   } else if (data?.limit) {
-    if (data?.limit < 1) {
-      error.limit = "Limit must be greater than 0";
+    if (data?.limit < 0) {
+      error.limit = "Limit can't be negetive";
     }
   }
 
