97319736d3fcd842cdb3a984f752ccb864715b4e
ShineD-161 (Arun) <ShineD-161@domain.wgs.local>
2023-03-31 16:06:15 +0530

    change to chapter select

--- src/Components/Library/CreateChange/PreviewContent.js
+++ src/Components/Library/CreateChange/PreviewContent.js
@@ -86,6 +86,10 @@ const PreviewContent = () => {
         setNewTemplateClicked(true);
       }
       loader("hide");
+
+      const div_img = document.querySelector(".alice-carousel__wrapper img");
+      div_img.click()
+
     } catch (err) {
       loader("hide");
     }
@@ -180,6 +184,7 @@ const PreviewContent = () => {
         },
       });
       getArticleData();
+     
     } catch (err) {
       loader("hide");
     }
@@ -225,6 +230,7 @@ const PreviewContent = () => {
           });
         }
         setPdfData(pdfData);
+ 
       } else {
         setPublishStatus(true);
         navigate("/content-detail", {
