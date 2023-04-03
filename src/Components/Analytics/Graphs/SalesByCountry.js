37da156a738b1b2ce056d483cef43a45e53c0af5
susheel0339 <susheel.shinedezign@gmail.com>
2023-04-03 15:24:42 +0530

    Analytics page redesign

--- src/Components/Analytics/Graphs/SalesByCountry.js
+++ src/Components/Analytics/Graphs/SalesByCountry.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 import Highcharts from "highcharts";
 import { loader } from "../../../loader";
 
@@ -10,6 +10,7 @@ import exportData from "highcharts/modules/export-data";
 import Select from "react-select";
 
 import HighchartsReact from "highcharts-react-official";
+import { Link } from "react-router-dom";
 
 exporting(Highcharts);
 exportData(Highcharts);
@@ -175,42 +176,53 @@ const years=useRef(null);
         {isDataFound ? (
           <div className="custom-container">
         <Row>
-  <div className="form-group d-flex align-items-center">
-    <label htmlFor="">Filter By</label>
-    <div className="d-flex">
-      <Select
-        options={All}
-        placeholder="All"
-        onChange={filterDataByDataType}
-        name="first"
-        className="dropdown-basic-button split-button-dropup mr-2"
-        isClearable
-      />
-      <Select
-        options={Years}
-        name="years"
-
-        placeholder="Filter By"
-        onChange={filterDataByYears}
-        className="dropdown-basic-button split-button-dropup"
-        isClearable
-      />
-    </div>
-  </div>
-</Row>
-
-            
-            <Row>
-              <div className="page-top-nav" >
-                <HighchartsReact
-                  highcharts={Highcharts}
-                  options={topClientOptions}
-                  ref={chart}
-                />
+          <div className="top-header">
+              <div className="page-title d-flex">
+                <Link className="btn btn-primary btn-bordered back-btn" to="/top-clients">
+                  <svg width="14" height="24" viewBox="0 0 14 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.159662 12.0019C0.159662 11.5718 0.323895 11.1417 0.65167 10.8138L10.9712 0.494292C11.6277 -0.16216 12.692 -0.16216 13.3482 0.494292C14.0044 1.15048 14.0044 2.21459 13.3482 2.8711L4.21687 12.0019L13.3479 21.1327C14.0041 21.7892 14.0041 22.8532 13.3479 23.5093C12.6917 24.1661 11.6274 24.1661 10.9709 23.5093L0.65135 13.19C0.323523 12.8619 0.159662 12.4319 0.159662 12.0019Z" fill="#97B6CF"/>
+                  </svg>
+                </Link>
+                <h2>Sales by country</h2>
               </div>
-            </Row>
+            </div>
+            <div className="create-change-content spc-content analytic-charts">
+            <div className="form_action">
+            <Form className="product-unit d-flex justify-content-between align-items-center">
+          <div className="form-group d-flex align-items-center">
+            <label htmlFor="">Filter By</label>
+              <Select
+                options={All}
+                placeholder="All"
+                onChange={filterDataByDataType}
+                name="first"
+                className="dropdown-basic-button split-button-dropup mr-2"
+                isClearable
+              />
+              <Select
+                options={Years}
+                name="years"
+
+                placeholder="Filter By"
+                onChange={filterDataByYears}
+                className="dropdown-basic-button split-button-dropup"
+                isClearable
+              />
           </div>
-        ) : null}
+          </Form>
+          </div>
+
+          <div className="high_charts">
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={topClientOptions}
+              ref={chart}
+            />
+          </div>
+      </div>
+    </Row>
+  </div>
+) : null}
       </Col>
     </>
   );
