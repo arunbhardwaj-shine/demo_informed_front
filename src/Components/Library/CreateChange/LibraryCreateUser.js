c9ad8924fe7357a8b911aaa007cfd8bc87ebcea6
Gaurav singh <gaurav.singh@shinedezign.com>
2023-04-04 15:06:54 +0530

    worked on library create changes file

--- src/Components/Library/CreateChange/LibraryCreateUser.js
+++ src/Components/Library/CreateChange/LibraryCreateUser.js
@@ -110,23 +110,28 @@ const LibraryCreateUser = () => {
       user_id: id,
     });
     let country = [];
-    hadData?.data?.data?.country.reduce((objEntries, key) => {
+    hadData?.data?.data?.country?.reduce((objEntries, key) => {
       country.push({
         label: key,
         value: key,
       });
     });
     let category = [];
-    hadData?.data?.data?.category.reduce((objEntries, key) => {
-      category.push({
-        label: key,
-        value: key,
+    if(hadData?.data?.data?.category?.length){
+      hadData?.data?.data?.category?.reduce((objEntries, key) => {
+        category.push({
+          label: key,
+          value: key,
+        });
       });
-    });
+      }
     let tags = [];
-    hadData?.data?.data?.tags.reduce((objEntries, key) => {
-      tags.push(key?.value);
-    });
+    if(hadData?.data?.data?.tags?.length){
+      hadData?.data?.data?.tags?.reduce((objEntries, key) => {
+        tags.push(key?.value);
+      });
+    }
+   
     setAllTags(tags)
     setUserDetail({
       user: hadData?.data?.data?.user,
