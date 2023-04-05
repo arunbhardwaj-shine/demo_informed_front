1a8245e3feb8894a8bc6573a8503a75a72006834
Gaurav singh <gaurav.singh@shinedezign.com>
2023-04-05 10:14:59 +0530

    worked on issues

--- src/Components/Library/CreateChange/LibraryCreateUser.js
+++ src/Components/Library/CreateChange/LibraryCreateUser.js
@@ -692,69 +692,7 @@ const LibraryCreateUser = () => {
                 </div>
               </div>
             </div>
-            {userDetail?.user?.[0]?.flag == 1 &&
-            userDetail?.user?.[0]?.group_id == 3 ? (
-              <div className="col-12 col-md-6 d-flex justify-content-end align-items-start right-change">
-                <div className="form-group justify-content-end">
-                  <label htmlFor="">HCP Type</label>
-                  <div className="input-group w-100">
-                    <div className="input-group-prepend">
-                      <button
-                        className="btn btn-filled btn-primary"
-                        type="button"
-                        id="tags-add"
-                        data-bs-toggle="modal"
-                        data-bs-target="#tagsModal"
-                        onClick={(e) =>
-                          topicButtonClicked(userDetail?.user[0]?.group_id)
-                        }
-                      >
-                        Add HCP +
-                      </button>
-                    </div>
-                    <div className="tags_added">
-                      <div className="select-tags">
-                        {/* {data?.tags?.length
-                        ? JSON.parse(data.tags)?.map((data) => {
-                            return <div>{data}</div>;
-                          })
-                        : ""} */}
-                      </div>
-                      <ul>
-                        <li className="list1">
-                          Excessive bleedings{" "}
-                          <img
-                            src="componentAssets/images/filter-close.svg"
-                            alt="Close-filter"
-                          />
-                        </li>
-                        <li className="list1">
-                          New tag 3{" "}
-                          <img
-                            src="componentAssets/images/filter-close.svg"
-                            alt="Close-filter"
-                          />
-                        </li>
-                        <li className="list1">
-                          New tag 6{" "}
-                          <img
-                            src="componentAssets/images/filter-close.svg"
-                            alt="Close-filter"
-                          />
-                        </li>
-                        <li className="list1">
-                          global{" "}
-                          <img
-                            src="componentAssets/images/filter-close.svg"
-                            alt="Close-filter"
-                          />
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ) : null}
+          
           </div>
         </div>
       </div>
