c94197ed98544b37b2b067e31dee9a88afbd46c3
Gagandeep shine <ShineD-117@domain.wgs.local>
2023-04-07 10:29:00 +0530

    work on changes

--- src/Components/Readers/AddReader/AddReader.js
+++ src/Components/Readers/AddReader/AddReader.js
@@ -305,7 +305,7 @@ const ReaderAdd = () => {
           email: userInputs?.email,
           alternativeEmail: userInputs?.alternativeEmail,
 
-          primary_phone: `${userInputs?.countryCode}-${userInputs?.primary_phone}`,
+          primary_phone: `${userInputs?.countryCode?.label}/${userInputs?.primary_phone}`,
 
           alternativePhone: userInputs?.alternativePhone,
           country: userInputs?.country,
