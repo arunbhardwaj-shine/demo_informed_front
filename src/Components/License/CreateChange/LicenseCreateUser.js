70987df5d51f5c6536d5a79ff4bc7bd51497aa61
varunshine <ShineD-97@domain.wgs.local>
2023-04-11 18:23:41 +0530

    Fixed remove ebook issue

--- src/Components/License/CreateChange/LicenseCreateUser.js
+++ src/Components/License/CreateChange/LicenseCreateUser.js
@@ -296,8 +296,9 @@ const LicenseCreateUser = () => {
   const deleteRecord = (i) => {
     const list = chapter;
     list.splice(i, 1);
-
+    ebookFile.splice(i, 1);
     setChapter(list);
+    setEbookFile(ebookFile);
     setCounterFlag(counterFlag + 1);
   };
 
