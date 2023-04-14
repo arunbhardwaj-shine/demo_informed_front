d24270b3b46b501a5d39fc108ed2d81dfecf9682
vikasdogra345 <120544805+vikasdogra345@users.noreply.github.com>
2023-04-14 15:27:25 +0530

    fixed analytics issue.

--- src/Components/Analytics/OctaCountry.js
+++ src/Components/Analytics/OctaCountry.js
@@ -12,7 +12,9 @@ const OctaCountry = () => {
   // Line Chart
   const [selectedRegion, setSelectRegions] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState([]);
-  // const selectRegionValue = useRef("EU");
+  const [selectCountryByRegion, setSelectCountryByRegion] = useState([]);
+  const [selectRegionVal, setSelectRegionVal] = useState("");
+
   const [optionsRev, setOptionsRev] = useState({
     title: {
       text: 'Total HCPs'
@@ -26,6 +28,7 @@ const OctaCountry = () => {
       }
     },
     legend: {
+      enabled: true,
       align: 'center',
       verticalAlign: 'bottom',
       layout: 'horizontal',
@@ -51,10 +54,13 @@ const OctaCountry = () => {
       height: '50%'
     },
     title: {
-      text: 'Sales by Product Category'
+      text: ''
     },
     xAxis: {
       categories: [],
+    },
+    exporting: {
+      showTable: true
     },
     yAxis: {
       title: {
@@ -63,6 +69,7 @@ const OctaCountry = () => {
       showZero: true
     },
     legend: {
+      enabled: true,
       align: 'center',
       verticalAlign: 'bottom',
       layout: 'horizontal',
@@ -71,74 +78,102 @@ const OctaCountry = () => {
     },
     plotOptions: {
       series: {
-        // stacking: 'normal'
+        stacking: 'normal'
       }
     },
     series: []
   });
 
-  // const selectRegionValue = useRef("EU");
-  // const selectCountryValue = useRef("Austria");
-  // const getCountry = useRef(null);
+  const selectRegionValue = useRef("EU");
+  const selectCountryValue = useRef("Austria");
+  const getCountry = useRef(null);
 
 
-  
-    const getDataFromApi = async () => {
-      try {
-        const region = "EU";
-        const country = "Austria";
-        const response = await postData(ENDPOINT.STATEBYREGION, { region, country });
-        const data = response.data.data;
-        console.log(data);
 
-        // setSelectRegions(data.uniqueRegions);
-        // setSelectedCountry(Object.entries(data.countryRegionArray));
-        // for line chart
-        const lineSeries = [
-          {
-            data: JSON.parse(data.haematology_data_rev),
-          },
-        ];
-        const lineCategories = JSON.parse(data.haematology_month_re);
-        const newLineOptions = {
-          ...optionsRev,
-          xAxis: {
-            categories: lineCategories,
-          },
-          series: lineSeries,
-        };
-        setOptionsRev(newLineOptions);
+  const getDataFromApi = async () => {
+    try {
+      const region = selectRegionValue.current;
+      const country = selectCountryValue.current;
+      const payload = { region, country };
+      console.log("payload", payload);
+      const response = await postData(ENDPOINT.STATEBYREGION, { region, country });
+      const data = response.data.data;
 
-        // for bar chart
+      setSelectRegions(data.uniqueRegions);
+      setSelectedCountry(Object.entries(data.countryRegionArray));
+      // for line chart
+      const lineData = JSON.parse(data.haematology_data_rev);
+      console.log(lineData);
+      const lastValue = lineData[lineData.length - 1];
+      const lineSeries = [
+        {
+          name: country+'('+lastValue+')',
+          data: JSON.parse(data.haematology_data_rev),
+        },
+      ];
 
-        const barSeries = [
-          {
-            data: JSON.parse(data.haematology_data).reverse(),
+      const lineCategories = JSON.parse(data.haematology_month_re);
+      const newLineOptions = {
+        ...optionsRev,
+        xAxis: {
+          categories: lineCategories,
+        },
+        series: lineSeries,
+      };
+      setOptionsRev(newLineOptions);
 
-          },
-        ];
-        const barCategory = JSON.parse(data.haematology_t_month).reverse();
+      // for bar chart
+      const barData = JSON.parse(data.haematology_data);
+      const totalValue = barData.reduce((acc, curr) => acc + curr, 0);
+      console.log(totalValue);
+      const barSeries = [
+        {
+          name: country + '(' +totalValue+ ')',
+          data: JSON.parse(data.haematology_data).reverse(),
+        },
+      ];
+      const barCategory = JSON.parse(data.haematology_t_month).reverse();
 
-        const newBarOption = {
-          ...optionBar,
-          xAxis: {
-            categories: barCategory,
-          },
-          series: barSeries,
-        };
-        setOptionBar(newBarOption);
+      const newBarOption = {
+        ...optionBar,
+        xAxis: {
+          categories: barCategory,
+        },
+        series: barSeries,
+      };
+      setOptionBar(newBarOption);
 
 
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-      // console.log(getCountry.current);
-      getDataFromApi();
-         }, []);
+  useEffect(() => {
+    // console.log(getCountry.current);
+    getDataFromApi();
+  }, []);
 
+
+  const filterByRegion = (e) => {
+    const selectedRegion = e;
+    selectRegionValue.current = selectedRegion;
+    setSelectRegionVal(selectedRegion);
+
+    const filteredCountries = selectedCountry.filter((country) => country[1] === selectedRegion);
+    setSelectCountryByRegion(filteredCountries);
+
+    const firstCountry = filteredCountries[0][0];
+    setSelectedCountry(filteredCountries[0]);
+    selectCountryValue.current = firstCountry;
+
+    getDataFromApi();
+  }
+
+  const filterByCountry = (e) => {
+    selectCountryValue.current = e.value;
+    getDataFromApi();
+  };
 
   return (
     <Col className="right-sidebar">
@@ -166,7 +201,28 @@ const OctaCountry = () => {
         </div>
         <Row>
           <div className="create-change-content spc-content analytic-charts">
-            
+            <div className="form_action">
+              <Form className="product-unit d-flex justify-content-between align-items-center">
+                <div className="form-group ">
+                  <label htmlFor=""></label>
+                  <Select
+                    options={selectedRegion.map((region) => ({ value: region, label: region }))}
+                    onChange={(selectedOption) => filterByRegion(selectedOption.value)}
+                    className="dropdown-basic-button split-button-dropup"
+                    value={{ value: selectRegionValue.current, label: selectRegionValue.current }}
+                  />
+                  <Select
+                    options={selectedCountry
+                      .filter((country) => country[1] === selectRegionValue.current)
+                      .map(([country]) => ({ value: country, label: country }))}
+                    onChange={(option) => filterByCountry(option)}
+                    className="dropdown-basic-button split-button-dropup"
+                    value={{ value: selectCountryValue.current, label: selectCountryValue.current }} // Set the selected value for the second dropdown
+                  // isDisabled={selectCountryByRegion.length === 0}
+                  />
+                </div>
+              </Form>
+            </div>
             <div className="high_charts">
               <HighchartsReact highcharts={Highcharts} options={optionsRev} />
             </div>
