3143f046e33408284e3e35f3e9dd308dbec7e4bd
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 <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>
2023-04-18 14:37:58 +0530

    graph

--- src/axios/index.js
+++ src/axios/index.js
@@ -25,20 +25,20 @@ requestHelper.interceptors.response.use(
     return res;
   },
   (err) => {
-      switch(err?.response?.status){
+    switch (err?.response?.status) {
       case 400:
-      toast.error(err?.response.data.message)
-      break;
+        toast.error(err?.response.data.message)
+        break;
       case 500:
-      toast.warning(err?.response.data.message)
-      break;
+        toast.warning(err?.response.data.message)
+        break;
       default:
-      toast.error(err?.response.data.message)
-      break;
+        toast.error(err?.response.data.message)
+        break;
     }
     return Promise.reject(err);
   }
-  
+
 );
 
 export default requestHelper;
