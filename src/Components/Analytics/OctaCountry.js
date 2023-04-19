ae59d20453aab3da83290ccdbdeb2d65f62cfadc
varunshine <ShineD-97@domain.wgs.local>
2023-04-19 17:12:42 +0530

    Fixed the analytics pages issue.

--- src/Components/Analytics/OctaCountry.js
+++ src/Components/Analytics/OctaCountry.js
@@ -180,6 +180,11 @@ const OctaCountry = () => {
     <Col className="right-sidebar">
       <div className="custom-container">
         <Row>
+          <div className="top-header">
+            <div className="page-title d-flex">
+              <h2>State By Region </h2>
+            </div>
+          </div>
           <div className="create-change-content spc-content analytic-charts">
             <div className="form_action">
               <Form className="product-unit d-flex justify-content-between align-items-center">
