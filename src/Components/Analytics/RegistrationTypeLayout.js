3fff4b75cf8d5cd54d4ead996532289b62a307f6
vikasdogra345 <120544805+vikasdogra345@users.noreply.github.com>
2023-04-21 17:23:37 +0530

    Fixed other analytics issues.

--- src/Components/Analytics/RegistrationTypeLayout.js
+++ src/Components/Analytics/RegistrationTypeLayout.js
@@ -92,6 +92,14 @@ const RenderLayout = ({ data }) => {
       <div ref={downloadRef}>
         {typeof data !== "undefined" && Object.keys(data).length > 0 ? (
           data?.map((element, index) => {
+            const currentDate = new Date();
+            let status = element.status;
+            if (new Date(element.exp_datetime) < currentDate) {
+              status = "expired";
+            } else if (new Date(element.exp_datetime) > currentDate) {
+              status = "active";
+            }
+
             return (
               <div
                 key={element.pdf_id}
@@ -126,7 +134,7 @@ const RenderLayout = ({ data }) => {
                           <strong>Company:</strong> {element.company}
                         </h5>
                         <h5 className="status">
-                          <strong>Status:</strong> <span>{element.status}</span>
+                          <strong>Status:</strong> <span>{status}</span>
                         </h5>
                         <h5 className="author_by">
                           <strong>Url code:</strong> {element.code}
@@ -250,7 +258,7 @@ const RenderLayout = ({ data }) => {
                                 innerRadius: "63%",
                                 y: Math.round(
                                   (element.over_all_unique_readers * 100) /
-                                    element.over_all_opening_readers
+                                  element.over_all_opening_readers
                                 ),
                                 z: element.over_all_unique_readers,
                               },
@@ -265,7 +273,7 @@ const RenderLayout = ({ data }) => {
                                 innerRadius: "38%",
                                 y: Math.round(
                                   (element.over_all_rtr * 100) /
-                                    element.over_all_opening_readers
+                                  element.over_all_opening_readers
                                 ),
                                 z: element.over_all_rtr,
                               },
@@ -298,9 +306,9 @@ const RenderLayout = ({ data }) => {
                           <span>
                             {element.pdf_limit != 0
                               ? Math.round(
-                                  (element.over_all_opening_readers * 100) /
-                                    element.pdf_limit
-                                ) + " %"
+                                (element.over_all_opening_readers * 100) /
+                                element.pdf_limit
+                              ) + " %"
                               : 0}
                           </span>
                         </strong>
