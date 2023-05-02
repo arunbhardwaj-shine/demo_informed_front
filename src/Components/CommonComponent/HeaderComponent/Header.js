1b11d57ec52511d65c775af1ffd6e632eeebf6f7
varunshine <ShineD-97@domain.wgs.local>
2023-05-02 18:02:12 +0530

    Fixed redirection after login.

--- src/Components/CommonComponent/HeaderComponent/Header.js
+++ src/Components/CommonComponent/HeaderComponent/Header.js
@@ -88,12 +88,12 @@ const Header = () => {
       <header>
         <nav className="navbar navbar-expand-sm navbar-light">
           <div className="container-fluid">
-            <a
+            <Link
               className="navbar-brand"
-              href="https://informed.pro/Libraries/home"
+              to={"/library-content"}
             >
               <img src={path + "inforMed_Logo_Blue_1.svg"} alt="" />
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
