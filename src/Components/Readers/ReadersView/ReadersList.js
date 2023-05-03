7af5b54199486e0dc671423c68a6dc5e891e5b63
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 <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>
2023-05-03 09:59:54 +0530

    changes

--- src/Components/Readers/ReadersView/ReadersList.js
+++ src/Components/Readers/ReadersView/ReadersList.js
@@ -861,11 +861,11 @@ const NewReaders = () => {
       setChanges(res?.data?.data);
       setSiteNumber((prevSiteNumbers) => ({
         ...prevSiteNumbers,
-        all: res.data.data.siteNumber,
+        all: res?.data?.data?.siteNumber,
       }));
       setSiteName((prevSiteName) => ({
         ...prevSiteName,
-        all: res.data.data.siteName,
+        all: res?.data?.data?.siteName,
       }));
     }
     setApiCallStatus(true);
