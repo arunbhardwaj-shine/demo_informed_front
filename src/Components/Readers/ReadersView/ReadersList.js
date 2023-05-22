4d95aa16717e07e041e97ab6e3652a325fdf53d5
susheel0339 <susheel.shinedezign@gmail.com>
2023-05-22 16:24:51 +0530

    heading change

--- src/Components/Readers/ReadersView/ReadersList.js
+++ src/Components/Readers/ReadersView/ReadersList.js
@@ -1039,7 +1039,7 @@ const NewReaders = () => {
         <div className="custom-container">
           <Row>
             <div className="top-header reader_list sticky">
-              <div className="page-title"><h4>Total HCP | <span>{totalCount}</span></h4></div>
+              <div className="page-title"><h4>Total HCP | <span>{totalCountFlag ? totalCount : 0}</span></h4></div>
               <div className="top-right-action library_content_view">
                 <div className="search-bar">
                   <form className="d-flex" onSubmit={(e) => submitHandler(e)}>
@@ -1408,9 +1408,9 @@ const NewReaders = () => {
             ) : null}
 
             <div className="library-content-box-layuot readerlist d-flex">
-              <h4>
+              {/* <h4>
                 <span>Total HCP</span> | {totalCountFlag ? totalCount : 0}
-              </h4>
+              </h4> */}
               {readerDataList?.length || updateflag ? (
                 readerDataList.map((data, index) => {
                   return (
