8aa45267c5b214a5ad79492bef530e25b31e3b0a
susheel0339 <susheel.shinedezign@gmail.com>
2023-05-24 17:24:18 +0530

    css update

--- src/Components/Readers/ReadersList/ReadersListAdd.js
+++ src/Components/Readers/ReadersList/ReadersListAdd.js
@@ -1142,7 +1142,7 @@ const ReadersListAdd = () => {
                               </div>
                               <div className="col-12 col-md-6">
                                 <div className="form-group">
-                                  <label htmlFor="">Email *</label>
+                                  <label htmlFor="">Email <span>*</span></label>
                                   <input
                                     type="email"
                                     className="form-control"
