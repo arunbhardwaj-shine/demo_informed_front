f094f791390babf9d72fa03d44a482abe2c20eba
susheel0339 <susheel.shinedezign@gmail.com>
2023-05-24 17:56:21 +0530

    css update

--- src/Components/Emails/AutoEmail.js
+++ src/Components/Emails/AutoEmail.js
@@ -1264,7 +1264,7 @@ const AutoEmail = () => {
                               </div>
                               <div className="col-12 col-md-6">
                                 <div className="form-group">
-                                  <label htmlFor="">Email *</label>
+                                  <label htmlFor="">Email <span>*</span></label>
                                   <input
                                     type="email"
                                     className="form-control"
--- src/Components/License/CreateChange/EditConsentOptions.js
+++ src/Components/License/CreateChange/EditConsentOptions.js
@@ -1758,7 +1758,7 @@ const EditConsentOptions = (props) => {
                               </div>
                               <div className="col-12 col-md-6">
                                 <div className="form-group">
-                                  <label htmlFor="">Email *</label>
+                                  <label htmlFor="">Email <span>*</span></label>
                                   <input
                                     type="email"
                                     className="form-control"
