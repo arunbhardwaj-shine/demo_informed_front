a6243ac9f376b84ab2b84e27df308f9f49e9c542
gauravshinedezign <111723510+gauravshinedezign@users.noreply.github.com>
2023-06-19 17:37:10 +0530

    worked on issues

--- src/Components/Readers/AddReader/AddReader.js
+++ src/Components/Readers/AddReader/AddReader.js
@@ -115,6 +115,7 @@ const ReaderAdd = () => {
     sub_role: "",
     title: "",
     ibu: "",
+    hospitalData:{}
   });
   const [userDetail, setUserDetail] = useState({
     speciality: [],
@@ -152,11 +153,23 @@ const ReaderAdd = () => {
       const newArr = userDetail[newProduct?.label];
       loader("show");
       try {
+    if (commonHeader != "Add New Hospital") {
         await postData(`${ENDPOINT.READER_ADD_FEATURES}`, {
           label: newProduct?.label,
           value: newProduct?.value,
         });
 
+      }else{
+        setAddReaderInputs({...userInputs, hospitalData:{
+          label: newProduct?.value,
+          value: newProduct?.value
+        },
+        hospital:""
+      
+      })
+        loader("hide");
+        return 
+      }
         let checkIndex = newArr.findIndex(
           (el) => el.value == newProduct?.value
         );
@@ -389,8 +402,19 @@ const ReaderAdd = () => {
     if (e?.target?.files?.length < 1) {
       return;
     }
+    if(isSelectedName == "hospital"){
 
-    if (isSelectedName == "country") {
+      setAddReaderInputs({
+        ...userInputs,
+        hospitalData:{},
+        [isSelectedName ? isSelectedName : e?.target?.name]: isSelectedName
+          ? e?.target?.files
+            ? e?.target?.files
+            : e
+          : e?.target?.value,
+      });
+
+    }else if (isSelectedName == "country") {
       // if(!userDetail?.flag){
       let newSite = [],
         newSiteNumber = [];
@@ -522,7 +546,7 @@ const ReaderAdd = () => {
           alternativePhone: userInputs?.alternativePhone,
           country: userInputs?.country,
           province: userInputs?.province,
-          hospital: userInputs?.hospital,
+          hospital: userInputs?.hospital?userInputs?.hospital:userInputs?.hospitalData?.value,
           title: userInputs?.title,
           speciality: userInputs?.speciality,
           discipline: userInputs?.discipline,
@@ -1008,12 +1032,17 @@ const ReaderAdd = () => {
 
                     {groupId == 2 || (groupId == 3 && flag == 0) ? (
                       <>
+
                         <Form.Group className="form-group">
                           <Form.Label htmlFor="">Hospital</Form.Label>
                           <Select
-                            options={userDetail?.hospital}
+                            options={hospital}
                             placeholder="Select hospital"
                             className="dropdown-basic-button split-button-dropup"
+                            value={Object.keys(userInputs?.hospitalData)?.length?userInputs.hospitalData: userInputs?.hospital?{
+                              label:userInputs?.hospital,
+                              value:userInputs?.hospital
+                            }:""}
                             isClearable
                             onChange={(e) => handleChange(e?.value, "hospital")}
                           />
