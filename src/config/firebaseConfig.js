5d37b098c1940893330317c09d40829415d1be04
gauravshinedezign <111723510+gauravshinedezign@users.noreply.github.com>
2023-07-03 18:12:09 +0530

    worked on issues

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC2_IAe4G4Y8U9D-27X3atKhUGWs1M1RuM",
+    authDomain: "triggers-ba47e.firebaseapp.com",
+    projectId: "triggers-ba47e",
+    storageBucket: "triggers-ba47e.appspot.com",
+    messagingSenderId: "743778002151",
+    appId: "1:743778002151:web:1077ccea556b954a906e42",
+    measurementId: "G-6DCCEYH7P1"
+  };
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+// Export firestore database
+// It will be imported into your react app whenever it is needed
+export const db = getFirestore(app);
