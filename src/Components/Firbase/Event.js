e79e2c8703f2e1a2c39622dc236c9ff5f117fab7
gauravshinedezign <111723510+gauravshinedezign@users.noreply.github.com>
2023-07-04 16:40:50 +0530

    worked on issues

--- src/Components/Firbase/Event.js
+++ src/Components/Firbase/Event.js
@@ -13,14 +13,15 @@ import {db} from "../../config/firebaseConfig"
 const Event = () =>{
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
+    
     const [eventId,setEvent] = useState(0)
-
     const q = query(collection(db, "chat"), where("triggered", '!=', 0),where("event_id","==",eventId));
     const [data,setData] = useState(0)
     const [show,setShow] = useState(false)
     const [apiData,setApiData] = useState([])
     const [answerPop,setAnswerPopup] = useState(false)
     useEffect(()=>{
+        console.log("-=-=-=->>112",queryParams.get("evnt"))
         EventDataFun()
     },[])
     const EventDataFun = async() =>{
