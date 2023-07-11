031bc1bb4cdb506e29a30f0314417f105926c8d8
Gaurav singh <gauravaswal60@gmail.com>
2023-07-11 17:41:09 +0530

    worked on isssues

---
+++ src/Components/Firbase/QuestionTrigger.js
@@ -0,0 +1,18 @@
+
+const QuestionTrigger = () =>{
+
+    return (
+        <>
+        <div>
+         <h1>Question</h1>
+        </div>
+        <div>
+        <h1>Answerd</h1>
+        </div>
+        <div>
+        <h1>Question</h1>
+        </div>
+        </>
+    )
+}
+export default QuestionTrigger
