371ffad3737da49c81298c3b818dafeb85d4946f
susheel0339 <susheel.shinedezign@gmail.com>
2023-07-13 17:36:50 +0530

    changes in the Question trigger file

--- src/Components/Firbase/QuestionTrigger.js
+++ src/Components/Firbase/QuestionTrigger.js
@@ -86,47 +86,106 @@ const QuestionTrigger = () =>{
 
     return (
         <Container>
+            <div className="webinar-question-results webinar_tabs_show">
         <Row>
           <Col md={4}>
+            <div class="webinar-top-sec d-flex justify-content-between align-items-center">
+                    <div class="top-heading">
+                        <h4>Questions:</h4>
+                    </div>
+                    <div class="webinar-top-btn question">
+                        <span class="btn default-side-buttons">1</span>                                                             
+                    </div>
+            </div>
             {
                 data?.question.length?data.question.map(item =>{
-                    return (
-                        <div>
-                         <input type="text" />
-                            <div>
-                            <Button onClick={()=>submitFun(0,item?.id)}>Ignore</Button>
-                            <Button onClick={()=>submitFun(2,item?.id)}>Answer</Button>
+                                        return (
+                    <div className="reader_list">
+                        <div className="detail-box">  
+                            <p className="user_name">Lorem Ipsum</p>
+                            <div className="user-question">
+                                <p>Learn more about our activities and future events</p>
+                            </div>
+                            <div className="reader_list_footer d-flex justify-content-between align-items-center" >
+                                <div className="question-post-time">
+                                    <small>09:16 am</small>
+                                </div>
+                                <div className="reader_list_footer_btns">
+                                    <Button className="ignored" onClick={()=>submitFun(0,item?.id)}>Ignore</Button>
+                                    <Button className="answer" onClick={()=>submitFun(2,item?.id)}>Answer</Button>
+                                </div>
                             </div>
                         </div>
+                    </div>
+                        
                     )
-                }):<h1>No Records Found</h1>
+                }):<h6 className="no-found">No Records Found</h6>
             }
           </Col>
-          <Col md={4}>{
+          <Col md={4}>
+                <div class="webinar-top-sec d-flex justify-content-between align-center">
+                    <div class="top-heading">
+                        <h4>Answered:</h4>
+                    </div>
+                    <div class="webinar-top-btn answered">
+                        <span class="btn default-side-buttons">1</span>                                                             
+                    </div>
+            </div>
+            {
                 data?.answer.length?data?.answer.map(item =>{
                     return (
-                        <div>
-                         <input type="text" />
-                            <div>
-                            <Button onClick={()=>submitFun(1,item?.id)}>Undo</Button>
+                        <div className="reader_list">
+                            <div className="detail-box">  
+                                <p className="user_name">Lorem Ipsum</p>
+                                <div className="user-question">
+                                    <p>Learn more about our activities and future events</p>
+                                </div>
+                                <div className="reader_list_footer d-flex justify-content-between align-items-center answer-footer" >
+                                    <div className="question-post-time">
+                                        <small>09:16 am</small>
+                                    </div>
+                                    <div className="reader_list_footer_btns">
+                                        <Button onClick={()=>submitFun(1,item?.id)}>Undo</Button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                    </div>
                     )
-                }):<h1>No Records Found</h1>
+                }):<h6 className="no-found">No Records Found</h6>
             }</Col>
-          <Col md={4}>{
+          <Col md={4}>
+            <div class="webinar-top-sec d-flex justify-content-between align-center">
+                    <div class="top-heading">
+                        <h4>Ignored:</h4>
+                    </div>
+                    <div class="webinar-top-btn ignored">
+                        <span class="btn default-side-buttons">1</span>                                                             
+                    </div>
+            </div>
+            {
                 data?.ignre.length?data?.ignre.map(item =>{
                     return (
-                        <div>
-                         <input type="text" />
-                            <div>
-                            <Button onClick={()=>submitFun(1,item?.id)}>Undo</Button>
+                        <div className="reader_list">
+                            <div className="detail-box">  
+                                <p className="user_name">Lorem Ipsum</p>
+                                <div className="user-question">
+                                    <p>Learn more about our activities and future events</p>
+                                </div>
+                                <div className="reader_list_footer d-flex justify-content-between align-items-center ignore-footer" >
+                                    <div className="question-post-time">
+                                        <small>09:16 am</small>
+                                    </div>
+                                    <div className="reader_list_footer_btns">
+                                        <Button onClick={()=>submitFun(1,item?.id)}>Undo</Button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                    </div>
                     )
-                }):<h1>No Records Found</h1>
+                }):<h6 className="no-found">No Records Found</h6>
             }</Col>
         </Row>
+        </div>
       </Container>
     )
 }
