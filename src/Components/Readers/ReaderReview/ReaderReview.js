c81d0863e3b719648a1b41a6a0b64248d141f925
Gagandeep shine <ShineD-117@domain.wgs.local>
2023-07-14 10:01:41 +0530

    work on changes

--- src/Components/Readers/ReaderReview/ReaderReview.js
+++ src/Components/Readers/ReaderReview/ReaderReview.js
@@ -166,7 +166,8 @@ const ReaderReview = () => {
                               Primary phone{" "}
                             </th>
                             <td>
-                              {readerData?.primary_phone !== "-informed-"
+                              {readerData?.primary_phone !== "-informed-" &&
+                              readerData?.primary_phone !== "-informed-null"
                                 ? readerData?.primary_phone.replace(
                                     "-informed-",
                                     "-"
