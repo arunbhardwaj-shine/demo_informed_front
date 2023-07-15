6cdd705348fe614b0b247eec485ba4de7bfb6c5d
varunshine <ShineD-97@domain.wgs.local>
2023-07-15 10:51:51 +0530

    revert the changes.

--- src/Components/Library/CreateChange/RenderPdf.js
+++ src/Components/Library/CreateChange/RenderPdf.js
@@ -42,20 +42,6 @@ const RenderPdf = ({
   const [wordData, setWordData] = useState([]);
   const [modalMessage, setModalMessage] = useState('');
   const [modalBtn, setModalBtn] = useState('');
-  const [startX, setStartX] = useState(0);
-  const [startY, setStartY] = useState(0);
-  const [endX, setEndX] = useState(0);
-  const [endY, setEndY] = useState(0);
-  const [startXCordinate, setStartXCordinate] = useState(0);
-  const [startYCordinate, setStartYCordinate] = useState(0);
-  const [endXCordinate, setEndXCordinate] = useState(0);
-  const [endYCordinate, setEndYCordinate] = useState(0);
-  const [highlighted, setHighlighted] = useState(false);
-  const [inputUrl, setInputUrl] = useState("");
-  const [dragging, setDragging] = useState(false);
-  const [error, setError] = useState(false);
-  const [showAddLink, setShowAddLink] = useState(false);
-  const parentRef=useRef(null);
   // const pdfjsVersion = packageJson.dependencies['pdfjs-dist'];
   let total_pages = 1000;
   // let url = "https://docintel.s3-eu-west-1.amazonaws.com/ebook/arunp/1679390009620.pdf";
@@ -224,187 +210,6 @@ const optimizeSinglePagePdf = () => {
   }
 }
 
-
-
-useEffect(() => {
-  parentRef.current.addEventListener("mousedown", handleMouseDown);
-  parentRef.current.addEventListener("mousemove", handleMouseMove);
-  parentRef.current.addEventListener("mouseup", handleMouseUp);
-
-  return () => {
-    parentRef.current.removeEventListener("mousedown", handleMouseDown);
-    parentRef.current.removeEventListener("mousemove", handleMouseMove);
-    parentRef.current.removeEventListener("mouseup", handleMouseUp);
-  };
-}, [dragging, startX, startY, endX, endY]);
-
-const handleMouseDown = (e) => {
-  if (e.target.name === "url") return;
-  if (e.target.name === "addurl") return;
-  if(e.target.className === "viewer-text-layer"){
-    console.log(e.clientX,"Mouse down first event");
-	  setHighlighted(false)
-	  setShowAddLink(true);
-	  const viewerRect = parentRef.current.getBoundingClientRect();
-	  const textLayer = parentRef.current.querySelector(".viewer-text-layer");
-	  // console.log(textLayer,"textLayer");
-	  const pageHeight = textLayer.getBoundingClientRect().height;
-	   // console.log(pageHeight,"pageHeight");
-	  const scrollLayer = document.querySelector(".viewer-layout-main");
-	  const scrollTop = scrollLayer.scrollTop;
-
-	  // Calculate the coordinates relative to the viewer
-	  const x = e.clientX - viewerRect.left;
-	  const y = e.clientY - viewerRect.top + scrollTop;
-
-	  // Calculate the page number
-	  const newPage = Math.floor((y - textLayer.offsetTop) / pageHeight);
-	  setPage(newPage);
-	  setStartXCordinate(e.clientX);
-	  setStartYCordinate(y-(newPage * pageHeight));
-	  setEndXCordinate(x);
-	  setEndYCordinate(y-(newPage * pageHeight));
-	  // Calculate the coordinates relative to the text layer
-	  const xInPage = x - textLayer.offsetLeft + 26;
-	  const yInPage = y - textLayer.offsetTop-scrollTop + 26;
-	  // console.log("Box1",x ,y,scrollTop,xInPage,yInPage)
-	  setDragging(true);
-	  setStartX(xInPage);
-	  setStartY(yInPage);
-	  setEndX(xInPage);
-	  setEndY(yInPage);
-
-
-      let top_height =  document.getElementsByClassName("viewer-text-layer").offsetTop;
-      console.log(top_height,e.clientY);
-      let top_height_main =   e.clientY-top_height;
-      top_height_main =  top_height_main/3.8;
-      console.log(top_height_main,"top_height_main");
-
-  }else{
-	  console.log("AM not here");
-	  setShowAddLink(false);
-	  return;
-  }
-};
-
-const handleMouseMove = (e) => {
-  if (e.target.name === "url") return;
-  if (e.target.name === "addurl") return;
-  window.getSelection().removeAllRanges();
-  if (!dragging) return;
-
-  const viewerRect = parentRef.current.getBoundingClientRect();
-  const scrollLayer = document.querySelector(".viewer-layout-main");
-  const scrollTop = scrollLayer.scrollTop;
-  const textLayer = parentRef.current.querySelector(".viewer-text-layer");
-  const pageHeight = textLayer.getBoundingClientRect().height;
-
-  // Calculate the coordinates relative to the viewer
-  const x = e.clientX - viewerRect.left;
-  const y = e.clientY - viewerRect.top + scrollTop;
-  setEndXCordinate(x);
-  setEndYCordinate(y-(page * pageHeight));
-
-
-  // Calculate the coordinates relative to the text layer
-  const xInPage = x - textLayer.offsetLeft;
-  const yInPage = y - textLayer.offsetTop  - scrollTop;
-
-  setEndX(xInPage);
-  setEndY(yInPage);
-};
-
-const handleMouseUp = (e) => {
-  if (e.target.name === "url") return;
-  if (e.target.name === "addurl") return;
-  const textLayer = parentRef.current.querySelector(".viewer-text-layer");
-  const pageHeight = textLayer.getBoundingClientRect().height;
-//  console.log("Box2",startX ,startY,startXCordinate,startYCordinate)
-
-  window.getSelection().removeAllRanges();
-  // console.log(startXCordinate,startYCordinate,endXCordinate,endYCordinate)
-  setDragging(false);
-  if(showAddLink){
-	setHighlighted(true);
-  }
-};
-
-
-const handleAddUrl = (e) => {
-	e.preventDefault();
-	let embed_url = inputUrl.trim();
-	if(embed_url.length > 0 && isValidUrl(embed_url)){
-		setError(false);
-	}else{
-		setError(true);
-	}
-
-  let layout_container = document.querySelector(".sublink_right_block");
-  let winow_width = layout_container.getBoundingClientRect().width;
-  let page_layer = parentRef.current.querySelector(".viewer-text-layer");
-  let document_width   = page_layer.getBoundingClientRect().width;
-  let main_width = winow_width - document_width;
-  let left_porion = main_width/2;
-  // let left_porion = main_width;
-  let box =  parentRef.current.querySelector(".highlight_box");
-  let box_height = box.getBoundingClientRect().height;
-  let box_width  = box.getBoundingClientRect().width;
-  let new_width  = startXCordinate-left_porion-box_width;
-  let x_cord     = new_width/3.9;
-
-
-  // var element1 = document.getElementsByClassName('sublink_right_block');
-  // var element2 = document.getElementsByClassName('viewer-text-layer');
-  var elements1 = document.getElementsByClassName('sublink_right_block'); // First elements
-  var elements2 = document.getElementsByClassName('viewer-text-layer'); // Second element
-
-  var element1 = elements1[0]; // First element
-  var element2 = elements2[0]; // Second element
-
-  console.log(element1.offsetTop,element2.offsetTop,element1.offsetHeight);
-  var margin = element2.offsetTop - element1.offsetTop - element1.offsetHeight;
-  console.log(margin,"margin");
-
-  // console.log(startX,left_porion,"Left Portion");
-  // console.log(box_width,box_height,"box_height");
-  console.log(x_cord,"x coordinates");
-  console.log(box_width/3.7,"box_accurate_width");
-  console.log(box_height/3.7,"box_accurate_height");
-  console.log(endY,"endY");
-  console.log(window.devicePixelRatio);
-  // console.log("startXCordinate",startXCordinate);
-	// console.log("HandleAddUrlFunction");
-	// console.log(startXCordinate,startYCordinate,endXCordinate,endYCordinate);
-  //
-	// console.log("Add link to pdf");
-  //
-	// console.log(Math.abs(endXCordinate - startXCordinate),Math.abs(endYCordinate - startYCordinate));
-  //
-	// console.log("Add box link to pdf");
-  //
-	// console.log(startXCordinate-7,startYCordinate-53,Math.abs(endXCordinate - startXCordinate),Math.abs(endYCordinate - startYCordinate));
-
-	  // setDragging(false);
-	  // setHighlighted(false);
-};
-
-const closePopup = () => {
-	setDragging(false);
-	setHighlighted(false);
-}
-
-const isValidUrl = (urlString) => {
-	 var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-  return !!urlPattern.test(urlString);
-}
-
-
     return (
       <div className="sublink_right_block">
       {
@@ -428,61 +233,16 @@ const isValidUrl = (urlString) => {
                     }
 
                         <div style={{ height: '750px' }} id="pdf_view_box">
-                          <div onScroll={scrollEve} className={previewArticle?"scroll_pdf":"scroll_pdf"} ref={parentRef}>
+                          <div onScroll={scrollEve} className={previewArticle?"scroll_pdf":"scroll_pdf"}>
                           <Viewer
                             onPageChange={handlePageChange}
                             onDocumentLoad={handleDocumentLoad}
                             renderMode = "canvas"
                             fileUrl={url}
                           />
-
-                          <div
-                            className = "highlight_box"
-                            style={{
-                              position: "absolute",
-                              border: "2px dashed rgb(204, 204, 204)",
-                              backgroundColor: "rgba(255, 0, 0, 0)",
-                              display: highlighted || dragging ? "block" : "none",
-                              pointerEvents: "none",
-                              left: `${Math.min(startX, endX)}px`,
-                              top: `${Math.min(startY, endY)}px`,
-                              width: `${Math.abs(startX - endX)}px`,
-                              height: `${Math.abs(startY - endY)}px`,
-                            }}
-                          />
                           </div>
-
-
-                          {highlighted && (
-                            <div
-                              className="link_popup"
-                            >
-                              <form action="#" id="addLinkForm">
-                                <button type="button" className="close" id="closeLinkPopup" onClick={() => closePopup()}>
-                                  <span aria-hidden="true">×</span>
-                                </button>
-                                <label for="targetURL">Add Link:</label>
-                                <input placeholder="https://example.com"
-                                  id="targetURL"
-                                  className="form-control input-xs"
-                                  type="text"
-                                  onChange={(e)=>setInputUrl(e.target.value)}
-                                 />
-
-								 {
-									 error ?
-									 <p className="err_class">
-										Please enter a valid link
-									 </p> : null
-								 }
-
-
-                                <input type="submit" value="Add Link" id="addLinkToPdfButton" onClick={(e) => handleAddUrl(e)} />
-                              </form>
-                            </div>
-                          )}
-
                         </div>
+
                   </>
                 )
               }
