21b85a4ad30ef3a52f66d85506e3c78212a6151d
hackmack4772 <aamir.saleem@shinedezign.com>
2023-07-17 14:00:38 +0530

    certificate download

--- src/Components/R&D/RDAnalytics.js
+++ src/Components/R&D/RDAnalytics.js
@@ -731,7 +731,7 @@ const RDAnalytics = () => {
         const url = window.URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
-        link.download = 'certificate.pdf';
+        link.download = 'certificate_.pdf';
         link.click();
         window.URL.revokeObjectURL(url);
       })
