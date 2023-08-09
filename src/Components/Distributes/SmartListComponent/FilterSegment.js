dce6b88d5c0a0f1d3cbb38bde83d59f51b133a64
varunshine <varun.verma@shinedezign.com>
2023-08-09 16:29:40 +0530

    display ibu in case of All readers cis.

--- src/Components/Distributes/SmartListComponent/FilterSegment.js
+++ src/Components/Distributes/SmartListComponent/FilterSegment.js
@@ -2489,7 +2489,7 @@ const FilterSegment = (props) => {
                               )}
 
                               {"ibu" in filters &&
-                                Object.keys(filters.ibu).length > 0 && selectedreaderselection == "CIS Reader" && (
+                                Object.keys(filters.ibu).length > 0 && (selectedreaderselection == "CIS Reader" || selectedreaderselection =="All User") && (
                                   <>
                                     <div className="col block-smart-name">
                                       <h6>IBU</h6>
