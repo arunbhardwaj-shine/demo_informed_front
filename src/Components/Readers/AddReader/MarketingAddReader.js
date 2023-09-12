511644e5512e9734c60d27b4b2d49afabf626030
Gagandeep shine <ShineD-117@domain.wgs.local>
2023-09-12 12:25:48 +0530

    work on add crm

--- src/Components/Readers/AddReader/MarketingAddReader.js
+++ src/Components/Readers/AddReader/MarketingAddReader.js
@@ -1,4 +1,57 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
-const MarketingAddReader = () => {};
+const MarketingAddReader = () => {
+  const Main = () => {
+    return <></>;
+  };
+
+  return (
+    <>
+      <Col className="right-sidebar custom-change">
+        <div className="custom-container">
+          <Row>
+            <div className="page-top-nav sticky">
+              <Row className="justify-content-end align-items-center">
+                <Col md={1}>
+                  <div className="header-btn-left"></div>
+                </Col>
+                <Col md={9}>
+                  <ul className="tabnav-link">
+                    <li className="active active-main">
+                      <a href="">Create Your Content</a>
+                    </li>
+
+                    <li className="">
+                      <a href="">Approve Your Content &amp; Publish</a>
+                    </li>
+                  </ul>
+                </Col>
+                <Col md={2}>
+                  <div className="header-btn">
+                    <Link
+                      className="btn btn-primary btn-bordered move-draft"
+                      to="/library-create"
+                    >
+                      Cancel
+                    </Link>
+
+                    <button
+                      className="btn btn-primary btn-filled next "
+                      //   onClick={nextButtonClicked}
+                    >
+                      Next
+                    </button>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+            {Main()}
+          </Row>
+        </div>
+      </Col>
+    </>
+  );
+};
 export default MarketingAddReader;
