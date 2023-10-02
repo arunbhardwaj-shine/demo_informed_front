dbe8b9b8d11e776a7e3835aa02db6a2818a1969b
gauravshinedezign <111723510+gauravshinedezign@users.noreply.github.com>
2023-10-02 14:32:08 +0530

    worked on issues of elsevier

--- src/Components/Readers/ReadersView/ReadersList.js
+++ src/Components/Readers/ReadersView/ReadersList.js
@@ -61,19 +61,20 @@ const NewReaders = () => {
     },
   ];
   const [totalCount, setCount] = useState(0);
-  const [appliedFilter, setAppliedFilter] = useState({
+  let staticFilter = {
     status: ["Registered"],
     "contact Type": ["HCP"],
-  });
+  }
+  let exceptionCase = {
+    "contact Type": ["HCP"],
+  }
+  if(localStorage.getItem("user_id") == "b3APser7L8OELDIG8ee2HQ=="){
+    staticFilter = {}
+  }
+  const [appliedFilter, setAppliedFilter] = useState(localStorage.getItem("user_id") == "b3APser7L8OELDIG8ee2HQ=="?exceptionCase:staticFilter);
   // const [appliedFilter, setAppliedFilter] = useState();
-  const [filterObject, setFilterObject] = useState({
-    status: ["Registered"],
-    "contact Type": ["HCP"],
-  });
-  const [apifilterObject, setApifilterObject] = useState({
-    status: ["Registered"],
-    "contact Type": ["HCP"],
-  });
+  const [filterObject, setFilterObject] = useState(staticFilter);
+  const [apifilterObject, setApifilterObject] = useState(staticFilter);
 
   const [filterApplyflag, setFilterApplyflag] = useState(1);
   const [pageAll, setPageAll] = useState(false);
@@ -101,12 +102,24 @@ const NewReaders = () => {
 
   const [forceRender, setForceRender] = useState(false);
   const [updateflag, setUpdateFlag] = useState(0);
-  const [types, setTypes] = useState([
+  // const [types, setTypes] = useState([
+  //   { value: "0", label: "HCP" },
+  //   { value: "1", label: "Staff User" },
+  //   { value: "3", label: "Test User" },
+  //   { value: "4", label: "Competitor" },
+  // ]);
+  let types = [
     { value: "0", label: "HCP" },
     { value: "1", label: "Staff User" },
     { value: "3", label: "Test User" },
     { value: "4", label: "Competitor" },
-  ]);
+  ]
+  if(localStorage.getItem("user_id") == "b3APser7L8OELDIG8ee2HQ=="){
+     types.push({
+      value: "5", label: "Pharma"
+     })
+  }
+  // 
   const [irtData, setIrtData] = useState([
     "All",
     "Site User-Blinded",
@@ -119,6 +132,7 @@ const NewReaders = () => {
     1: "Staff User",
     3: "Test User",
     4: "Competitor",
+    5: "Pharma"
   };
   const [changeCountry, setChangeCountry] = useState([]);
   const [changeUserType, setChangeUserType] = useState([]);
@@ -159,11 +173,17 @@ const NewReaders = () => {
   const [defaultOwner, setDefaultOwner] = useState("");
 
   useEffect(() => {
-    if (localStorage.getItem("user_id") == "56Ek4feL/1A8mZgIKQWEqg==") {
+    if (localStorage.getItem("user_id") == "56Ek4feL/1A8mZgIKQWEqg==" ) {
       setAppliedFilter({});
       setFilterObject({});
       setApifilterObject({});
-    } else {
+    } else if(localStorage.getItem("user_id") == "b3APser7L8OELDIG8ee2HQ=="){
+      setAppliedFilter({"contact Type": ["HCP"]});
+      setFilterObject({});
+      setApifilterObject({});
+    
+    }
+    else {
       setAppliedFilter({ status: ["Registered"], "contact Type": ["HCP"] });
       setFilterObject({ status: ["Registered"], "contact Type": ["HCP"] });
       setApifilterObject({ status: ["Registered"], "contact Type": ["HCP"] });
@@ -249,14 +269,24 @@ const NewReaders = () => {
         limit: limit,
       };
       let payload = {};
-      if (localStorage.getItem("user_id") == "56Ek4feL/1A8mZgIKQWEqg==") {
+      if (localStorage.getItem("user_id") == "56Ek4feL/1A8mZgIKQWEqg==" ) {
         payload = {
           ...data,
           ...obj,
           status: ["Registered"],
           "contact Type": ["HCP"],
         };
-      } else {
+      }
+      else if(localStorage.getItem("user_id") == "b3APser7L8OELDIG8ee2HQ==") {
+        payload = {
+          "contact Type": ["HCP"],
+          ...data,
+          ...obj,
+          status: ["Registered"],
+          
+        };
+        
+      }else {
         payload = { ...data, ...obj };
       }
 
@@ -1401,6 +1431,7 @@ const NewReaders = () => {
       getReaderListData(page, obj, search);
       setSearch("");
     }
+    
     if (originalFilterData?.role?.length) {
       setFilterData({ ...filterdata, role: originalFilterData.role });
     }
@@ -1641,6 +1672,14 @@ const NewReaders = () => {
                       filterObject?.["Content Owners"]?.includes(
                         defaultOwner
                       )) ||
+
+                      (localStorage.getItem("user_id") ==
+                      "b3APser7L8OELDIG8ee2HQ==" &&
+                      (( (  Object.keys(filterObject)?.length ==0) ||
+                        Object.keys(filterObject)?.length <=1 &&
+                        filterObject?.["contact Type"]?.includes("HCP")) )
+                     
+                      ) ||
                     (localStorage.getItem("user_id") ==
                       "56Ek4feL/1A8mZgIKQWEqg==" &&
                       Object.keys(filterObject)?.length <= 0)
