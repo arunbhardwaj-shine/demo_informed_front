7008ae1e03a17bfee297202f6eb446b182edbccf
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2023-10-02 17:46:11 +0530

    work on sorting country in filters

--- src/Components/Readers/ReadersView/ReadersList.js
+++ src/Components/Readers/ReadersView/ReadersList.js
@@ -273,6 +273,15 @@ const NewReaders = () => {
             label: item == "B&H" ? "Bosnia and Herzegovina" : item,
           });
 
+          countries.sort((a, b) => {
+            const countryA = a.value.toLowerCase();
+            const countryB = b.value.toLowerCase();
+
+            if (countryA < countryB) return -1;
+            if (countryA > countryB) return 1;
+            return 0;
+          });
+
           setCountryAll(countries);
         });
         setSpcFlag(1);
@@ -296,11 +305,14 @@ const NewReaders = () => {
           ...res?.data?.data?.result,
         ]);
 
-        setConsetCountry({...consetCountry,...res?.data?.data?.otherCountry});
+        setConsetCountry({
+          ...consetCountry,
+          ...res?.data?.data?.otherCountry,
+        });
       } else {
         total_results = res?.data?.data?.result?.length;
         setReaderDataList(res?.data?.data?.result);
-        setConsetCountry(res?.data?.data?.otherCountry)
+        setConsetCountry(res?.data?.data?.otherCountry);
       }
 
       // if ( total_results <= res?.data?.data?.total) {
@@ -484,6 +496,13 @@ const NewReaders = () => {
         Object.keys(filterdata?.regionCountry)?.forEach((values) => {
           if (filterdata?.regionCountry[values] == item) {
             newCountry.push(values);
+            newCountry.sort((a, b) => {
+              const countryA = a.toLowerCase();
+              const countryB = b.toLowerCase();
+              if (countryA < countryB) return -1;
+              if (countryA > countryB) return 1;
+              return 0;
+            });
           }
         });
         delete apifilterObject.country;
@@ -2303,25 +2322,28 @@ const NewReaders = () => {
                                         ""
                                       ) : (
                                         <>
-                                        {
-                                          localStorage.getItem('group_id') == 3 ?
+                                          {localStorage.getItem("group_id") ==
+                                          3 ? (
+                                            <li>
+                                              <h6 className="tab-content-title">
+                                                Consent Country
+                                              </h6>
+                                              <h6>
+                                                {consetCountry?.[data?.id]
+                                                  ? consetCountry?.[data?.id] ==
+                                                    "B&H"
+                                                    ? "Bosnia and Herzegovina"
+                                                    : consetCountry?.[data?.id]
+                                                  : "N/A"}
+                                              </h6>
+                                            </li>
+                                          ) : null}
                                           <li>
                                             <h6 className="tab-content-title">
-                                              Consent Country
+                                              User Status
                                             </h6>
-                                            <h6>
-                                              {
-                                                consetCountry?.[data?.id] ?  consetCountry?.[data?.id] == "B&H" ? "Bosnia and Herzegovina" : consetCountry?.[data?.id] : "N/A"
-                                              }
-                                            </h6>
-                                          </li>: null
-                                        }
-                                        <li>
-                                          <h6 className="tab-content-title">
-                                            User Status
-                                          </h6>
-                                          <h6>{data?.user_status}</h6>
-                                        </li>
+                                            <h6>{data?.user_status}</h6>
+                                          </li>
                                         </>
                                       )}
 
