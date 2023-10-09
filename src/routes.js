6cc8e0b116d535080a8969c06eb4ea4dc7f66445
susheel0339 <susheel.shinedezign@gmail.com>
2023-10-09 10:54:11 +0530

    new images added, route file update and css changes

--- src/routes.js
+++ src/routes.js
@@ -155,6 +155,9 @@ import MarketingEditReader from "./Components/Readers/ReaderEdit/MarketingEditRe
 import MarketingReaderReview from "./Components/Readers/ReaderReview/MarketingReaderReview";
 import PageNotFound from "./Components/CommonComponent/PageNotFound";
 import Feedback from "./Components/R&D/Feedback";
+import MainLanding from "./Components/Library/MainLanding";
+import PharmaMarketing from "./Components/Library/PharmaMarketing";
+import LandingPublisher from "./Components/Library/LandingPublisher";
 let platform = 0;
 let show = 0;
 
@@ -670,6 +673,12 @@ const Routing = () => {
           element={<LoginLayout component={EditLicense} />}
         />
         <Route path="*" element={<PageNotFound />} />
+
+        <Route path="/main-landing" element={<DefaultLayout component={MainLanding} />}/>
+
+        <Route path="/landing-publisher" element={<DefaultLayout component={LandingPublisher} />} />
+
+        <Route path="/pharma-marketing" element={<DefaultLayout component={PharmaMarketing} />}/>
       </Routes>
     </Router>
   );
