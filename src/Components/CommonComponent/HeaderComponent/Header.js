3756f1e1ad420515f652628c3035b6ca88a2c763
varunshine <varun.verma@shinedezign.com>
2023-10-18 11:27:55 +0530

    fixed accept cookie option.

--- src/Components/CommonComponent/HeaderComponent/Header.js
+++ src/Components/CommonComponent/HeaderComponent/Header.js
@@ -59,7 +59,7 @@ const Header = () => {
 
   const logout = () => {
     // localStorage.clear();
-    const keysToKeep = ['uname', 'pass']; 
+    const keysToKeep = ['uname', 'pass', 'acceptedCookies']; 
     for (let i = localStorage.length - 1; i >= 0; i--) {
       const key = localStorage.key(i);
       if (!keysToKeep.includes(key)) {
--- src/Components/Login/Informed.js
+++ src/Components/Login/Informed.js
@@ -161,7 +161,7 @@ const Informed = () => {
   };
 
   const clearLocalStorageExcept = () => {
-    const keysToKeep = ['uname', 'pass']; 
+    const keysToKeep = ['uname', 'pass', 'acceptedCookies']; 
     for (let i = localStorage.length - 1; i >= 0; i--) {
       const key = localStorage.key(i);
       if (!keysToKeep.includes(key)) {
