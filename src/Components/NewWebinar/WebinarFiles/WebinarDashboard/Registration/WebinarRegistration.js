429e107bb21b95b34d705feac54c5a641e83d2b8
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2023-10-19 11:15:31 +0530

    work on changes

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
@@ -8,10 +8,9 @@ const WebinarRegistration = () => {
   const [file, setFile] = useState();
   const [foot, setfoot] = useState();
   const [showModal, setModal] = useState(false);
-  const [formData, setFormData] = useState([])
-  useEffect(() => { }, []);
+  const [formData, setFormData] = useState([]);
+  useEffect(() => {}, []);
   const handleFileSelect = (e, flag) => {
-
     const fileInput = document.createElement("input");
     fileInput.type = "file";
     fileInput.style.display = "none";
@@ -33,12 +32,10 @@ const WebinarRegistration = () => {
   };
 
   const handleModalSave = (form) => {
-    let updateFormData = [...formData]
-    updateFormData.push(form)
-    setFormData(updateFormData)
-
-
-  }
+    let updateFormData = [...formData];
+    updateFormData.push(form);
+    setFormData(updateFormData);
+  };
 
   return (
     <>
@@ -73,52 +70,50 @@ const WebinarRegistration = () => {
                   <button onClick={() => setModal(true)}>AddQuestion</button>
 
                   {/* {showmodel && <Question />} */}
-                  {formData&&formData?.length>0 ? (formData?.map((data, index) => (
-                    <div key={index}>
-                      <form>
-                        <div className="add_hcp_boxes">
-                          <div className="form_action">
-                            <div className="row">
-                              <div className="col-12 col-md-6">
-                                <div className="form-group">
-                                  <label htmlFor="">{data?.label}</label>
-                                  {data?.option?.length>0?(
-                                  data?.inputType==="radio"?
-                                  (data?.option?.map((item,index)=>(
-                                    <div key={index}>
-                                    <label htmlFor="">{item?.label}</label>
-                                    <input
-                                    type="radio"
-                                    
-                                    />
+                  {formData && formData?.length > 0
+                    ? formData?.map((data, index) => (
+                        <div key={index}>
+                          <form>
+                            <div className="add_hcp_boxes">
+                              <div className="form_action">
+                                <div className="row">
+                                  <div className="col-12 col-md-6">
+                                    <div className="form-group">
+                                      <label htmlFor="">{data?.label}</label>
+                                      {data?.option?.length > 0 ? (
+                                        data?.inputType === "radio" ? (
+                                          data?.option?.map((item, index) => (
+                                            <div key={index}>
+                                              <input type="radio" />
+                                              <label htmlFor="">
+                                                {item}
+                                              </label>
+                                            </div>
+                                          ))
+                                        ) : data?.inputType == "checkbox" ? (
+                                          data?.option?.map((item, index) => (
+                                            <div key={index}>
+                                              <input type="checkbox" />
+                                              <label htmlFor="">{item}</label>
+                                            </div>
+                                          ))
+                                        ) : (
+                                          <input
+                                            className="form-control"
+                                            type={data?.inputType}
+                                            placeholder={data?.placeholder}
+                                          />
+                                        )
+                                      ) : null}
                                     </div>
-                                  ))):data?.inputType=="checkbox"?(
-                                  data?.option?.map((item,index)=>(
-                                    <div key={index}>
-                                      <label htmlFor="">{item?.label}</label>
-                                      <input 
-                                      type="checkbox"
-                                      />
-                                    </div>
-                                  )))
-                                 
-                                  :
-                                  (<input
-                                    className="form-control"
-                                    type={data?.inputType}
-                                    placeholder={data?.placeholder} />))
-                                  :null}
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
+                          </form>
                         </div>
-                      </form>
-                    </div>
-                  )) ): null} 
-
-
-                
+                      ))
+                    : null}
                 </div>
               </section>
 
