9370a1c350e55be339006cab5b146c4cb95a4220
susheel0339 <susheel.shinedezign@gmail.com>
2023-10-23 18:28:40 +0530

    chnages in files and css

--- src/Components/Library/LandingFooter.js
+++ src/Components/Library/LandingFooter.js
@@ -472,7 +472,7 @@ const LandingFooter = () => {
         <Modal className="cookies-popup" show={cookieshow} onHide={(e) => handleCookieClose("cookie")}>
             <div className='cookies-popup-inset'>
                 <Modal.Header closeButton>
-                <Modal.Title>Used Cookies</Modal.Title>
+                <Modal.Title>Cookies</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                 <div class="used-cookies-table"><table><thead><tr><th>Name</th><th>Purpose</th></tr></thead><tbody><tr><td>userlang</td><td>For storing language preferance of user</td></tr><tr><td>LANGUAGE_COOKIE_NAME</td><td>For storing language preferance of user </td></tr><tr><td>csrftoken</td><td>This cookie helps to authenticated calls to webservices.</td></tr></tbody></table></div>
