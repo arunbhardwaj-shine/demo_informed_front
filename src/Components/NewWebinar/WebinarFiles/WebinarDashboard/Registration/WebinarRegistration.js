3cbd5ec85e840a9c5d5d175de4c79203475a708e
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2023-11-01 13:36:45 +0530

    work on validation of webinar registration

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
@@ -6,6 +6,7 @@ import Select from "react-select";
 import { loader } from "../../../../../loader";
 import { getData, postData } from "../../../../../axios/apiHelper";
 import { ENDPOINT } from "../../../../../axios/apiConfig";
+import WebinarRegistrationValidation from "./WebinarRegistrationValidation";
 
 // import Question from "./AddQuestion";
 
@@ -71,7 +72,7 @@ const WebinarRegistration = () => {
 
       try {
         const uploadedImageUrl = await uploadImageToServer(file);
-        console.log(uploadedImageUrl, "==>imageUrl");
+
         setFormData({ ...formData, [isSelectedName]: uploadedImageUrl });
       } catch (error) {
         console.error("Error uploading image:", error);
@@ -121,9 +122,6 @@ const WebinarRegistration = () => {
   };
 
   const handleChange = (e, isSelectedName) => {
-    console.log(e);
-    console.log("e-->", e?.target?.name, "-->value--->", e?.target?.value);
-
     if (isSelectedName) {
       let updateFormBody = formData?.body;
 
@@ -180,28 +178,35 @@ const WebinarRegistration = () => {
     e.preventDefault();
 
     try {
-      loader("show");
-      let data = {
-        eventId: eventData?.event_id,
-        companyId: eventData?.company_id,
-        content: JSON.stringify(formData),
-      };
-      console.log("data--->", data);
-      const response = await postData(
-        ENDPOINT.CREATE_WEBINAR_REGISTRATION,
-        data
-      );
-      console.log("res--->", response);
-      setFormData({
-        pageTitle: "",
-        bodyText: "",
-        headerImageUrl: "",
-        body: [],
-        footerImageUrl: "",
-      });
-      setEventData({ event_id: "", company_id: "" });
-      setFile("");
-      setFoot("");
+      const error = WebinarRegistrationValidation(formData, eventData);
+
+      if (Object.keys(error)?.length) {
+        toast.error(error[Object.keys(error)[0]]);
+        return;
+      } else {
+        loader("show");
+        let data = {
+          eventId: eventData?.event_id,
+          companyId: eventData?.company_id,
+          content: JSON.stringify(formData),
+        };
+     
+        const response = await postData(
+          ENDPOINT.CREATE_WEBINAR_REGISTRATION,
+          data
+        );
+
+        setFormData({
+          pageTitle: "",
+          bodyText: "",
+          headerImageUrl: "",
+          body: [],
+          footerImageUrl: "",
+        });
+        setEventData({ event_id: "", company_id: "" });
+        setFile("");
+        setFoot("");
+      }
     } catch (err) {
       console.log("--err", err);
     } finally {
@@ -247,7 +252,7 @@ const WebinarRegistration = () => {
                 </div>
                 <div className="row">
                   <div className="col-lg-3 ">
-                  <label htmlFor="">Registration Page Title</label>
+                    <label htmlFor="">Registration Page Title</label>
                     {/* <h5>Registration Page Title</h5> */}
                   </div>
                   <div className="col-lg-6">
@@ -483,9 +488,7 @@ const WebinarRegistration = () => {
                                                 ) : data?.inputType ==
                                                   "selection" ? (
                                                   <div key={index}>
-                                                    <Select
-                                                      className="dropdown-basic-button split-button-dropup webinar-select"
-                                                    >
+                                                    <Select className="dropdown-basic-button split-button-dropup webinar-select">
                                                       {data?.option?.map(
                                                         (item) => (
                                                           <option
