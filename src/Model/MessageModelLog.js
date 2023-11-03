5bdc75e59a5c4a4862dba2e6de678cca23c36c96
hackmack4772 <aamir.saleem@shinedezign.com>
2023-11-03 12:20:00 +0530

    new changes for activty log

---
+++ src/Model/MessageModelLog.js
@@ -0,0 +1,68 @@
+import { Modal } from "react-bootstrap";
+
+const MessageModelLog = ({
+    show=true,
+    onClose,
+    heading,
+    data,
+    footerButton,
+    handleSubmit,
+    handleQR,
+  }) => {
+    // const handleClose = () => {
+    //   onClose(false);
+    // };
+  
+    return (
+      <>
+        <Modal
+          show={show}
+          className="send-confirm"
+          id="message_modal"
+        >
+          <Modal.Header>
+            <h5 className="modal-title" id="staticBackdropLabel">
+              {heading}
+            </h5>
+  
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              onClick={onClose}
+            ></button>
+          </Modal.Header>
+          <Modal.Body>
+            {
+              /*
+              <h4 className="message"
+              onClick={() => {handleClose();}}>
+  
+              </h4>
+              */
+  
+                footerButton !== ""
+                ?
+                <>
+                  {data != "" ? <h4 id="message_change">{data}</h4> : null}
+                  {/* <button
+                    type="button"
+                    className="btn btn-primary save btn-filled"
+                    onClick={() => {
+                      handleSubmit();
+  
+                      onClose();
+                    }}
+                  >
+                    {footerButton}
+                  </button> */}
+                </>
+                :null
+            }
+  
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  };
+  export default MessageModelLog;
