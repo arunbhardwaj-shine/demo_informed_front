e164a3f68bd64d09e63f07bbe5925f3dee3cb9c3
hackmack4772 <aamir.saleem@shinedezign.com>
2023-11-03 15:04:33 +0530

    changes

--- src/Components/Readers/AddReader/MarketingAddReader.js
+++ src/Components/Readers/AddReader/MarketingAddReader.js
@@ -1461,7 +1461,7 @@ const MarketingAddReader = () => {
                             />
                           </svg>{" "}
                         </button>
-                        <button class="pawword_img" onClick={() => {
+                        <button className="pawword_img" onClick={() => {
                           setCommanLogShow(true)
                           setCommanLogData(logs[index].value)
                         }}>
