e161e87cf715939efe50270703b6d0e4b85cbc80
susheel0339 <susheel.shinedezign@gmail.com>
2023-11-08 16:08:13 +0530

    file update registration

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
@@ -1441,7 +1441,7 @@ const WebinarRegistration = () => {
                                                                 ?.length > 0 &&
                                                               item?.checked ==
                                                                 true ? (
-                                                                <div>
+                                                                <div className="extension">
                                                                   {item?.extension?.map(
                                                                     (
                                                                       extItem,
