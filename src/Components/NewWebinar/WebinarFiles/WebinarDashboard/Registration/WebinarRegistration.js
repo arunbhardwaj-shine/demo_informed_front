86bc7a7ff935b7dec692aaaf3ce286b537f35fc2
hackmack4772 <aamir.saleem@shinedezign.com>
2023-11-08 15:51:12 +0530

    new changes

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/WebinarRegistration.js
@@ -1433,7 +1433,7 @@ const WebinarRegistration = () => {
                                                                 ?.length > 0 &&
                                                               item?.checked ==
                                                                 true ? (
-                                                                <div>
+                                                                <div className="extension">
                                                                   {item?.extension?.map(
                                                                     (
                                                                       extItem,
