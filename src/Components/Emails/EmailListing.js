a920a1752ccbf09c95903847c39306ccd2f8e2c5
varunshine <varun.verma@shinedezign.com>
2023-11-21 15:18:03 +0530

    add badge

--- src/Components/Emails/EmailListing.js
+++ src/Components/Emails/EmailListing.js
@@ -1175,6 +1175,14 @@ const EmailList = (props) => {
                           <div className="mail-box-content">
                             <div className="mail-box-content-top">
                               <div className="mail-box-content-top-view">
+                                {
+                                  data?.resend_badge >= 2 ? 
+                                  <div className="mail-resend">
+                                  <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"><g id="Glyph"><g data-name="Glyph" id="Glyph-2"><path d="M49,35a8,8,0,0,0-3.17.66l.12-.34a1,1,0,1,0-1.9-.64l-1,3a1,1,0,0,0,.58,1.25l2.5,1a1,1,0,0,0,.74-1.86l-.76-.3A6,6,0,1,1,43,43a1,1,0,0,0-2,0,8,8,0,1,0,8-8Z" fill="#0066be"/><path d="M56,32.06V16.23A8.24,8.24,0,0,0,47.77,8H10.23A8.24,8.24,0,0,0,2,16.23V37.77A8.24,8.24,0,0,0,10.23,46H36.36A13,13,0,1,0,56,32.06ZM34.19,27.64a8.11,8.11,0,0,1-10.37,0L6.63,42.86A6.38,6.38,0,0,1,5.2,41.45l17.09-15.1L5.52,12.15a6.56,6.56,0,0,1,1.57-1.3L25,26a6.14,6.14,0,0,0,8,0L50.91,10.85a6.56,6.56,0,0,1,1.57,1.3L35.74,26.33l6.51,5.56a12.46,12.46,0,0,0-1.67,1.21ZM49,54A11,11,0,1,1,60,43,11,11,0,0,1,49,54Z" fill="#0066be"/></g></g></svg>
+                                  <span>{data?.resend_badge - 1}</span>
+                                  </div>
+                                   : null
+                                }
                                 <h5>{data.subject}</h5>
                                 <p>{data.description}</p>
                                 <div className="mailbox-table">
