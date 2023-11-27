7077205e0f89cddeb85bc67ad4e8aee81747510b
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2023-11-27 18:02:34 +0530

    worked on the filter

--- src/Components/Emails/MedpakSelectSmartListUsers.js
+++ src/Components/Emails/MedpakSelectSmartListUsers.js
@@ -59,6 +59,10 @@ const MedpakSelectSmartListUsers = (props) => {
     const [updateCounter, setUpdateCounter] = useState(0);
     const [sortingCount, setSortingCount] = useState(0);
     const [countryWiseData, setCountryWiseData] = useState({});
+    const [showfilter, setShowFilter] = useState(false);
+    const buttonRef = useRef(null);
+    const filterRef = useRef(null);
+    const [filterObject, setFilterObject] = useState({});
     const [hpc, setHpc] = useState([
         {
             firstname: "",
@@ -1201,9 +1205,79 @@ const MedpakSelectSmartListUsers = (props) => {
 
                         <section className="search-hcp">
                             <div className="result-hcp-table">
-                                <div className="table-title">
+
+                            <div
+                    className={
+                      showfilter
+                        ? "filter-by nav-item dropdown highlight"
+                        : "filter-by nav-item dropdown"
+                    }
+                  >
+                    <button
+                      ref={buttonRef}
+                      className={
+                        Object.keys(filterObject).length > 0
+                          ? "btn btn-secondary dropdown filter_applied"
+                          : "btn btn-secondary dropdown"
+                      }
+                      type="button"
+                      id="dropdownMenuButton2"
+                      onClick={() => setShowFilter((showfilter) => !showfilter)}
+                    >
+                      Filter By
+                      {showfilter ? (
+                        <svg
+                          className="close-arrow"
+                          width="13"
+                          height="12"
+                          viewBox="0 0 13 12"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <rect
+                            width="2.09896"
+                            height="15.1911"
+                            rx="1.04948"
+                            transform="matrix(0.720074 0.693897 -0.720074 0.693897 11.0977 0)"
+                            fill="#0066BE"
+                          />
+                          <rect
+                            width="2.09896"
+                            height="15.1911"
+                            rx="1.04948"
+                            transform="matrix(0.720074 -0.693897 0.720074 0.693897 0 1.45898)"
+                            fill="#0066BE"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          className="filter-arrow"
+                          width="16"
+                          height="14"
+                          viewBox="0 0 16 14"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M0.615385 2.46154H3.07692C3.07692 3.14031 3.62892 3.69231 4.30769 3.69231H5.53846C6.21723 3.69231 6.76923 3.14031 6.76923 2.46154H15.3846C15.7243 2.46154 16 2.18646 16 1.84615C16 1.50585 15.7243 1.23077 15.3846 1.23077H6.76923C6.76923 0.552 6.21723 0 5.53846 0H4.30769C3.62892 0 3.07692 0.552 3.07692 1.23077H0.615385C0.275692 1.23077 0 1.50585 0 1.84615C0 2.18646 0.275692 2.46154 0.615385 2.46154Z"
+                            fill="#97B6CF"
+                          />
+                          <path
+                            d="M15.3846 6.15362H11.6923C11.6923 5.47485 11.1403 4.92285 10.4615 4.92285H9.23077C8.552 4.92285 8 5.47485 8 6.15362H0.615385C0.275692 6.15362 0 6.4287 0 6.76901C0 7.10931 0.275692 7.38439 0.615385 7.38439H8C8 8.06316 8.552 8.61516 9.23077 8.61516H10.4615C11.1403 8.61516 11.6923 8.06316 11.6923 7.38439H15.3846C15.7243 7.38439 16 7.10931 16 6.76901C16 6.4287 15.7243 6.15362 15.3846 6.15362Z"
+                            fill="#97B6CF"
+                          />
+                          <path
+                            d="M15.3846 11.077H6.76923C6.76923 10.3982 6.21723 9.84619 5.53846 9.84619H4.30769C3.62892 9.84619 3.07692 10.3982 3.07692 11.077H0.615385C0.275692 11.077 0 11.352 0 11.6923C0 12.0327 0.275692 12.3077 0.615385 12.3077H3.07692C3.07692 12.9865 3.62892 13.5385 4.30769 13.5385H5.53846C6.21723 13.5385 6.76923 12.9865 6.76923 12.3077H15.3846C15.7243 12.3077 16 12.0327 16 11.6923C16 11.352 15.7243 11.077 15.3846 11.077Z"
+                            fill="#97B6CF"
+                          />
+                        </svg>
+                      )}
+                    </button>
+                  </div>
+
+                                {/* <div className="table-title">
                                     <h4>
-                                        {/* HCPs <span>| {smartListSelected.readers_count}</span> */}
+                                        {/* HCPs <span>| {smartListSelected.readers_count}</span>
                                     </h4>
                                     <div className="selected-hcp-table-action">
                                         {editable == false ? (
@@ -1302,7 +1376,8 @@ const MedpakSelectSmartListUsers = (props) => {
                                             </>
                                         ) : null}
                                     </div>
-                                </div>
+                                </div> */}
+
                                 {/* <div className="selected-hcp-list">
                                     {/* <table className="table">
                                         {/* <thead>
