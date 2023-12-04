297f56868a3b12ea7cb401b2826f0c06b1065d50
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2023-12-04 18:37:55 +0530

    worked on the getDetails page

--- src/Components/CommonComponent/GetDetailsLayout.js
+++ src/Components/CommonComponent/GetDetailsLayout.js
@@ -7,6 +7,7 @@ const GetDetailsLayout = () => {
         <>
             {localStorage.getItem("user_id") == "m5JI5zEDY3xHFTZBnSGQZg==" ? (<>
                 {/* {localStorage.getItem("user_id") == "B7SHpAc XDXSH NXkN0rdQ==" ? (<> */}
+                {/* {localStorage.getItem("user_id") == "rjiGlqA9DXJVH7bDDTX0Lg==" ? (<> */}
                 <GetMedpakDetails />
             </>) : (
                 <GetDetails />
