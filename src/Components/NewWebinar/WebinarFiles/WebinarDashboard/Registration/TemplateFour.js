8386e623f2bdab8e137a3252d9d0d51b06950e52
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2023-12-08 15:38:37 +0530

    chnage the date format and fix the comma issue in new template

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/TemplateFour.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/TemplateFour.js
@@ -26,7 +26,7 @@ export default function TemplateFour({ children, formData }) {
     "YYYY-MM-DD"
   );
   if (startDate.isSame(endDate, "day")) {
-    formattedDateRange = startDate.format("D MMMM YYYY");
+    formattedDateRange = startDate.format("D. MMMM YYYY");
   } else if (startDate.isSame(endDate, "month")) {
     formattedDateRange = `${startDate.format("D")} - ${endDate.format(
       "D MMMM YYYY"
@@ -50,38 +50,63 @@ export default function TemplateFour({ children, formData }) {
             </div>
           </div>
           <div className="octapharma_event_content">
-            <h4 style={{ color: formData?.content?.eventDetails?.pageTitle?.color}}>
+            <h4
+              style={{
+                color: formData?.content?.eventDetails?.pageTitle?.color,
+              }}
+            >
               {formData?.content?.eventDetails?.pageTitle?.value}
             </h4>
 
-            <h4 style={{color: formData?.content?.eventDetails?.bodyText?.color}}>
+            <h4
+              style={{
+                color: formData?.content?.eventDetails?.bodyText?.color,
+              }}
+            >
               {formData?.content?.eventDetails?.bodyText?.value}
             </h4>
 
-            <h3 style={{ color: formData?.content?.eventDetails?.SubHeading?.color}}>
+            <h3
+              style={{
+                color: formData?.content?.eventDetails?.SubHeading?.color,
+              }}
+            >
               {formData?.content?.eventDetails?.SubHeading?.value}
             </h3>
 
             <div className="speaker">
-            <h4
-              className="mb-4"
-              style={{textTransform: "capitalize",color:eventDataSample?.speakerName?.color}}
-            >
-             by {eventDataSample?.speakerName?.value
-                ? eventDataSample?.speakerName?.value
-                : eventData?.speaker_name},
-            </h4>
+              <h4
+                className="mb-4"
+                style={{
+                  textTransform: "capitalize",
+                  color: eventDataSample?.speakerName?.color,
+                }}
+              >
+                by
+                {eventDataSample?.speakerName?.value
+                  ? eventDataSample?.speakerName?.value
+                  : eventData?.speaker_name}
+                {formData?.content?.eventDetails?.Specialization?.value && ","}
+              </h4>
 
-            <h4 style={{ color: formData?.content?.eventDetails?.Specialization?.color}} className="speaker-specialization"> 
-            {formData?.content?.eventDetails?.Specialization?.value}
-            </h4>
+              <h4
+                style={{
+                  color: formData?.content?.eventDetails?.Specialization?.color,
+                }}
+                className="speaker-specialization"
+              >
+                {formData?.content?.eventDetails?.Specialization?.value}
+              </h4>
             </div>
 
-            <h4 style= {{ color:eventDataSample?.eventStartDate?.color}}>
+            <h4 style={{ color: eventDataSample?.eventStartDate?.color }}>
               {formattedDateRange}
             </h4>
 
-            <h4>{formData?.content?.eventDetails?.eventStartTime?.value} ({eventData?.countryTimezone}) online in One Source</h4>
+            <h4>
+              {formData?.content?.eventDetails?.eventStartTime?.value} (
+              {eventData?.countryTimezone}) online in One Source
+            </h4>
 
             <div className="octapharma_event_form">{children}</div>
           </div>
