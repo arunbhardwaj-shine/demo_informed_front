e22fcda3615782629279b729fffda22d98ba800d
varunshine <varun.verma@shinedezign.com>
2023-12-11 18:00:56 +0530

    implemented sold unsold functionality for new account under license tab.

--- src/Components/License/CreateChange/LicenseCreateUser.js
+++ src/Components/License/CreateChange/LicenseCreateUser.js
@@ -64,6 +64,9 @@ const LicenseCreateUser = () => {
     allowVideo: false,
     comDatetime: "",
     cpdValue: "",
+    sold_unsold_status: "sold",
+    request_quote: false,
+    pharmaArr: '',
   });
   const [blindType, setBlindType] = useState([
     { value: "blind", label: "blind" },
@@ -80,6 +83,7 @@ const LicenseCreateUser = () => {
   const [userDetail, setUserDetail] = useState({
     user: {},
     production: [],
+    pharmaArr: [],
     sales: [],
     country: [],
     format: [],
@@ -152,6 +156,7 @@ const LicenseCreateUser = () => {
     setUserDetail({
       user: hadData?.data?.data?.user,
       production: hadData?.data?.data?.production,
+      pharmaArr: hadData?.data?.data?.pharma_arr,
       country: country,
       costCenter: hadData?.data?.data?.costCenter,
       sales: hadData?.data?.data?.sale,
@@ -312,6 +317,15 @@ const LicenseCreateUser = () => {
           "tags",
           tagClickedFirst?.length ? JSON.stringify(tagClickedFirst) : ""
         );
+
+        if(userDetail?.user?.[0]?.retailer == 1){
+          formData.append("request_quote", userInputs?.request_quote ? 1 : 0);
+          formData.append("sold_unsold_status", userInputs?.sold_unsold_status);
+          formData.append(
+            "pharama_val",
+            userInputs?.pharmaArr ? userInputs?.pharmaArr : ""
+          );
+        }
 
         const res = await postFormData(ENDPOINT.LIBRARYCREATE, formData, {
           header: {
@@ -581,6 +595,21 @@ const LicenseCreateUser = () => {
                   isClearable
                 />
               </div>
+
+              {
+                 userDetail?.user?.[0]?.retailer == 1  ? 
+                  <div className="form-group">
+                    <label htmlFor="">Pharma</label>
+                    <Select
+                      options={userDetail?.pharmaArr}
+                      onChange={(e) => handleChange(e?.value, "pharmaArr")}
+                      placeholder="Select pharma person"
+                      className="dropdown-basic-button split-button-dropup edit-production-dropdown"
+                      isClearable
+                    />
+                  </div>
+                 : null
+              }
 
               {localStorage.getItem("user_id") ==
                 "rOhdD02MgXkownQqcreqAw==" && (
@@ -1005,6 +1034,72 @@ const LicenseCreateUser = () => {
                     */}
                 </fieldset>
               </div>
+
+              {
+                userDetail?.user?.[0]?.retailer == 1  ? 
+                  <>
+                    <div className="form-group">
+                      <label htmlFor="">Status 
+                      {/* <span>*</span> */}
+                      </label>
+                      <fieldset id="group2">
+                        <input
+                          type="radio"
+                          value="sold"
+                          name="sold_unsold_status"
+                          checked={userInputs?.sold_unsold_status == 'sold' ? true : false}
+                          onClick={(e) =>
+                            handleChange('sold', "sold_unsold_status")
+                          }
+                          id="sold"
+                        />
+                        <label htmlFor="sold">Sold</label>
+                        <input
+                          type="radio"
+                          value="unsold"
+                          name="sold_unsold_status"
+                          checked={userInputs?.sold_unsold_status == 'unsold' ? true : false}
+                          onClick={(e) =>
+                            handleChange('unsold', "sold_unsold_status")
+                          }
+                          id="unsold"
+                        />
+                        <label htmlFor="unsold">Unsold</label>
+                      </fieldset>
+                      {error?.status ? (
+                        <div className="login-validation">
+                          {error?.status}
+                        </div>
+                      ) : null}
+                    </div>
+
+                    <div className="form-group">
+                    <label htmlFor="setasdraft1">Request quote</label>
+                    <fieldset id="request_quote">
+                      <div className="switch">
+                        <label className="switch-light">
+                          <input
+                            type="checkbox"
+                            value="value1"
+                            name="request_quote"
+                            id="setasdraft1"
+                            onChange={(e) => {
+                              handleChange(e.target?.checked, "request_quote");
+                            }}
+                          />
+                          <span>
+                            <span className="switch-btn active">No</span>
+                            <span className="switch-btn ">Yes</span>
+                          </span>
+                          <a className="btn"></a>
+                        </label>
+                      </div>
+                    </fieldset>
+                    </div>
+                  </>
+                
+                : null
+              }
             </div>
             <div className="col-12 col-md-6 d-flex justify-content-end align-items-start right-change">
               <div className="form-group justify-content-end">
