15c6a59db54ae91af61e83be973504b32e8e6890
varunshine <varun.verma@shinedezign.com>
2023-12-27 09:45:50 +0530

    enable reminder enable disable functionality for R&D account.

--- src/Components/CommonComponent/GetDetailsLayout.js
+++ src/Components/CommonComponent/GetDetailsLayout.js
@@ -5,7 +5,7 @@ import GetMedpakDetails from "../Distributes/GetMedpakDetails";
 const GetDetailsLayout = () => {
     return (
         <>
-            {localStorage.getItem("user_id") == "m5JI5zEDY3xHFTZBnSGQZg==" ? (<>
+            {localStorage.getItem("user_id") == "m5JI5zEDY3xHFTZBnSGQZg==" || localStorage.getItem("user_id") == "56Ek4feL/1A8mZgIKQWEqg==" ? (<>
                 {/* {localStorage.getItem("user_id") == "B7SHpAc XDXSH NXkN0rdQ==" ? (<> */}
                 {/* {localStorage.getItem("user_id") == "rjiGlqA9DXJVH7bDDTX0Lg==" ? (<> */}
                 <GetMedpakDetails />
