8389217ce088c6509d6a13a92bf5a97df338f4a5
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2023-12-29 10:41:09 +0530

    work on pie chart component and slider

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/QuestionPollsPieChart.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/QuestionPollsPieChart.js
@@ -41,30 +41,30 @@ const QuestionPollsPieChart = () => {
             {
                 name: 'Questions',
                 colorByPoint: true,
-                // data: [
-                //     {
-                //         name: 'Water',
-                //         y: 55.02
-                //     },
-                //     {
-                //         name: 'Fat',
-                //         sliced: true,
-                //         selected: true,
-                //         y: 26.71
-                //     },
-                //     {
-                //         name: 'Carbohydrates',
-                //         y: 1.09
-                //     },
-                //     {
-                //         name: 'Protein',
-                //         y: 15.5
-                //     },
-                //     {
-                //         name: 'Ash',
-                //         y: 1.68
-                //     }
-                // ]
+                data: [
+                    {
+                        name: 'Water',
+                        y: 55.02
+                    },
+                    {
+                        name: 'Fat',
+                        sliced: true,
+                        selected: true,
+                        y: 26.71
+                    },
+                    {
+                        name: 'Carbohydrates',
+                        y: 1.09
+                    },
+                    {
+                        name: 'Protein',
+                        y: 15.5
+                    },
+                    {
+                        name: 'Ash',
+                        y: 1.68
+                    }
+                ]
             }
         ],
 
