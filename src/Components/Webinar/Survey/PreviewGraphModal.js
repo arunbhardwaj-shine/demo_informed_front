e685c79bc2350f271da769d2af024f373ea30d74
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2023-12-29 11:52:33 +0530

    worked on the poll-listing page

--- src/Components/Webinar/Survey/PreviewGraphModal.js
+++ src/Components/Webinar/Survey/PreviewGraphModal.js
@@ -5,241 +5,25 @@ import Highcharts from "highcharts";
 const PreviewGraphModal = ({ graphType, answerOption }) => {
   const [chartPieOptions, setPieChartOptions] = useState();
   const [chartBarOptions, setBarChartOptions] = useState();
-
-//   useEffect(() => {
-//     console.log(answerOption, "====>questionData");
-//     let percentage = parseInt(100 / answerOption?.length);
-//     let colors = [];
-//     let seriesData = answerOption.map((item, index) => {
-//       colors.push(item?.color);
-//       return {
-//         name: item?.answer,
-//         y: percentage,
-//         color: item?.color,
-//       };
-//     });
-//     console.log(seriesData, "seriesData");
-//     setPieChartOptions({
-//       chart: {
-//         type: "pie",
-//       },
-//       tooltip: {
-//         valueSuffix: "%",
-//       },
-
-//       plotOptions: {
-//         series: {
-//           allowPointSelect: true,
-//           cursor: "pointer",
-//           dataLabels: [
-//             {
-//               enabled: true,
-//               distance: 20,
-//             },
-//             {
-//               enabled: true,
-//               distance: -40,
-//               format: "{point.percentage:.1f}%",
-//               style: {
-//                 fontSize: "1.2em",
-//                 textOutline: "none",
-//                 opacity: 0.7,
-//               },
-//               // filter: {
-//               //     operator: '>',
-//               //     property: 'percentage',
-//               //     value: 10
-//               // }
-//             },
-//           ],
-//         },
-//       },
-//       series: [
-//         {
-//           name: "Percentage",
-//           colorByPoint: true,
-//           data: seriesData,
-//           colors,
-//         },
-//       ],
-//     });
-
-//     setBarChartOptions({
-//       chart: {
-//         type: "bar",
-//       },
-//       tooltip: {
-//         valueSuffix: "%",
-//       },
-
-//       plotOptions: {
-//         series: {
-//           allowPointSelect: true,
-//           cursor: "pointer",
-//           dataLabels: [
-//             {
-//               enabled: true,
-//               distance: 20,
-//             },
-//             {
-//               enabled: true,
-//               distance: -40,
-//               format: "{point.percentage:.1f}%",
-//               style: {
-//                 fontSize: "1.2em",
-//                 textOutline: "none",
-//                 opacity: 0.7,
-//               },
-//               // filter: {
-//               //     operator: '>',
-//               //     property: 'percentage',
-//               //     value: 10
-//               // }
-//             },
-//           ],
-//         },
-//       },
-//       series: [
-//         {
-//           name: "Percentage",
-//           colorByPoint: true,
-//           data: [
-//             {
-//               name: "Water",
-//               y: 55.02,
-//             },
-//             {
-//               name: "Fat",
-//               sliced: true,
-//               selected: true,
-//               y: 26.71,
-//             },
-//             {
-//               name: "Carbohydrates",
-//               y: 1.09,
-//             },
-//             {
-//               name: "Protein",
-//               y: 15.5,
-//             },
-//             {
-//               name: "Ash",
-//               y: 1.68,
-//             },
-//           ],
-//         },
-//       ],
-//     });
-//   }, []);
-
-// useEffect(() => {
-//     console.log(answerOption, "====>questionData");
-//     let percentage = parseInt(100 / answerOption?.length);
-//     let seriesData = answerOption.map((item, index) => ({
-//       name: item?.answer,
-//       y: percentage,
-//       color: item?.color,
-//     }));
-  
-//     console.log(seriesData, "seriesData");
-  
-//     setPieChartOptions({
-//       chart: {
-//         type: "pie",
-//       },
-//       tooltip: {
-//         valueSuffix: "%",
-//       },
-//       plotOptions: {
-//         series: {
-//           allowPointSelect: true,
-//           cursor: "pointer",
-//           dataLabels: [
-//             {
-//               enabled: true,
-//               distance: 20,
-//             },
-//             {
-//               enabled: true,
-//               distance: -40,
-//               format: "{point.percentage:.1f}%",
-//               style: {
-//                 fontSize: "1.2em",
-//                 textOutline: "none",
-//                 opacity: 0.7,
-//               },
-//             },
-//           ],
-//         },
-//       },
-//       series: [
-//         {
-//           name: "Percentage",
-//           colorByPoint: true,
-//           data: seriesData,
-//         },
-//       ],
-//     });
-
-//     setBarChartOptions({
-//         chart: {
-//           type: "bar",
-//         },
-//         tooltip: {
-//           valueSuffix: "%",
-//         },
-//         plotOptions: {
-//           series: {
-//             allowPointSelect: true,
-//             cursor: "pointer",
-//             dataLabels: [
-//               {
-//                 enabled: true,
-//                 distance: 20,
-//               },
-//               {
-//                 enabled: true,
-//                 distance: -40,
-//                 format: "{point.percentage:.1f}%",
-//                 style: {
-//                   fontSize: "1.2em",
-//                   textOutline: "none",
-//                   opacity: 0.7,
-//                 },
-//               },
-//             ],
-//           },
-//         },
-//         series: [
-//           {
-//             name: "Percentage",
-//             colorByPoint: true,
-//             data: seriesData,
-//           },
-//         ],
-//       });
-//   }, [answerOption]);
-  
+ 
   useEffect(() => {
-    console.log(answerOption, "====>questionData");
     let percentage = parseInt(100 / answerOption?.length);
   
     // Create seriesData for Pie Chart
     let pieSeriesData = answerOption.map((item, index) => ({
       name: item?.answer,
       y: percentage,
-      color: item?.color,
+      // color: item?.color,
+      colors: ["#ff5366","#0053a0","#ff8649","#89A550","#4098B7","#DB843D","#FFBE3C","#3cff79","#b58cca","#8c95ca"]
     }));
   
     // Create seriesData for Bar Chart
     let barSeriesData = answerOption.map((item, index) => ({
       name: item?.answer,
       y: percentage,
-      color: item?.color,
+      // color: item?.color,
+      colors: ["#ff5366","#0053a0","#ff8649","#89A550","#4098B7","#DB843D","#FFBE3C","#3cff79","#b58cca","#8c95ca"]
     }));
-  
-    console.log(pieSeriesData, "pieSeriesData");
-    console.log(barSeriesData, "barSeriesData");
   
     setPieChartOptions({
       chart: {
