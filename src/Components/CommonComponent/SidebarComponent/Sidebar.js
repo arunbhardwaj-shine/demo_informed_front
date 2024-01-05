f2e7a5cebdfbdd2b1ac0f9204b4e52629ce6412e
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2024-01-05 15:01:36 +0530

    worked on the chat modal

--- src/Components/CommonComponent/SidebarComponent/Sidebar.js
+++ src/Components/CommonComponent/SidebarComponent/Sidebar.js
@@ -197,7 +197,7 @@ const Sidebar = () => {
     const currentLocation = window.location.pathname;
     const divElement = document.querySelector('.left-sidebar');
 
-    if (currentLocation === '/webinar/event-listing') {
+    if (currentLocation === '/webinar/event-listing' ) {
       divElement?.classList.add('hidesidebar');
     } else {
       divElement?.classList.remove('hidesidebar');
