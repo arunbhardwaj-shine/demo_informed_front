77fbea31ba601056ea135cf880fb5c1c31c3a54e
susheel0339 <susheel.shinedezign@gmail.com>
2024-01-23 18:37:48 +0530

    slider setting

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/LivePollsQuestions.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/LivePollsQuestions.js
@@ -31,12 +31,14 @@ const settings = {
   touchMove: false,
   responsive: [
     {
-      breakpoint: 558,
+      breakpoint: 767,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        dots: true,
-        arrows: true,
+        dots: false,
+        arrows: false,
+        vertical: false,
+  verticalScrolling: false,
       },
     },
   ],
