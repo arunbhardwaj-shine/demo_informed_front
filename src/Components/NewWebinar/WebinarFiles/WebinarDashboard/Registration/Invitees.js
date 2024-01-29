ae1ddec6d88f6e1ab286df53902b0c249fe58988
hackmack4772 <aamir.saleem@shinedezign.com>
2024-01-29 14:38:36 +0530

    Invites page filter changes

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/Invitees.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Registration/Invitees.js
@@ -34,12 +34,20 @@ const Invitees = () => {
     // "User type ": ["HCP", "Staff user", "Test user"],
     // "User ": ["Registered", "Blocked"]
   });
-  const [otherFilter, setOtherFilter] = useState({});
+  const [otherFilter, setOtherFilter] = useState({
+    "UserType": [
+        "HCP"
+    ]
+});
   const [page, setPage] = useState(1)
   const [totalReaders, setTotalReaders] = useState()
   const [totalPage, setTotalPage] = useState()
   const [pageAll, setPageAll] = useState(false);
-  const [appliedFilter, setAppliedFilter] = useState({})
+  const [appliedFilter, setAppliedFilter] = useState({
+    "UserType": [
+        "HCP"
+    ]
+})
 
   const {eventIdContext,handleEventId } = useSidebar();
   const localStorageEvent=JSON.parse(localStorage.getItem("EventIdContext"))
@@ -52,7 +60,7 @@ const Invitees = () => {
     //   handleEventId(localStorageEvent)
     // }  
    if(eventId){
-    getWebinarData(page);
+    getWebinarData(page,otherFilter);
    }
       
    
@@ -421,7 +429,8 @@ const Invitees = () => {
           delete updatedFilter[key]
         }
       }
-    }
+    }    console.log(updatedFilter,"updatedFilter");
+
     setOtherFilter(updatedFilter);
   };
 
