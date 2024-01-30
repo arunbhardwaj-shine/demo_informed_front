23c1f5b805cc608fd6ddf431e1fa8b813ff2ca6c
hackmack4772 <aamir.saleem@shinedezign.com>
2024-01-30 18:31:51 +0530

    Download changes

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/LiveStream.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/LiveStream.js
@@ -789,7 +789,7 @@ const LiveStream = () => {
         }else{
           finalData.LoginTime = item?.login_time ? item?.login_time : "N/A";
         const extraDetails = JSON.parse(item?.extra_details || "{}");
-        finalData.Speed = extraDetails?.speed+"Mb" || "N/A";
+        finalData.Speed = extraDetails?.speed+"Mb" || "0.78MB";
         }
         
         // finalData.Username = item?.username ? item?.username.trim() : "N/A";
