9e742e1f77c75aaf2b538775c9a050c2dd58664d
varunshine <varun.verma@shinedezign.com>
2024-02-02 11:42:18 +0530

    remove the heading and add class over emory_2023 question

--- src/Model/SessionModel.js
+++ src/Model/SessionModel.js
@@ -314,12 +314,16 @@ const [comment,setComment]=useState("")
                 :
                 <p className="event_sub_heading">Thank you for attending the Factor VIII Relevance Academy. We would be very grateful if you would complete and return this evaluation form. Your feedback will help us in our efforts to provide high-quality scientific meetings in the future.</p>
               } */}
-              <h4
-                style={{ color: item?.questionColor }}
-                dangerouslySetInnerHTML={{
-                  __html: item?.parentQuestion,
-                }}
-              ></h4>
+              {
+               item?.parentId != 1660 ?
+                <h4
+                  style={{ color: item?.questionColor }}
+                  dangerouslySetInnerHTML={{
+                    __html: item?.parentQuestion,
+                  }}
+                ></h4>
+                : null
+              }
              
 
               {item?.groupId == 0 && item?.canCustomAnswer == 1 ? (
@@ -373,7 +377,7 @@ const [comment,setComment]=useState("")
                       </div>
                     ) : null}
 
-                    <div className={item?.hasParent ? "form-group" : "form-group no_child"}>
+                    <div className={item?.hasParent && item?.parentId == 1660  ? "form-group custom_ans" : item?.hasParent ? "form-group" : "form-group no_child"}>
                       {
                         item?.hasParent ?
                           <label
