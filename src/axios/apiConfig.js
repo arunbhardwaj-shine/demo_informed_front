c3b47082f6d83e9724df6d3689f02a1412b80dc8
hackmack4772 <aamir.saleem@shinedezign.com>
2024-03-15 16:01:11 +0530

    Ul list update

--- src/axios/apiConfig.js
+++ src/axios/apiConfig.js
@@ -184,5 +184,6 @@ export const ENDPOINT = {
   WEBINAR_EMAIL_GET_FILTER_LIST:"/webinarEmail/get-filter-list",
   WEBINAR_EMAIL_DELETE_COMPAIGN:"/webinarEmail/delete-list",
   WEBINAR_EMAIL_GET_TEMPLATE_LIST:"/webinarEmail/get-templates-list",
-  INTERNAL_HCP:"/webinarEmail/internal-hcps"
+  INTERNAL_HCP:"/webinarEmail/internal-hcps",
+  US_LIST:"/webinarEmail/us-list"
 };
