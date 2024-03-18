2e8c90f162fc9991a8c0b0a3fcf7d8e1e81269ab
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2024-03-18 13:13:30 +0530

    work on routes in webinar verify hcp

--- src/routes.js
+++ src/routes.js
@@ -210,6 +210,7 @@ import WebinarSmartlistUploadExcel from "./Components/NewWebinar/WebinarFiles/We
 import WebinarSelectSmartListUsersLayout from "./Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarSelectSmartListUsersLayout";
 import WebinarVerifyHCP from "./Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarVerifyHCP";
 import WebinarVerifyHcpMAIL from "./Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarVerifyHcpMAIL";
+import WebinarVerifyMAIL from "./Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarVerifyMAIL";
 
 let platform = 0;
 let show = 0;
@@ -664,7 +665,7 @@ const Routing = () => {
         />
         <Route
           path="/webinar/email/verifyMAIL"
-          element={<LoginLayout component={WebinarVerifyHcpMAIL} />}
+          element={<LoginLayout component={WebinarVerifyMAIL} />}
         />
          <Route
           path="/webinar/email/verifyHCP"
