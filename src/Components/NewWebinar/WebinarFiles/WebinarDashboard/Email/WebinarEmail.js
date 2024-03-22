74b7d061080a09d4c3d4af89fc43358e2ee35e0a
hackmack4772 <aamir.saleem@shinedezign.com>
2024-03-22 18:02:23 +0530

    delete functionality

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarEmail.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarEmail.js
@@ -404,8 +404,8 @@ const WebinarEmail = (props) => {
     }
   }
 
-  const showConfirmationPopup = (id) => {
-    setCampaignId(id);
+  const showConfirmationPopup = (id,previous_campaign) => {
+    setCampaignId({id:id,previousCampaign:previous_campaign});
     setPopupMessage({
       message1: "You are about to remove this compaign.",
       message2: "Are you sure you want to do this?",
@@ -418,14 +418,40 @@ const WebinarEmail = (props) => {
     }
   };
 
-  const deleteCompaign = async (id) => {
+  const deleteCompaign = async (item) => {
     loader("show");
     try {
       let body = {
         eventId: eventId,
-        emailAutoresponserId: id
+        emailAutoresponserId: item?.id
       }
-      const res = await postData(ENDPOINT.WEBINAR_EMAIL_DELETE_COMPAIGN, body);
+
+      if(item?.previousCampaign==1){
+        const body = {
+          user_id: localStorage.getItem("user_id"),
+          campaign_id: item?.id,
+        };
+        axios.defaults.baseURL = process.env.REACT_APP_API_KEY;
+        loader("show");
+        axios
+          .post(`emailapi/delete_campaign`, body)
+          .then((res) => {
+            if (res.data.status_code == 200) {
+        
+            } else {
+              toast.warning(res.data.message);
+            }
+            loader("hide");
+          })
+          .catch((err) => {
+            loader("hide");
+            toast.error("Something went wrong");
+          });
+      }
+      else{
+        const res = await postData(ENDPOINT.WEBINAR_EMAIL_DELETE_COMPAIGN, body);
+
+      }
       loader("hide");
       popup_alert({
         visible: "show",
@@ -433,7 +459,8 @@ const WebinarEmail = (props) => {
         type: "success",
         redirect: "",
       });
-      const updatedRes = emailListData?.filter((item) => item?.auto_id !== id);
+      let filterId=item?.id
+      const updatedRes = emailListData?.filter((item) => item?.auto_id !== filterId);
       setEmailListData(updatedRes);
       hideConfirmationModal();
       loader("hide");
@@ -443,6 +470,9 @@ const WebinarEmail = (props) => {
       console.log("--err", err)
     }
   }
+
+
+
 
   const hideConfirmationModal = () => {
     setConfirmationPopup(false);
@@ -1086,7 +1116,7 @@ const WebinarEmail = (props) => {
                               <div className="dlt_btn">
                                 <button
                                   onClick={(e) =>
-                                    showConfirmationPopup(data?.auto_id)
+                                    showConfirmationPopup(data?.auto_id,data?.previous_campaign)
                                   }
                                 >
                                   <img
