a76e3b7a930357c6106c98d5b6dd7b80bc6454a9
susheel0339 <susheel.shinedezign@gmail.com>
2024-03-28 11:10:24 +0530

    css chages as per new design

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarAutoEmails.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarAutoEmails.js
@@ -1345,7 +1345,7 @@ const WebinarAutoEmail = () => {
                                           : ""}
                                       </p>
                                       {/* {indexClicked !== index ? ( */}
-                                      {templateId !== template?.id ? (
+                                      {/* {templateId !== template?.id ? ( */}
                                         <button
                                           onClick={() =>
                                             viewButtonClicked(template, index)
@@ -1354,7 +1354,7 @@ const WebinarAutoEmail = () => {
                                         >
                                           View
                                         </button>
-                                      ) : null}
+                                      {/* ) : null} */}
                                     </div>
                                   </div>
                                 </>
@@ -1403,7 +1403,7 @@ const WebinarAutoEmail = () => {
                                           : ""}
                                       </p>
                                       {/* {indexClicked !== index ? ( */}
-                                      {templateId !== template?.id ? (
+                                      {/* {templateId !== template?.id ? ( */}
                                         <button
                                           onClick={() =>
                                             viewButtonClicked(template, index)
@@ -1412,7 +1412,7 @@ const WebinarAutoEmail = () => {
                                         >
                                           View
                                         </button>
-                                      ) : null}
+                                      {/* ) : null} */}
                                     </div>
                                   </div>
                                 </>
@@ -1424,7 +1424,7 @@ const WebinarAutoEmail = () => {
                   </div>
                 </div>
 
-                <div className="auto_mail_trigger_right col-md-8 col-sm-8">
+                <div className="auto_mail_trigger_right webinar-auto-right col-md-8 col-sm-8">
                   {!templateClicked && !createNewTemplate ? (
                     <div className="mail_trigger_right_dummy">
                       <div className="mail_trigger_dummy_content d-flex justify-content-center">
--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/SmartList/SmartList.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/SmartList/SmartList.js
@@ -1026,7 +1026,14 @@ const SmartList = (props) => {
                     );
                   })
                 ) : (
-                  <div className="not_found">No Data Found</div>
+                  <>
+                  <div className="mail_trigger_right_dummy">
+                    <div className="mail_trigger_dummy_content d-flex justify-content-center">
+                      <h3>Create your first smart list by clicking on <img src={path_image + "add_smartlist.svg"} alt="" /></h3>
+                    </div>
+                  </div>
+                    </>
+                  // <div className="not_found">No Data Found</div>
                 )}
               </div>
             </div>
