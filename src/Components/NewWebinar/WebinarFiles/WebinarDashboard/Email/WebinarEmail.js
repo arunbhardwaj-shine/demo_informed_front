ccfd43e739e20eb36afc570041f819cb1f5599fa
hackmack4772 <aamir.saleem@shinedezign.com>
2024-04-01 16:27:35 +0530

    disabled the regsitered and non registered hcp in   webinar selectHCP

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarEmail.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarEmail.js
@@ -212,6 +212,7 @@ const WebinarEmail = (props) => {
         filterData = response?.response?.data
       }
       setEmailListData(filterData)
+      localStorage.setItem("inviteFlag",response?.response?.invite_flag)
       setTotalEmailListData(response?.response?.data)
       if (Object.keys(filterdata)?.length == 0) {
         getFilterList()
