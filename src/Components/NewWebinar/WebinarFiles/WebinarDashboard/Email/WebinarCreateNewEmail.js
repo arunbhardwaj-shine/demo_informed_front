8d5cb79f3a293c9b452f8d5ad2c0724c5678849d
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2024-04-03 16:25:40 +0530

    work on issue no 7

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarCreateNewEmail.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Email/WebinarCreateNewEmail.js
@@ -1311,6 +1311,7 @@ const WebinarCreateNewEmail = (props) => {
                             res?.data?.response?.data?.map((data) => {
                                 setSelectedHcp((oldArray) => [...oldArray, data]);
                             });
+                           
                             setIsOpenAdd(false);
                             setIsOpensend(true);
                             setValidationError({})
@@ -1634,7 +1635,7 @@ const WebinarCreateNewEmail = (props) => {
             toast.warning("Please select smart list");
         }
         e.preventDefault();
-        setSelectedHcp((oldArray) => [...readers, ...oldArray]);
+        // setSelectedHcp((oldArray) => [...readers, ...oldArray]);
         setIsOpensend(true);
         setAddListOpen(false);
     };
