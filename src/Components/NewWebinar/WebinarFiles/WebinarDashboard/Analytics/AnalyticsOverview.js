aeb30e0035c14a059617f57a102fe9e941c5b363
hackmack4772 <120358827+hackmack4772@users.noreply.github.com>
2024-04-12 14:59:24 +0530

    overview api

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/AnalyticsOverview.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/AnalyticsOverview.js
@@ -1,7 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Col } from 'react-bootstrap';
+import { postData } from '../../../../../axios/apiHelper';
+import { loader } from '../../../../../loader';
+import { ENDPOINT } from '../../../../../axios/apiConfig';
+import { useSidebar } from '../../../../CommonComponent/LoginLayout';
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 const AnalyticsOverview = () => {
     const path_image = process.env.REACT_APP_ASSETS_PATH_INFORMED_DESIGN;
+    const { eventIdContext, handleEventId } = useSidebar();
+      const localStorageEvent = JSON.parse(localStorage.getItem("EventIdContext"));
+      const [eventId, setEventId] = useState(eventIdContext?.eventId || localStorageEvent?.eventId);
+      const [overviewData, setOverviewData] = useState([]);
+      useEffect(() => {
+        const fetchAnalyticsData = async () => {
+            try {
+                loader("show");
+                const body = { eventId };
+                const response = await postData(ENDPOINT.GET_OVERVIEW_DATA, body);
+                const result = response?.data?.data;
+                setOverviewData(result);
+  
+                loader("hide");
+            } catch (error) {
+                loader("hide");
+                console.error('Error fetching analytics data:', error);
+            }
+        };
+  
+        fetchAnalyticsData();
+    }, [eventId]);
   return (
    <>
         <div className='rd-analytics-box'>
