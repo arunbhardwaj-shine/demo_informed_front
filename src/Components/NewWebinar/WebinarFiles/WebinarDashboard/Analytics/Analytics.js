a179cf6ecb350a7780382aed9387ff5dbee5d7bc
hackmack4772 <aamir.saleem@shinedezign.com>
2024-04-19 18:28:08 +0530

    download and other issues fixed

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/Analytics.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/Analytics.js
@@ -167,6 +167,10 @@ const Analytics = (props) => {
   const [appliedFilter, setAppliedFilter] = useState({});
   const [filterObject, setFilterObject] = useState({});
   const [eventData, setEventData] = useState({});
+  const countryBarRef = useRef(null);
+  const countryPieRef = useRef(null);
+  const regionBarRef = useRef(null);
+  const regionPieRef = useRef(null);
   const clearFilter = () => {
     setAppliedFilter({});
     setApifilterObject({});
@@ -531,6 +535,30 @@ const Analytics = (props) => {
       );
     });
   };
+
+    const handleDownload = (format,key) => {
+      // Accessing Highcharts chart object using document.getElementById
+      let chart = key.current && key.current.chart;
+  
+      if (chart) {
+        switch (format) {
+          case 'PNG':
+            chart.exportChart({ type: 'image/png' });
+            break;
+          case 'JPEG':
+            chart.exportChart({ type: 'image/jpeg' });
+            break;
+          case 'PDF':
+            chart.exportChart({ type: 'application/pdf' });
+            break;
+          case 'SVG':
+            chart.exportChart({ type: 'image/svg+xml' });
+            break;
+          default:
+            break;
+        }
+      }
+    }
   
   
   
@@ -889,30 +917,30 @@ const Analytics = (props) => {
                           </label>
                         </div>
                         <Dropdown>
-                          <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="6"
-                              height="24"
-                              viewBox="0 0 6 24"
-                              fill="none"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                clip-rule="evenodd"
-                                d="M6 3C6 4.65685 4.65685 6 3 6C1.34315 6 0 4.65685 0 3C0 1.34315 1.34315 0 3 0C4.65685 0 6 1.34315 6 3ZM6 12C6 13.6569 4.65685 15 3 15C1.34315 15 0 13.6569 0 12C0 10.3431 1.34315 9 3 9C4.65685 9 6 10.3431 6 12ZM3 24C4.65685 24 6 22.6569 6 21C6 19.3431 4.65685 18 3 18C1.34315 18 0 19.3431 0 21C0 22.6569 1.34315 24 3 24Z"
-                                fill="#0066BE"
-                              />
-                            </svg>
-                          </Dropdown.Toggle>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="6"
+          height="24"
+          viewBox="0 0 6 24"
+          fill="none"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M6 3C6 4.65685 4.65685 6 3 6C1.34315 6 0 4.65685 0 3C0 1.34315 1.34315 0 3 0C4.65685 0 6 1.34315 6 3ZM6 12C6 13.6569 4.65685 15 3 15C1.34315 15 0 13.6569 0 12C0 10.3431 1.34315 9 3 9C4.65685 9 6 10.3431 6 12ZM3 24C4.65685 24 6 22.6569 6 21C6 19.3431 4.65685 18 3 18C1.34315 18 0 19.3431 0 21C0 22.6569 1.34315 24 3 24Z"
+            fill="#0066BE"
+          />
+        </svg>
+      </Dropdown.Toggle>
 
-                          <Dropdown.Menu>
-                            <Dropdown.Item>Download PNG</Dropdown.Item>
-                            <Dropdown.Item>Download JPEG</Dropdown.Item>
-                            <Dropdown.Item>Download PDF</Dropdown.Item>
-                            <Dropdown.Item>Download SVG</Dropdown.Item>
-                          </Dropdown.Menu>
-                        </Dropdown>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => handleDownload('PNG',whichTypeGraph==0?countryBarRef:countryPieRef)}>Download PNG</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleDownload('JPEG',whichTypeGraph==0?countryBarRef:countryPieRef)}>Download JPEG</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleDownload('PDF',whichTypeGraph==0?countryBarRef:countryPieRef)}>Download PDF</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleDownload('SVG',whichTypeGraph==0?countryBarRef:countryPieRef)}>Download SVG</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
                         {/* <Button>
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -934,7 +962,8 @@ const Analytics = (props) => {
                     <div className="graph-view">
                       {whichTypeGraph == 0 ? (
                         <HighchartsReact
-                          highcharts={Highcharts}
+                        ref={countryBarRef}
+                        highcharts={Highcharts}
                           options={{
                             chart: {
                               marginTop: 100,
@@ -1014,6 +1043,8 @@ const Analytics = (props) => {
                         />
                       ) : (
                         <HighchartsReact
+                        ref={countryPieRef}
+
                           highcharts={Highcharts}
                           options={pieOptions}
                         />
@@ -1058,30 +1089,30 @@ const Analytics = (props) => {
                           </label>
                         </div>
                         <Dropdown>
-                          <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="6"
-                              height="24"
-                              viewBox="0 0 6 24"
-                              fill="none"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                clip-rule="evenodd"
-                                d="M6 3C6 4.65685 4.65685 6 3 6C1.34315 6 0 4.65685 0 3C0 1.34315 1.34315 0 3 0C4.65685 0 6 1.34315 6 3ZM6 12C6 13.6569 4.65685 15 3 15C1.34315 15 0 13.6569 0 12C0 10.3431 1.34315 9 3 9C4.65685 9 6 10.3431 6 12ZM3 24C4.65685 24 6 22.6569 6 21C6 19.3431 4.65685 18 3 18C1.34315 18 0 19.3431 0 21C0 22.6569 1.34315 24 3 24Z"
-                                fill="#0066BE"
-                              />
-                            </svg>
-                          </Dropdown.Toggle>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="6"
+          height="24"
+          viewBox="0 0 6 24"
+          fill="none"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M6 3C6 4.65685 4.65685 6 3 6C1.34315 6 0 4.65685 0 3C0 1.34315 1.34315 0 3 0C4.65685 0 6 1.34315 6 3ZM6 12C6 13.6569 4.65685 15 3 15C1.34315 15 0 13.6569 0 12C0 10.3431 1.34315 9 3 9C4.65685 9 6 10.3431 6 12ZM3 24C4.65685 24 6 22.6569 6 21C6 19.3431 4.65685 18 3 18C1.34315 18 0 19.3431 0 21C0 22.6569 1.34315 24 3 24Z"
+            fill="#0066BE"
+          />
+        </svg>
+      </Dropdown.Toggle>
 
-                          <Dropdown.Menu>
-                            <Dropdown.Item>Download PNG</Dropdown.Item>
-                            <Dropdown.Item>Download JPEG</Dropdown.Item>
-                            <Dropdown.Item>Download PDF</Dropdown.Item>
-                            <Dropdown.Item>Download SVG</Dropdown.Item>
-                          </Dropdown.Menu>
-                        </Dropdown>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => handleDownload('PNG',whichTypeGraphRegion==0?regionBarRef:regionPieRef)}>Download PNG</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleDownload('JPEG',whichTypeGraphRegion==0?regionBarRef:regionPieRef)}>Download JPEG</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleDownload('PDF',whichTypeGraphRegion==0?regionBarRef:regionPieRef)}>Download PDF</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleDownload('SVG',whichTypeGraphRegion==0?regionBarRef:regionPieRef)}>Download SVG</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
                         {/* <Button>
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -1103,6 +1134,7 @@ const Analytics = (props) => {
                     <div className="graph-view">
                       {whichTypeGraphRegion == 0 ? (
                         <HighchartsReact
+ref={regionBarRef}
                           highcharts={Highcharts}
                           options={{
                             chart: {
@@ -1181,6 +1213,8 @@ const Analytics = (props) => {
                         />
                       ) : (
                         <HighchartsReact
+                        ref={regionPieRef}
+
                           highcharts={Highcharts}
                           options={pieOptionsRegion}
                         />
