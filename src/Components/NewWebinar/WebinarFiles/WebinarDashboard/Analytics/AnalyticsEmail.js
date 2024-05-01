0412a91d7c16fd133f913712c440a9bca4b79835
hackmack4772 <aamir.saleem@shinedezign.com>
2024-05-01 11:38:48 +0530

    fixed the issues

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/AnalyticsEmail.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/AnalyticsEmail.js
@@ -58,7 +58,7 @@ const AnalyticsEmail = () => {
                   {emailData.map(data => (
                     <tr key={data.id}>
                       <td valign='middle'>{data.subject}<span>{data.created_at}</span></td>
-                      <td valign='middle'>Invitation</td>
+                      <td valign='middle'>{data.invitationType}</td>
                       <td valign='middle'>Internal</td>
                       <td valign='middle' className='email-options'>
                         <div className='td-bordered'>
