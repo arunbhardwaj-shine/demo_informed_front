2d2f1a330df62c8adc73cd7199659d5ef777222a
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2024-05-07 15:45:41 +0530

    work on design change

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/Analytics.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/Analytics.js
@@ -2503,7 +2503,7 @@ let listData=response?.response?.data?.filter((item)=>!item?.subject?.includes("
                     highcharts={Highcharts}
                     options={splineChartData}
                   />
-                  : <div className="no_found">No Data Found</div>
+                  : <div className="no_found"><p>No Data Found</p></div>
 
                 }
 
@@ -2514,7 +2514,7 @@ let listData=response?.response?.data?.filter((item)=>!item?.subject?.includes("
                     highcharts={Highcharts}
                     options={pieChartData}
                   />
-                  : <div className="no_found">No Data Found</div>
+                  : <div className="no_found"><p>No Data Found</p></div>
 
                 }
               </div>
