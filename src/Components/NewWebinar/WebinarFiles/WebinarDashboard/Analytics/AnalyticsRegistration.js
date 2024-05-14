723960bed2b31bc89751df81cbcf6973f0ab6446
hackmack4772 <aamir.saleem@shinedezign.com>
2024-05-14 16:42:59 +0530

    Post event view functionality

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/AnalyticsRegistration.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/Analytics/AnalyticsRegistration.js
@@ -331,7 +331,7 @@ const AnalyticsRegistration = ({ dropdownClicked, setEventData }) => {
                 <h6 className="regi-hcp">Registered HCPs</h6>
                 <div className="d-flex">
                   <div className="count-number">
-                    {pieChartData?.totalRegistrations?.totalUsers}
+                    {data?.registeredHcpData?.totalHcps }
                   </div>
                   <img src={path_image + "irt.svg"} alt="" className="doctor" />
                 </div>
