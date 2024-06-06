0deb5f0fc3906f469ace972ebc1f40feb85c8065
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2024-06-06 17:02:20 +0530

    work on pdf link

--- src/Components/Library/CreateChange/AddLinkToPdf.js
+++ src/Components/Library/CreateChange/AddLinkToPdf.js
@@ -909,8 +909,35 @@ const AddLinkToPdf = () => {
                     :(x_cord>=300&&x_cord<600)?x_cord/1.3:(x_cord>=600&&x_cord<1000)?x_cord/1.32:x_cord/1.35;
       }
     }
+    else if (initialscale >= 3100 && initialscale < 3300) {
+      // 3405 900
 
-    else if (initialscale >= 3100 && initialscale < 3500) {
+      if(screenSize>=1600){
+
+        box_width_y = box_width_y / 1.6;
+        box_width_x = box_width_x / 1.65;
+  
+        y_cord = y_cord <= 0 ? 1 : y_cord < 100 ? y_cord / 1.5 : (y_cord >= 100 && y_cord < 200) ? y_cord / 1.55 
+        : (y_cord >= 200 && y_cord < 300) ? y_cord / 1.6: (y_cord >= 300 && y_cord < 400) ? y_cord / 1.62 
+        : (y_cord >= 400 && y_cord < 550) ? y_cord / 1.64 : (y_cord >= 550 && y_cord < 800) ? y_cord / 1.68 : y_cord / 1.7;
+  
+        x_cord = x_cord < 100 ? x_cord / 1.40 : (x_cord >= 100 && x_cord < 200) ? x_cord / 1.55 : (x_cord >= 200 && x_cord < 400) ? x_cord / 1.64
+          : (x_cord >= 400 && x_cord < 600) ? x_cord / 1.66 : (x_cord >= 600 && x_cord < 1200) ? x_cord / 1.70 
+          : (x_cord > 1200 && x_cord < 1300) ? x_cord / 1.72 : x_cord / 1.74;
+      } else{
+        console.log("inside 3200 else ");
+        box_width_y = box_width_y / 1.2
+        box_width_x = box_width_x / 1.2
+
+          y_cord = y_cord<=0?1:(y_cord<50&&y_cord>=1)?y_cord/0.9:(y_cord<100&&y_cord>=50)?y_cord/1.1:(y_cord>=100&&y_cord<200)?y_cord/1.2
+          :(y_cord>=200&&y_cord<300)?y_cord/1.22:(y_cord>=300&&y_cord<500)?y_cord/1.25:(y_cord>=500&&y_cord<800)?y_cord/1.3 :y_cord/1.35;
+
+          x_cord = x_cord<100?x_cord/0.9:(x_cord>=100&&x_cord<300)?x_cord/1.2:(x_cord>=300&&x_cord<400)?x_cord/1.22
+                    :(x_cord>=400&&x_cord<700)?x_cord/1.25:(x_cord>=700&&x_cord<900)?x_cord/1.3:x_cord/1.32;
+      }
+      }
+
+    else if (initialscale >= 3300 && initialscale < 3500) {
       // 3405 900
 
       if(screenSize>=1600){
@@ -930,7 +957,7 @@ const AddLinkToPdf = () => {
         box_width_y = box_width_y / 1.2
         box_width_x = box_width_x / 1.2
 
-          y_cord = y_cord<=0?1:(y_cord<100&&y_cord>=1)?y_cord/0.9:(y_cord>=100&&y_cord<200)?y_cord/1.08:(y_cord>=200&&y_cord<300)?y_cord/1.12
+          y_cord = y_cord<=0?1:(y_cord<100&&y_cord>=1)?y_cord/0.85:(y_cord>=100&&y_cord<200)?y_cord/1.08:(y_cord>=200&&y_cord<300)?y_cord/1.12
                   :(y_cord>=300&&y_cord<500)?y_cord/1.15:(y_cord>=500&&y_cord<800)?y_cord/1.2 :y_cord/1.25;
 
           x_cord = x_cord<100?x_cord/0.9:(x_cord>=100&&x_cord<300)?x_cord/1.05:(x_cord>=300&&x_cord<400)?x_cord/1.1
