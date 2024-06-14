e5bfabc47c3d133ec4bd4e2406c75de5150e932b
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2024-06-14 14:47:27 +0530

    work on to show list name properly in download event stats in analytics section

--- src/Model/WebinarAnalyticCommonModal.js
+++ src/Model/WebinarAnalyticCommonModal.js
@@ -70,10 +70,11 @@ const WebinarAnalyticCommonModal=({data,id,options})=>{
                           <td>{data?.smart_list_name 
                                         ? data?.smart_list_name?.includes("_internal_hcps_temporary") 
                                         ?"All HCPs"
+                                         :data?.smart_list_name?.includes("_no_registered_users_temporary")
+                                        ?"Non registered HCPs"
                                         :data?.smart_list_name?.includes("_registered_users_temporary") 
                                         ?"Registered HCPs"
-                                        :data?.smart_list_name?.includes("_no_registered_users_temporary")
-                                        ?"Non registered HCPs"
+                                       
                                         :data?.smart_list_name?.includes("_uslist_temporary")
                                         ?"US list"
                                         :data?.smart_list_name
