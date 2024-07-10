74b95369ab91025618c1700a78bdaadc527c75a6
varunshine <varun.verma@shinedezign.com>
2024-07-10 13:22:32 +0530

    update norgine account excel file

--- src/Components/Emails/SelectSmartList.js
+++ src/Components/Emails/SelectSmartList.js
@@ -463,8 +463,10 @@ const SelectSmartList = (props) => {
       user_id == "UbCJcnLM9fe HsRMgX8c1A=="
     ) {
       link.href = "https://webinar.informed.pro/sample_st.xlsx";
-    } else if (user_id == "56Ek4feL/1A8mZgIKQWEqg==" || user_id == "sNl1hra39QmFk9HwvXETJA==") {
+    } else if (user_id == "56Ek4feL/1A8mZgIKQWEqg==") {
       link.href = "https://webinar.informed.pro/R_Dsample.xlsx";
+    } else if(user_id == "sNl1hra39QmFk9HwvXETJA==") {
+      link.href = "https://webinar.informed.pro/Norgine_sample.xlsx";
     } else {
       link.href = "https://webinar.informed.pro/sample.xlsx";
     }
