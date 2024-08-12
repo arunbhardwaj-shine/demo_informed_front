0d370af4387fab497438f5d598b83aacc1603bfe
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2024-08-12 11:30:16 +0530

    fix the add user issue

--- src/Components/Emails/VerifyHCP.js
+++ src/Components/Emails/VerifyHCP.js
@@ -952,7 +952,8 @@ const VerifyHCP = (props) => {
             data?.lastname == "" ||
             data?.country == "" ||
             data?.email == "" ||
-            data?.institutionType == ""
+            data?.institutionType == "" ||
+            (data?.optIrt === 'yes' && (data?.siteNumber === "" || data?.siteName === ""))
           ) {
             return "false";
           } else {
