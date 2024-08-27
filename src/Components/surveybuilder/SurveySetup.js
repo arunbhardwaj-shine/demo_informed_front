2421a2e5934696a746879c509678c193f3f840a0
susheel0339 <susheel.shinedezign.com>
2024-08-27 12:02:48 +0530

    show inner page active under survey tab

--- src/Components/surveybuilder/SurveySetup.js
+++ src/Components/surveybuilder/SurveySetup.js
@@ -390,7 +390,7 @@ const SurveySetup = (props) => {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <Col className="right-sidebar custom-change survey-builder">
+      <Col className="right-sidebar custom-change survey-set">
         <div className="container-fluid">
           <div className="row">
             <div className="top-right-action preview">
