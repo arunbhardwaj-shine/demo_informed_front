f52152cae12e71d661d97016bed0c055ff8beb8c
susheel0339 <susheel.shinedezign.com>
2024-08-30 18:20:03 +0530

    sublink change

--- src/Components/surveybuilder/SurveySublinkListing.js
+++ src/Components/surveybuilder/SurveySublinkListing.js
@@ -1,23 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {
-  Button,
-  Col,
-  Dropdown,
-  Modal,
-  DropdownButton,
-  Form,
-  Row,
-  ProgressBar,
-  Tab,
-  Tabs,
-} from "react-bootstrap";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import { surveyAxiosInstance } from "./CommonFunctions/CommonFunction";
 import { toast } from "react-toastify";
 import QRCode from "qrcode.react";
-let path_image = process.env.REACT_APP_ASSETS_PATH_INFORMED_DESIGN;
 
 const SurveySublinkListing = ({ survey_id, render, count }) => {
+  let path_image = process.env.REACT_APP_ASSETS_PATH_INFORMED_DESIGN;
   console.log(survey_id);
 
   const [sectionLoader, setSectionLoader] = useState(false);
