02b3d763a181faeebd4b44486a6035782bdb67e4
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2024-09-17 11:30:48 +0530

    sorting added in Survey Analytics page

--- src/Components/surveybuilder/SurveyAnalytics.js
+++ src/Components/surveybuilder/SurveyAnalytics.js
@@ -36,7 +36,8 @@ const SurveyAnalytics = () => {
   const [getoriginalsendlistdata, setOriginalSendListData] = useState([]);
   const [data,setData]=useState([])
   const [apiStatus,setApiStatus]=useState(false)
-  
+  const [sortBy, setSortBy] = useState('name'); // Initial sort key
+  const [sortOrder, setSortOrder] = useState('asc');  
 
   useEffect(() => {
     //props.getEmailData(null);
@@ -258,6 +259,26 @@ const getSurveyAnalyticsDetail=async()=>{
     setFilter(getfilter);
     let up = updateflag + 1;
     setUpdateFlag(up);
+  };
+
+  const handleSort = (key) => {
+    setSortBy(key);
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); 
+  };
+  const sortData = (data, key, order) => {
+    return data.sort((a, b) => {
+      const valueA = a[key];
+      const valueB = b[key];
+  
+      // Handle different data types (numbers, strings)
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        return order === 'asc' ? valueA - valueB : valueB - valueA;
+      } else {
+        return order === 'asc'
+          ? valueA?.localeCompare(valueB) // Handle string sorting with locale awareness
+          : valueB?.localeCompare(valueA);
+      }
+    });
   };
 
   return (
@@ -615,9 +636,18 @@ const getSurveyAnalyticsDetail=async()=>{
                     <thead className="sticky-header">
                       <tr>
                         <th className="sort_option">
-                          <span>
+                          <span onClick={() => handleSort('Status')}>
                             Status
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "Status" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                            onClick={() => handleSort('Status')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -642,9 +672,18 @@ const getSurveyAnalyticsDetail=async()=>{
                         </th>
 
                         <th className="sort_option">
-                          <span>
+                          <span  onClick={() => handleSort('Title')}>
                             Title & Subtitle
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "Title" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                            onClick={() => handleSort('Title')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -669,9 +708,18 @@ const getSurveyAnalyticsDetail=async()=>{
                         </th>
 
                         <th className="sort_option">
-                          <span>
+                          <span onClick={() => handleSort('Consent')}>
                             Consent
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "Consent" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                              onClick={() => handleSort('Consent')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -696,9 +744,18 @@ const getSurveyAnalyticsDetail=async()=>{
                         </th>
 
                         <th className="sort_option">
-                          <span>
+                          <span onClick={() => handleSort('Creator')}>
                             Creator
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "Creator" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                              onClick={() => handleSort('Creator')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -723,9 +780,18 @@ const getSurveyAnalyticsDetail=async()=>{
                         </th>
 
                         <th className="sort_option">
-                          <span>
+                          <span  onClick={() => handleSort('CreatedDate')}>
                             Created Date
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "CreatedDate" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                              onClick={() => handleSort('CreatedDate')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -750,9 +816,18 @@ const getSurveyAnalyticsDetail=async()=>{
                         </th>
 
                         <th className="sort_option">
-                          <span>
+                          <span onClick={() => handleSort('Completed')}>
                             Completed
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "Completed" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                              onClick={() => handleSort('Completed')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -777,9 +852,18 @@ const getSurveyAnalyticsDetail=async()=>{
                         </th>
 
                         <th className="sort_option">
-                          <span>
+                          <span onClick={() => handleSort('Dropoff')}>
                             Drop-off
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "Dropoff" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                              onClick={() => handleSort('Dropoff')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -830,9 +914,18 @@ const getSurveyAnalyticsDetail=async()=>{
                           </span>
                         </th>
                         <th className="sort_option">
-                          <span>
+                          <span  onClick={() => handleSort('questionCount')}>
                             Questions
-                            <button className="event_sort_btn">
+                            <button 
+                            // className="event_sort_btn"
+                            className={`event_sort_btn ${sortBy == "questionCount" ?
+                              sortOrder == "asc"
+                              ? "svg_asc"
+                              : "svg_active"
+                              : "" 
+                              }`}
+                              onClick={() => handleSort('questionCount')}
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="8"
@@ -860,7 +953,8 @@ const getSurveyAnalyticsDetail=async()=>{
                     </thead>
                     <tbody>                  
 
-                      {data?.length?data?.map((item,index)=>{
+                      {data?.length>0?(<>
+                      {sortData(data, sortBy, sortOrder)?.map((item,index)=>{
                         return(<>
                          <tr className="view">
                         <td className={`status ${item?.Status==0?'draft':item?.Status==1?'live':item?.Status==2?'completed':""}`}>
@@ -912,7 +1006,7 @@ const getSurveyAnalyticsDetail=async()=>{
                         </td>
                       </tr>
                         </>)
-                      }):!apiStatus?<div className="no_found"><p>No Data Found</p></div>:""}
+                     } )}</>):!apiStatus?<div className="no_found"><p>No Data Found</p></div>:""}
                     </tbody>
                   </Table>
                 </div>
