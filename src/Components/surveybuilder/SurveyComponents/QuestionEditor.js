cf70cdb02b4395b345bddafbbd61145ecf9e59d7
unknown <shivam.chauhan@shinedezign.com>
2024-09-20 14:33:30 +0530

    updated by defalut highlight b

--- src/Components/surveybuilder/SurveyComponents/QuestionEditor.js
+++ src/Components/surveybuilder/SurveyComponents/QuestionEditor.js
@@ -19,6 +19,7 @@ const QuestionEditor = ({
         onInit={(evt, editor) =>{ (editorRef.current = editor)
           if (headingBoldFlag) {
             editor.execCommand('Bold');
+            
           }
         }}
 
@@ -51,59 +52,3 @@ const QuestionEditor = ({
 export default QuestionEditor;
 
 
-
-// import React, { useRef, useState, useEffect } from "react";
-// import { Editor } from "@tinymce/tinymce-react";
-
-// const QuestionEditor = ({
-//   value,
-//   handleUpdateElement,
-//   index,
-//   Placeholder,
-//   headingBoldFlag,
-// }) => {
-//   const editorRef = useRef(null);
-//   const [editorValue, setEditorValue] = useState(value);
-
-//   useEffect(() => {
-//     if (headingBoldFlag && editorRef.current) {
-//       editorRef.current.execCommand('Bold');
-//     }
-//   }, [headingBoldFlag]);
-
-//   return (
-//     <div className="text-editor">
-//       <Editor
-//         apiKey="gpl" // Replace with your TinyMCE API key if needed
-//         tinymceScriptSrc={window.location.origin + "/tinymce/tinymce.min.js"} // Ensure this path is correct
-//         onInit={(evt, editor) => {
-//           editorRef.current = editor;
-//           if (headingBoldFlag) {
-//             editor.execCommand('Bold');
-//           }
-//         }}
-//         initialValue={ headingBoldFlag ? `<strong>${editorValue}</strong>` : editorValue}
-//         init={{
-//           height: "120px",
-//           branding: false,
-//           menubar: false,
-//           plugins: "link",
-//           toolbar: "fontsize bold italic underline strikethrough | link forecolor",
-//           content_style:
-//             "body { font-family:Roboto,Helvetica,Arial,sans-serif; font-size:14px;color:#0066be;cursor: text; } body p img{width:100%;} body p{color:#0066be; margin: 2px 0;}, body *{margin:2px 0;}",
-//           automatic_uploads: true,
-//           image_caption: true,
-//           auto_focus: false,
-//           fontsize_formats: "8px 10px 12px 14px 18px 24px 36px", // Optional: Customize font sizes
-//           placeholder: Placeholder,
-//         }}
-//         onEditorChange={(content) => {
-//           setEditorValue(content); // Update the local state
-//           handleUpdateElement(index, "question", content);
-//         }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default QuestionEditor;
