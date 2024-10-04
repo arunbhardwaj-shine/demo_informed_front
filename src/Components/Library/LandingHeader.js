8bdd6b16616a2eb6a7817d4c2f8e116e5435ae14
hackmack4772 <aamir.saleem@shinedezign.com>
2024-10-04 18:44:43 +0530

    issues fixed

--- src/Components/Library/LandingHeader.js
+++ src/Components/Library/LandingHeader.js
@@ -134,7 +134,7 @@ const LandingHeader = () => {
         if (res?.data?.data?.loginCounter == 0) {
           navigate("/change-password");
         } else {
-          if (res?.data?.data?.userToken === "56Ek4feL/1A8mZgIKQWEqg=="  || res?.data?.data?.userToken=="MXl8m36VZFYXpgFVz3Pg0g==") {
+          if (res?.data?.data?.userToken === "56Ek4feL/1A8mZgIKQWEqg==") {
             navigate("/home-timeline")
           } else {
             navigate("/home");
