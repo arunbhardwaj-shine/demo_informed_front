bb61a11459fb4925f4fffa697cec69fcc3851121
varunshine <varun.verma@shinedezign.com>
2024-10-16 15:11:16 +0530

    implement confirm password seperetaly.

--- src/Components/Login/ClientAccount.js
+++ src/Components/Login/ClientAccount.js
@@ -19,6 +19,7 @@ const ClinetAccount = () => {
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
   const [passshow, setPassShow] = useState(false);
+  const [cpassshow, setcPassShow] = useState(false);
   const [successMsg, setSuccessMsg] = useState("");
   const [loginerrors, setLoginerrors] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
@@ -227,6 +228,10 @@ const ClinetAccount = () => {
   // for toggle the password hide show field
   const toggleState = () => {
     setPassShow(!passshow);
+  };
+
+  const toggleCPass = () => {
+    setcPassShow(!cpassshow);
   };
 
   //Encrypt the data
@@ -491,7 +496,7 @@ const ClinetAccount = () => {
                             <FormGroup as={Col} md={6} className="mb-4">
                               <div className={`form-group ${pharmaErrors?.cpassword ? 'error' : ''}`}>
                                 <input
-                                  type={passshow ? "text" : "password"}
+                                  type={cpassshow ? "text" : "password"}
                                   name="cpassword"
                                   placeholder="Confirm Password"
                                   value={pharmaFormData.cpassword}
@@ -499,7 +504,7 @@ const ClinetAccount = () => {
                                   onChange={handlePharmaChange}
                                 />
                                 <span><svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.625 7.5H13V5C13 2.2425 10.7575 0 7.99999 0C5.2425 0 3 2.2425 3 5V7.5H2.375C1.34167 7.5 0.5 8.34083 0.5 9.37499V18.125C0.5 19.1592 1.34167 20 2.375 20H13.625C14.6583 20 15.5 19.1592 15.5 18.125V9.37499C15.5 8.34083 14.6583 7.5 13.625 7.5ZM4.66666 5C4.66666 3.16166 6.16166 1.66667 7.99999 1.66667C9.83833 1.66667 11.3333 3.16166 11.3333 5V7.5H4.66666V5ZM8.83333 13.935V15.8333C8.83333 16.2933 8.46083 16.6667 7.99999 16.6667C7.53916 16.6667 7.16666 16.2933 7.16666 15.8333V13.935C6.67083 13.6458 6.33333 13.1142 6.33333 12.5C6.33333 11.5808 7.08083 10.8333 7.99999 10.8333C8.91916 10.8333 9.66666 11.5808 9.66666 12.5C9.66666 13.1142 9.32916 13.6458 8.83333 13.935Z" fill="#97B6CF"></path></svg></span>
-                                <span className="pawword_img"><img src={passshow ? path_image + "show_p.svg" : path_image + "hide.svg"} alt="" onClick={toggleState} /></span>
+                                <span className="pawword_img"><img src={cpassshow ? path_image + "show_p.svg" : path_image + "hide.svg"} alt="" onClick={toggleCPass} /></span>
                                 {pharmaErrors.cpassword && <p className="error-msg">{pharmaErrors.cpassword}</p>}
                               </div>
                             </FormGroup>
@@ -602,7 +607,7 @@ const ClinetAccount = () => {
                             <FormGroup as={Col} md={12} className="mb-4">
                               <div className={`form-group ${resetErrors?.cpassword ? 'error' : ''}`}>
                                 <input
-                                  type={passshow ? "text" : "password"}
+                                  type={cpassshow ? "text" : "password"}
                                   name="cpassword"
                                   placeholder="Confirm Password"
                                   value={resetFormData.cpassword}
@@ -610,7 +615,7 @@ const ClinetAccount = () => {
                                   onChange={handleResetChange}
                                 />
                                 <span><svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.625 7.5H13V5C13 2.2425 10.7575 0 7.99999 0C5.2425 0 3 2.2425 3 5V7.5H2.375C1.34167 7.5 0.5 8.34083 0.5 9.37499V18.125C0.5 19.1592 1.34167 20 2.375 20H13.625C14.6583 20 15.5 19.1592 15.5 18.125V9.37499C15.5 8.34083 14.6583 7.5 13.625 7.5ZM4.66666 5C4.66666 3.16166 6.16166 1.66667 7.99999 1.66667C9.83833 1.66667 11.3333 3.16166 11.3333 5V7.5H4.66666V5ZM8.83333 13.935V15.8333C8.83333 16.2933 8.46083 16.6667 7.99999 16.6667C7.53916 16.6667 7.16666 16.2933 7.16666 15.8333V13.935C6.67083 13.6458 6.33333 13.1142 6.33333 12.5C6.33333 11.5808 7.08083 10.8333 7.99999 10.8333C8.91916 10.8333 9.66666 11.5808 9.66666 12.5C9.66666 13.1142 9.32916 13.6458 8.83333 13.935Z" fill="#97B6CF"></path></svg></span>
-                                <span className="pawword_img"><img src={passshow ? path_image + "show_p.svg" : path_image + "hide.svg"} alt="" onClick={toggleState} /></span>
+                                <span className="pawword_img"><img src={cpassshow ? path_image + "show_p.svg" : path_image + "hide.svg"} alt="" onClick={toggleCPass} /></span>
                                 {resetErrors.cpassword && <p className="error-msg">{resetErrors.cpassword}</p>}
                               </div>
                             </FormGroup>
