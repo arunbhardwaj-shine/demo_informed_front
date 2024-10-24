c55c7bef54e4b422349aa0a7fc7d632134597009
varunshine <varun.verma@shinedezign.com>
2024-10-24 18:26:03 +0530

    fixed date issue in R&D survey stats.

--- src/Components/surveybuilder/SurveyAnalyticsDetail.js
+++ src/Components/surveybuilder/SurveyAnalyticsDetail.js
@@ -1722,10 +1722,13 @@ const SurveyAnalyticsDetail = () => {
 
                                           <td>{item?.country}</td>
                                           <td>
+                                            {item?.date ? item?.date : "N/A"}
+                                          </td>
+                                          {/* <td>
                                             {moment(item?.date).format(
                                               "DD MMM. YYYY"
                                             )}
-                                          </td>
+                                          </td> */}
                                           <td className={item?.status}>
                                             {item?.status}
                                           </td>
