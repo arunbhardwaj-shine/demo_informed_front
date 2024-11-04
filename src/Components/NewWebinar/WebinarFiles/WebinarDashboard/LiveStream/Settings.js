5589f76678aaa99f36c408099b4e0aa475cc9734
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2024-11-04 12:08:16 +0530

    change the poster images for the upcoming event

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/Settings.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/Settings.js
@@ -41,7 +41,7 @@ const Settings = () => {
       {
         label: "Thank you message with speaker image",
         value:
-          "https://docintel.s3.eu-west-1.amazonaws.com/image/CP_Brand_thanks_banner_natascha.jpg",
+          "https://onesourcedoc.s3.eu-west-1.amazonaws.com/images/CP_Brand_thanks_banner_anna-min.jpg",
       },
       {
         label: "Event delayed",
@@ -51,7 +51,7 @@ const Settings = () => {
       {
         label: "Stay tuned",
         value:
-          "https://docintel.s3.eu-west-1.amazonaws.com/image/CP_Brand_video_banner_natascha.jpg",
+          "https://onesourcedoc.s3.eu-west-1.amazonaws.com/images/cp_brand_video_banner_anna-pavlova-min.jpg",
       },
       {
         label: "Technical difficulties",
