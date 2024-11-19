9bed9ae235405b0ff16db18ff377a5b44943c8df
Gagandeep Singh <gagandeep.singh@shinedezign.com>
2024-11-19 13:16:25 +0530

    work on changes for custom answer graph labels

--- src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/QuestionPollsPieChart.js
+++ src/Components/NewWebinar/WebinarFiles/WebinarDashboard/LiveStream/QuestionPollsPieChart.js
@@ -226,9 +226,12 @@ const QuestionPollsPieChart = ({ data,show,type }) => {
                 }
             }
         }
-    } : {
+    }    
+    :
+     {
         ...baseBarChartOptions,
         exporting: {
+            
             enabled: false,
         }
     });
@@ -292,6 +295,7 @@ const QuestionPollsPieChart = ({ data,show,type }) => {
                 series: [{ ...pieChartOptions?.series[0], data: seriesData?.slice(1) }], 
                 drilldown : {"series": drilldownData} })
         } else if(data?.graphType=="bar"){
+           
             let totalAnswer = data?.pollAnswers?.map(item => item.y) // Extracting the 'y' values
             .reduce((total, yValue) => total + yValue, 0);
           data?.pollAnswers?.map((item, index) => {
@@ -308,7 +312,27 @@ const QuestionPollsPieChart = ({ data,show,type }) => {
                 categories: options,
             }, 
             // series: [{ ...barChartOptions?.series[0], data: seriesData }] })
-            series:barSeriesData?.slice(1)})
+            plotOptions:{...barChartOptions.plotOptions,series:{...barChartOptions.plotOptions.series, dataLabels: [
+                {
+                    enabled: true,
+                    formatter: function() {
+                        // var pcnt = this.point.p.toFixed(0);
+                        // return data?.canCustomAnswer==1?'<tspan >' + this.point.y +  '</tspan>':'<tspan >' + pcnt + "%" + '</tspan>';
+                        var pcnt=data?.canCustomAnswer==1? this.point.y :this.point.p.toFixed(0) + "%" ;
+                        return '<tspan >' + pcnt  + '</tspan>';
+                    },
+                    style: {
+                        fontSize: "1.2em",
+                        textOutline: "none",
+                        opacity: 0.7,
+                    },
+                },
+            ],}},
+            series:barSeriesData?.slice(1),            
+           
+           
+        })
+            
            
         }
         
@@ -326,13 +350,13 @@ const QuestionPollsPieChart = ({ data,show,type }) => {
                 options={ pieChartOptions }
             />
           
-            :(data?.graphType=="bar" && data?.pollAnswers?.length)?
-           
+            :(data?.graphType=="bar" && data?.pollAnswers?.length)?(<>
             <HighchartsReact
                 key={"bar"}
                 highcharts={Highcharts}
                 options={ barChartOptions }
             />
+            </>)
             
             :
             <div className="no_found">
