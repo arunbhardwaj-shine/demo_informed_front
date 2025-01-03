41c1ae7024682deb438ef4a4aaaf76d2f73ad6d8
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2025-01-03 12:47:26 +0530

    fix images error

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Change this to your desired port
+    port: 3001, // Change this to your desired port
   },
   optimizeDeps: {
     include: [
