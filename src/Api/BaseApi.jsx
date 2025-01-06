959395e03baec3ce45cdeaa9b882d8e645874bfb
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2025-01-06 18:42:38 +0530

    remove comented code

--- src/Api/BaseApi.jsx
+++ src/Api/BaseApi.jsx
@@ -2,12 +2,9 @@ import { create } from 'apisauce';
 sessionStorage.setItem("user_idsd","demo");
 		console.log(sessionStorage.getItem("user_idsd"));
 const BaseApi = create({
-    //  baseURL: 'http://192.168.0.101:8000/api',
-      //  baseURL: 'http://192.168.0.101:8000/api',
 
       baseURL: 'https://webinarapi.shinedezign.pro/api',
     headers: {Authorization: localStorage.getItem("Token")},
 });
   const BaseUrlImage = 'https://webinarapi.shinedezign.pro'
-  // const BaseUrlImage = 'http://192.168.0.101:8000'
 export { BaseApi, BaseUrlImage }
