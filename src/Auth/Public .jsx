31e10d66ee0324761e1d98aeae06b5e00cc5ac93
unknown <shivam.chauhan@shinedezign.com>
2025-01-07 10:05:28 +0530

    side bar images removed

--- src/Auth/Public .jsx
+++ src/Auth/Public .jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
 import Sidebar from '../Components/CommonComponent/SidebarComponent/Sidebar'
 import SidebarWebinar from './../Components/Webinar/Layout/Sidebar'
 const Public = () => {
