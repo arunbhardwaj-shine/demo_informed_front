70bdcaee3c78c3ce46134783ced2245c603b33c4
varunshine <varun.verma@shinedezign.com>
2025-02-25 18:13:02 +0530

    Update default expire time for WileyBundles account.

--- src/Components/License/CreateChange/LicenseCreateUser.jsx
+++ src/Components/License/CreateChange/LicenseCreateUser.jsx
@@ -40,7 +40,14 @@ const LicenseCreateUser = () => {
   const navigate = useNavigate();
   const [error, setError] = useState({});
   const [userInputs, setCreateLibraryInputs] = useState({
-    expDatetime: new Date(
+    expDatetime:
+    localStorage.getItem('user_id') == ' kCeCVRyC5/qwI7BcPJqDQ==' || localStorage.getItem('user_id') == 'kCeCVRyC5/qwI7BcPJqDQ=='
+    ?
+    new Date(
+      moment(new Date()).add(1, "months").format("MM/DD/YYYY")
+    )
+    :
+    new Date(
       moment(new Date()).add(1, "years").format("MM/DD/YYYY")
     ),
     limit: "",
@@ -993,7 +1000,17 @@ const LicenseCreateUser = () => {
                   selected={
                     userInputs?.expDatetime
                       ? new Date(userInputs?.expDatetime)
-                      : new Date(
+                      :
+                      localStorage.getItem('user_id') == ' kCeCVRyC5/qwI7BcPJqDQ=='
+                      || localStorage.getItem('user_id') == 'kCeCVRyC5/qwI7BcPJqDQ=='
+                      ?
+                      new Date(
+                        moment(new Date(), "MM/DD/YYYY")
+                          .add("months", 1)
+                          .format("MM/DD/YYYY")
+                      )
+                      :
+                      new Date(
                           moment(new Date(), "MM/DD/YYYY")
                             .add("years", 1)
                             .format("MM/DD/YYYY")
