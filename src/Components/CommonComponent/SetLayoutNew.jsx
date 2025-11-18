02f717ffa42b6d13808684e29b582b213540fced
Radhe-2001 <radhey.shyam@shinedezign.com>
2025-11-18 12:51:50 +0530

    added filter in demo informed

--- src/Components/CommonComponent/SetLayoutNew.jsx
+++ src/Components/CommonComponent/SetLayoutNew.jsx
@@ -55,6 +55,10 @@ const SetLayoutNew = () => {
     },
   ];
 
+    const buttonRef = useRef(null);
+    const filterRef = useRef(null);
+  
+
   const [data, setData] = useState([]);
   const [sortBy, setSortBy] = useState('site_number'); // Initial sort key
   const [sortOrder, setSortOrder] = useState('desc');
@@ -75,6 +79,14 @@ const SetLayoutNew = () => {
     content: false,
     top_content: false,
   });
+
+  const [filterApplyflag, setFilterApplyflag] = useState(0);
+  const [filterObject, setFilterObject] = useState({});
+  const [showfilter, setShowFilter] = useState(false);
+  const [appliedFilter, setAppliedFilter] = useState({});
+  const [selectedFilter,setSelectedFilter]= useState({});
+  const [showFilterBox,setShowFilterBox] = useState(false)
+  
 
   const getStatusColor = (code) => {
     switch (code) {
@@ -130,7 +142,6 @@ const SetLayoutNew = () => {
     ],
     'user_type': ['Site User-Blinded', 'Investigator-Blinded', 'Site unblinded pharmacist'],
     'site_number': []
-
   });
   let createdBy = localStorage.getItem("user_id")
   
@@ -293,10 +304,7 @@ const SetLayoutNew = () => {
     event.stopPropagation();
   }
 
-
-  
-  
-  useEffect( async () => {
+  useEffect(() => {
     let newdata = [...dummyData];
     if (localStorage.getItem("group_id") == 2) {
       newdata.push({
@@ -394,12 +402,48 @@ const SetLayoutNew = () => {
     return `${hours12}:${minutesFormatted}:${secondsFormatted} ${ampm}`;
 }
 
+  const handleOnFilterChange = (e, item, index, key, data = []) => {
+    let newObj = JSON.parse(JSON.stringify(selectedFilter));
+    if (!newObj[key]) newObj[key] = [];
+
+    if (e.target.checked) {
+      newObj[key].push(item);
+    } else {
+      const filteredItem = newObj[key].filter((itm) => itm !== item);
+      newObj[key] = filteredItem;
+    }
+    setSelectedFilter(newObj);
+  };
+
+  const applyFilter = () => {
+    setAppliedFilter(selectedFilter);
+    setShowFilter(false);
+  };
+
+  const clearFilter = () => {
+    setSelectedFilter({});
+    setAppliedFilter({});
+    setShowFilterBox(false);
+  };
+
+  useEffect(() => {
+    let allowFilterShow = false;
+    Object.keys(appliedFilter).forEach((key) => {
+      const value = appliedFilter[key];
+      const isArray = Array.isArray(value);
+      if (isArray  ) {
+       if(value.length > 0) allowFilterShow = true;
+      } else if (value !== null && value !== "" && value !== undefined) {
+        allowFilterShow = true;
+      }
+    });
+
+    setShowFilterBox(allowFilterShow);
+  }, [appliedFilter]);
+
   return (
     <>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       {isAuthenticated ? (
         <>
           <Header />
@@ -433,16 +477,298 @@ const SetLayoutNew = () => {
                 <div className="timeline-right-list">
                   <div className="timeline-right-header">
                     <div className="timeline-indicator">
-                      <img src={path_image + "informed-circle-icon.svg"} alt="" />
+                      <img
+                        src={path_image + "informed-circle-icon.svg"}
+                        alt=""
+                      />
                     </div>
                     <div className="timeline-date">
                       <h3>Trial</h3>
-                      <p>July. 29. 2024 <span>|</span> 3:00 PM  <sub>last update</sub></p>
+                      <p>
+                        July. 29. 2024 <span>|</span> 3:00 PM{" "}
+                        <sub>last update</sub>
+                      </p>
+                    </div>
+                    <div>
+                      <button
+                        ref={buttonRef}
+                        className={
+                          Object.keys(filterObject)?.length &&
+                          filterApplyflag == 1
+                            ? "btn btn-secondary dropdown filter_applied"
+                            : "btn btn-secondary dropdown"
+                        }
+                        type="button"
+                        id="dropdownMenuButton2"
+                        onClick={() =>
+                          setShowFilter((showfilter) => !showfilter)
+                        }
+                      >
+                        Filter By
+                        {showfilter ? (
+                          <svg
+                            className="close-arrow"
+                            width="13"
+                            height="12"
+                            viewBox="0 0 13 12"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <rect
+                              width="2.09896"
+                              height="15.1911"
+                              rx="1.04948"
+                              transform="matrix(0.720074 0.693897 -0.720074 0.693897 11.0977 0)"
+                              fill="#0066BE"
+                            />
+                            <rect
+                              width="2.09896"
+                              height="15.1911"
+                              rx="1.04948"
+                              transform="matrix(0.720074 -0.693897 0.720074 0.693897 0 1.45898)"
+                              fill="#0066BE"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            className="filter-arrow"
+                            width="16"
+                            height="14"
+                            viewBox="0 0 16 14"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M0.615385 2.46154H3.07692C3.07692 3.14031 3.62892 3.69231 4.30769 3.69231H5.53846C6.21723 3.69231 6.76923 3.14031 6.76923 2.46154H15.3846C15.7243 2.46154 16 2.18646 16 1.84615C16 1.50585 15.7243 1.23077 15.3846 1.23077H6.76923C6.76923 0.552 6.21723 0 5.53846 0H4.30769C3.62892 0 3.07692 0.552 3.07692 1.23077H0.615385C0.275692 1.23077 0 1.50585 0 1.84615C0 2.18646 0.275692 2.46154 0.615385 2.46154Z"
+                              fill="#97B6CF"
+                            ></path>
+                            <path
+                              d="M15.3846 6.15362H11.6923C11.6923 5.47485 11.1403 4.92285 10.4615 4.92285H9.23077C8.552 4.92285 8 5.47485 8 6.15362H0.615385C0.275692 6.15362 0 6.4287 0 6.76901C0 7.10931 0.275692 7.38439 0.615385 7.38439H8C8 8.06316 8.552 8.61516 9.23077 8.61516H10.4615C11.1403 8.61516 11.6923 8.06316 11.6923 7.38439H15.3846C15.7243 7.38439 16 7.10931 16 6.76901C16 6.4287 15.7243 6.15362 15.3846 6.15362Z"
+                              fill="#97B6CF"
+                            ></path>
+                            <path
+                              d="M15.3846 11.077H6.76923C6.76923 10.3982 6.21723 9.84619 5.53846 9.84619H4.30769C3.62892 9.84619 3.07692 10.3982 3.07692 11.077H0.615385C0.275692 11.077 0 11.352 0 11.6923C0 12.0327 0.275692 12.3077 0.615385 12.3077H3.07692C3.07692 12.9865 3.62892 13.5385 4.30769 13.5385H5.53846C6.21723 13.5385 6.76923 12.9865 6.76923 12.3077H15.3846C15.7243 12.3077 16 12.0327 16 11.6923C16 11.352 15.7243 11.077 15.3846 11.077Z"
+                              fill="#97B6CF"
+                            ></path>
+                          </svg>
+                        )}
+                      </button>
+
+                      {showfilter && (
+                        <div
+                          ref={filterRef}
+                          className="dropdown-menu filter-options"
+                          aria-labelledby="dropdownMenuButton2"
+                        >
+                          <h4>Filter By</h4>
+
+                          <Accordion defaultActiveKey="0" flush>
+                            {Object.keys(filterdata)?.map(function (
+                              key,
+                              index
+                            ) {
+                              return (
+                                <>
+                                  {filterdata[key]?.length > 0 ? (
+                                    <Accordion.Item
+                                      className={
+                                        key == "role" ? "card upper" : "card"
+                                      }
+                                      eventKey={index}
+                                    >
+                                      <Accordion.Header className="card-header">
+                                        {key == "training_status_code"
+                                          ? "Status"
+                                          : key == "user_type"
+                                          ? "Role"
+                                          : key == "site_number"
+                                          ? "Site"
+                                          : key}
+                                      </Accordion.Header>
+
+                                      <Accordion.Body className="card-body">
+                                        <ul>
+                                          {filterdata[key]?.length
+                                            ? filterdata[key]?.map(
+                                                (item, index) => (
+                                                  <li key={index}>
+                                                    {item != "" ? (
+                                                      <label className="select-multiple-option">
+                                                        <input
+                                                          type="checkbox"
+                                                          id={`custom-checkbox-${item}-${index}`}
+                                                          value={
+                                                            typeof item ==
+                                                            "object"
+                                                              ? item?.title
+                                                              : item
+                                                          }
+                                                          name={key}
+                                                          checked={
+                                                            typeof item ==
+                                                            "object"
+                                                              ? selectedFilter[
+                                                                  key
+                                                                ]?.includes(
+                                                                  item.id
+                                                                )
+                                                                ? true
+                                                                : false
+                                                              : selectedFilter[
+                                                                  key
+                                                                ]?.includes(
+                                                                  item
+                                                                )
+                                                              ? true
+                                                              : false
+                                                          }
+                                                          onChange={(e) =>
+                                                            handleOnFilterChange(
+                                                              e,
+                                                              typeof item ==
+                                                                "object"
+                                                                ? item.id
+                                                                : item,
+                                                              index,
+                                                              key,
+                                                              [
+                                                                ...filterdata[
+                                                                  key
+                                                                ],
+                                                              ]
+                                                            )
+                                                          }
+                                                        />
+                                                        {typeof item == "object"
+                                                          ? item?.title
+                                                          : item}
+
+                                                        <span className="checkmark"></span>
+                                                      </label>
+                                                    ) : null}
+                                                  </li>
+                                                )
+                                              )
+                                            : null}
+                                        </ul>
+                                      </Accordion.Body>
+                                    </Accordion.Item>
+                                  ) : null}
+                                </>
+                              );
+                            })}
+                          </Accordion>
+
+                          <div className="filter-footer">
+                            <Button
+                              className="btn btn-primary btn-bordered"
+                              onClick={clearFilter}
+                            >
+                              Clear
+                            </Button>
+                            <Button
+                              className="btn btn-primary btn-filled"
+                              onClick={applyFilter}
+                            >
+                              Apply
+                            </Button>
+                          </div>
+                        </div>
+                      )}
                     </div>
                   </div>
+                  {showFilterBox && (
+                    <div
+                      style={{ display: "flex", border: "dotted", padding: 2 }}
+                    >
+                      <div>
+                        {Object.keys(appliedFilter).map((key, index) => {
+                          const value = appliedFilter[key];
+                          if (
+                            value === null ||
+                            value === undefined ||
+                            value === "" ||
+                            (Array.isArray(value) && value.length === 0)
+                          )
+                            return;
+                          return (
+                            <div
+                              key={index}
+                              style={{ display: "flex", gap: 2 }}
+                            >
+                              <span>
+                                {key == "training_status_code"
+                                  ? "Status"
+                                  : key == "user_type"
+                                  ? "Role"
+                                  : key == "site_number"
+                                  ? "Site"
+                                  : key}
+                              </span>
+                              :
+                              {appliedFilter[key]?.length &&
+                                appliedFilter[key]?.map((item, i) => {
+                                  const totalData = filterdata[key];
+                                  const isObj = totalData?.length > 0 && typeof totalData[0] === "object";
+                                  let text = "";
+                                  if (isObj) {
+                                    const found = totalData.find(
+                                      (itm) => itm.id === item
+                                    );
+                                    text = found ? found.title : "";
+                                  } else {
+                                    text = item;
+                                  }
+                                  const removeItem = () => {
+                                    const selectedData = selectedFilter[key];
+                                    let filterdData = selectedData.filter(
+                                      (itm) => itm !== item
+                                    );
+                                    setSelectedFilter({
+                                      ...selectedFilter,
+                                      [key]: filterdData,
+                                    });
+                                    setAppliedFilter({
+                                      ...selectedFilter,
+                                      [key]: filterdData,
+                                    });
+                                    
+                                  };
+                                  return (
+                                    <span key={key + text}>
+                                      {text != "" && (
+                                        <span>
+                                          {text}{" "}
+                                          <button
+                                            type="button"
+                                            onClick={removeItem}
+                                            style={{padding:2}}
+                                          >
+                                            X
+                                          </button>
+                                        </span>
+                                      )}
+                                    </span>
+                                  );
+                                })}
+                            </div>
+                          );
+                        })}
+                      </div>
+                      <div style={{ marginLeft: "auto" }} onClick={clearFilter}>
+                        Clear All
+                      </div>
+                    </div>
+                  )}
                   <div className="timeline-right-body">
-                    <TrialCompletionTable createdBy={createdBy} pathImage={path_image} />
-                  </div>                </div>
+                    <TrialCompletionTable
+                      createdBy={createdBy}
+                      pathImage={path_image}
+                      appliedFilters={appliedFilter}
+                      filterdata={filterdata}
+                    />
+                  </div>{" "}
+                </div>
               </div>
             </div>
           </div>
