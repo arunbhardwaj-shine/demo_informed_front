fae744f96635b05c0f615bd536c1283dcce87ad3
tarunwadhwa-ux <tarun.wadhwa@shinedezign.com>
2025-11-18 11:39:09 +0530

    CSS changes and added dummy page for training-compliances

---
+++ src/Components/CommonComponent/TrainingCompliances.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TrainingCompliances = () => {
+  return (
+    <div className="col-sm-9 col-md-10 right-container">
+      <div className="coming-soon-container" style={{ padding: "40px", textAlign: "center", color: "var(--light-blue)" }}>
+        <h1>Training & Compliances</h1>
+        <h2>Coming Soon</h2>
+      </div>
+    </div>
+  );
+};
+
+export default TrainingCompliances;
